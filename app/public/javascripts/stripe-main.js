ebee206bb11b373e07cec217500815573e3cb6f8
chrisfinch <chrisfinchy@gmail.com>
2014-04-22 16:53:51 +0100

    working stripe implementation

--- app/public/javascripts/stripe-main.js
+++ app/public/javascripts/stripe-main.js
@@ -22,8 +22,39 @@ require([
 
     "use strict";
 
+    var config = {
+        stripePublishableKey: 'pk_test_Qm3CGRdrV4WfGYCpm0sftR0f'
+    };
+
+    var stripeResponseHandler = function (status, response) {
+        var $form = $('#payment-form');
+
+        if (response.error) {
+            // Show the errors on the form
+            $form.find('.payment-errors').text(response.error.message);
+            $form.find('button').prop('disabled', false);
+        } else {
+            // token contains id, last4, and card type
+            var token = response.id;
+            // Insert the token into the form so it gets submitted to the server
+            $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+            // and submit
+            $form.get(0).submit();
+        }
+    };
+
     var init = function () {
-        console.log($, $.payment, Stripe);
+        Stripe.setPublishableKey(config.stripePublishableKey);
+
+        $('#payment-form').submit(function(event) {
+            event.preventDefault();
+            var $form = $(this);
+
+            // Disable the submit button to prevent repeated clicks
+            $form.find('button').prop('disabled', true);
+
+            Stripe.card.createToken($form, stripeResponseHandler);
+        });
     };
 
     $(init);
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var stripe = require('stripe')("***REMOVED***");
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Membership prototypes' });
@@ -9,6 +11,28 @@ router.get('/', function(req, res) {
 /* GET home page. */
 router.get('/stripe', function(req, res) {
   res.render('stripe', { title: 'Stripe' });
+});
+
+router.post('/stripe', function(req, res) {
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here https://manage.stripe.com/account
+    stripe.setApiKey("***REMOVED***");
+
+    // (Assuming you're using express - expressjs.com)
+    // Get the credit card details submitted by the form
+    var stripeToken = req.body.stripeToken;
+
+    var charge = stripe.charges.create({
+      amount: 1337, // amount in cents, again
+      currency: "gbp",
+      card: stripeToken,
+      description: "payinguser@example.com"
+    }, function(err, charge) {
+      if (err && err.type === 'StripeCardError') {
+        // The card has been declined
+      }
+      res.send(200, "Everything is ok: "+arguments.toString());
+    });
 });
 
 module.exports = router;
