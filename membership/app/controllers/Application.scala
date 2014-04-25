9bc0a60c3af739e538a0330a23db6e453d63dfaa
chrisfinch <chrisfinchy@gmail.com>
2014-04-25 12:28:23 +0100

    display some events

--- membership/app/controllers/Application.scala
+++ membership/app/controllers/Application.scala
@@ -7,10 +7,26 @@ import com.stripe.model._
 import scala.collection.convert.wrapAll._
 import play.api.data._
 import play.api.data.Forms._
+import play.api.libs.ws._
+import scala.concurrent.Future
+import play.api.libs.json.{Json, JsValue}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object Application extends Controller {
 
   case class StripePayment(token: String)
+
+  case class RichText(text: String, html: String)
+
+  case class EBEvent(name: RichText, description: RichText)
+
+  case class EBResponse(events: Seq[EBEvent])
+
+  implicit val ebRichText = Json.reads[RichText]
+
+  implicit val ebEventReads = Json.reads[EBEvent]
+
+  implicit val ebResponseReads = Json.reads[EBResponse]
 
   val stripePaymentForm = Form(
     mapping(
@@ -18,8 +34,25 @@ object Application extends Controller {
     )(StripePayment.apply)(StripePayment.unapply)
   )
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action.async {
+    for {
+      events <- getEventsList
+    } yield {
+      Ok(views.html.index("Your new application is ready.", events))
+    }
+  }
+
+  def getEventsList:Future[Seq[EBEvent]] = {
+
+    val url = "https://www.eventbriteapi.com/v3/users/99154249965/owned_events"
+
+    val request = WS.url(url).withQueryString(("token", "***REMOVED***"))
+
+    for {
+      response <- request.get()
+    } yield {
+      response.json.as[EBResponse].events
+    }
   }
 
   def stripe = Action {
