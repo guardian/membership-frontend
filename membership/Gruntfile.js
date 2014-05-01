32e9e909365bf189c69403f905e2616a0d2b9801
chrisfinch <chrisfinchy@gmail.com>
2014-05-01 11:51:43 +0100

    got karma working in gruntfile

--- membership/Gruntfile.js
+++ membership/Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function (grunt) {
 
     var isDev = (grunt.option('dev') !== undefined) ? Boolean(grunt.option('dev')) : process.env.GRUNT_ISDEV === '1';
     var pkg = grunt.file.readJSON('package.json');
-    var testConfDir = './common/test/assets/javascripts/conf/';
     var singleRun = grunt.option('single-run') !== false;
 
     if (isDev) {
@@ -90,11 +89,10 @@ module.exports = function (grunt) {
                 reporters: isDev ? ['dots'] : ['progress'],
                 singleRun: singleRun
             },
-            common: {
-                configFile: testConfDir + 'common.js'
-            },
-            facia: {
-                configFile: testConfDir + 'facia.js'
+
+            unit: {
+                configFile: 'karma.conf.js',
+                browsers: ['PhantomJS']
             }
         },
 
@@ -182,6 +180,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-karma');
 
     grunt.registerTask('compile', [
         'compile:css'
@@ -197,26 +196,12 @@ module.exports = function (grunt) {
 //        grunt.config('casperjsLogFile', app + '.xml');
 //        grunt.task.run(['env:casperjs', 'casperjs:' + app]);
 //    });
-    grunt.registerTask('test:unit', function(app) {
-        var apps = [];
-        // have we supplied an app
-        if (app) {
-            // does a karma setup exist for this app
-            if (!grunt.config('karma')[app]) {
-                grunt.log.warn('No tests for app "' + app + '"');
-                return true;
-            }
-            apps = [app];
-        } else { // otherwise run all
-            apps = Object.keys(grunt.config('karma')).filter(function(app) { return app !== 'options'; });
-        }
+    grunt.registerTask('test:unit', function() {
         grunt.config.set('karma.options.singleRun', (singleRun === false) ? false : true);
-        apps.forEach(function(app) {
-            grunt.task.run(['karma:' + app]);
-        });
+        grunt.task.run(['karma:unit']);
     });
     // TODO - don't have common as default?
-    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('test', ['jshint', 'test:unit']);
 
     grunt.registerTask('compile:css', ['clean:css', 'sass:compile']);
 };
