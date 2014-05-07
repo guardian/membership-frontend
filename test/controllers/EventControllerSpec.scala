3c9716e7a3cd2d63d3494b2decd0affd0afa8d5c
Ikenna Nwaiwu <ikenna.nwaiwu@guardian.co.uk>
2014-05-07 14:19:21 +0100

    Refactoring EventbriteService and the EventController

---
+++ test/controllers/EventControllerSpec.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import play.api.test.{FakeRequest, WithApplication, PlaySpecification}
+import services.EventbriteService
+
+
+object EventControllerSpec extends PlaySpecification with EventController {
+
+  val eventService: EventbriteService = SandboxEventbriteService
+
+  "Event Index page" should {
+
+    "display list of all events" in new WithApplication {
+      val result = renderEventsIndex(FakeRequest())
+      status(result) must equalTo(OK)
+      contentAsString(result) must contain("An evening with Chris Finch")
+      contentAsString(result) must contain("Chris High Res Party time extravaganza")
+    }
+
+    "display a single event" in new WithApplication {
+      val result = renderEventPage("11464752383")(FakeRequest())
+      status(result) must equalTo(OK)
+      contentAsString(result) must contain("Chris High Res Party time extravaganza")
+      contentAsString(result) must contain("11464752383")
+    }
+
+  }
+}
+
+
+object SandboxEventbriteService extends EventbriteService {
+  override val forEventList: String = "https://www.eventbriteapi.com/v3/users/99154249965/owned_events"
+  override val eventUrl: String = "https://www.eventbriteapi.com/v3/events/"
+  override val token: (String, String) = ("token", "***REMOVED***")
+}
