ff069b586f7db2519ccb36e3a1803b9889965299
chrisfinch <chrisfinchy@gmail.com>
2014-05-09 12:33:50 +0100

    a basic route matcher

---
+++ common/app/assets/javascripts/src/utils/router.js
@@ -0,0 +1,34 @@
+define(function () {
+
+    function Router () {
+        this.routes = {};
+    }
+
+    Router.prototype.match = function (route) {
+        this.route = route;
+
+        return this;
+    };
+
+    Router.prototype.to = function (fn) {
+        this.routes[this.route] = fn;
+    };
+
+    Router.prototype.go = function () {
+        var path = document.location.pathname;
+
+        this.routes['*']();
+        delete this.routes['*'];
+
+        for (var key in this.routes) {
+            var regxp = new RegExp('^'+ key);
+
+            if (path.match(regxp)) {
+                this.routes[key]();
+            }
+        }
+    };
+
+    return new Router();
+
+});
