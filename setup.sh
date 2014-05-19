5a81017e8b147c12404e485c48ae4e86b7646928
Chris Finch <chris.finch@guardian.co.uk>
2014-05-19 16:04:04 +0100

    stop swallowing output in setup script v2

--- setup.sh
+++ setup.sh
@@ -75,4 +75,5 @@ grunt hookup
 # Done
 #####################################################
 
+printf "\n\r\n\r====================================\n\r\n\r"
 printf "> Good to go -> 'play run' to start app\n\r\n\r"
