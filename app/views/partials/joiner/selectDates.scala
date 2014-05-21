b43541a81e50deaa3aed6f710726075720a18b0b
Chris Finch <chris.finch@guardian.co.uk>
2014-05-21 15:09:07 +0100

    make dates yy rather than yyyy

--- app/views/partials/joiner/selectDates.scala
+++ app/views/partials/joiner/selectDates.scala
@@ -1,10 +1,17 @@
 package views.partials.joiner
 
-import org.joda.time.DateTime
+import com.github.nscala_time.time.Imports._
 
 object selectDates {
-  def thisYear = DateTime.now().year().get()
-  def validCardYears = thisYear until (thisYear + 20)
+  val yearFormatter = DateTimeFormat.forPattern("yy")
 
-  def validCardMonths = 1 until 13
+  def validCardYears(): Seq[(Int, String)] = {
+    val now = DateTime.now
+    for (yearsFromNow <- 0 until 20) yield {
+      val date: DateTime = now + yearsFromNow.years
+      (date.getYear, yearFormatter.print(date))
+    }
+  }
+
+  def validCardMonths = 1 to 12
 }
