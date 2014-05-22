ae2ab26eed3f99e31807139f8d02c53b6ad77841
Ben Chidgey <ben@feedmypixel.com>
2014-05-22 15:28:34 +0100

    -1 removed from for loop

--- common/app/assets/javascripts/src/modules/events/ctaButton.js
+++ common/app/assets/javascripts/src/modules/events/ctaButton.js
@@ -19,7 +19,7 @@ define([
         var isUserLoggedIn = user.isLoggedIn();
 
         if(ctaButtons.length && isUserLoggedIn){
-            for (var i = 0, ctaButtonsLength = ctaButtons.length - 1; i <= ctaButtonsLength; i++) {
+            for (var i = 0, ctaButtonsLength = ctaButtons.length; i < ctaButtonsLength; i++) {
                 $(ctaButtons[i]).toggleClass(config.classes.LOGGED_IN_CLASS).text(config.text.LOGGED_IN_CTA_BUTTON);
             }
         }
