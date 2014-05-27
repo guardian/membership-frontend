f1ec5be26dc0313bdb62f588c2c7a465c1dd3d8f
Ben Chidgey <ben@feedmypixel.com>
2014-05-27 11:59:25 +0100

    refactor of todayDate method

--- frontend/app/views/Dates.scala
+++ frontend/app/views/Dates.scala
@@ -2,11 +2,6 @@ package views
 
 import com.github.nscala_time.time.Imports._
 import org.joda.time.{ PeriodType, Hours, Instant }
-import java.io.Serializable
-import org.joda.convert.FromString
-import org.joda.time.base.BasePeriod
-import org.joda.time.chrono.ISOChronology
-import org.joda.time.field.FieldUtils
 import org.joda.time.format.{ PeriodFormatterBuilder, ISOPeriodFormat, PeriodFormatter }
 
 object Dates {
@@ -23,5 +18,5 @@ object Dates {
     lazy val pretty = formatter.print(dateTime).replace("AM", "am").replace("PM", "pm")
   }
 
-  def todayDate() = DateTimeFormat.forPattern("dd/MM/yyyy").print(new DateTime())
+  def todayDate = DateTime.now.toString("dd/MM/yyyy")
 }
