3cf17a205d2d73eec10e6e2ee15a0df085cc3a76
Ikenna Nwaiwu <ikenna.nwaiwu@guardian.co.uk>
2014-06-02 16:26:19 +0100

    Improving error messge on Cache-Control filter

--- frontend/app/filters/CheckCacheHeadersFilter.scala
+++ frontend/app/filters/CheckCacheHeadersFilter.scala
@@ -9,7 +9,7 @@ object CheckCacheHeadersFilter extends Filter {
   def apply(nextFilter: RequestHeader => Future[SimpleResult])(requestHeader: RequestHeader): Future[SimpleResult] = {
     nextFilter(requestHeader).map { result =>
       val hasCacheControl = result.header.headers.contains("Cache-Control")
-      assert(hasCacheControl, "Cache-Control not set. Ensure any modified controllers have Cache-Control header set. Throwing exception... ")
+      assert(hasCacheControl, s"Cache-Control not set. Ensure controller response has Cache-Control header set for ${requestHeader.path}. Throwing exception... ")
       result
     }
   }
