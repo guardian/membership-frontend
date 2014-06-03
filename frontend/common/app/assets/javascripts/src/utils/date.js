028540c239af141c63d841a126ad7f374f944048
Chris Finch <chris.finch@guardian.co.uk>
2014-06-03 16:54:42 +0100

    guard for date prototype

--- frontend/common/app/assets/javascripts/src/utils/date.js
+++ frontend/common/app/assets/javascripts/src/utils/date.js
@@ -1,27 +1,29 @@
 /* http://stackoverflow.com/questions/1056728/where-can-i-find-documentation-on-formatting-a-date-in-javascript */
 define(function () {
-    Date.prototype.format = function(format) {
-        var o = {
-            'M+' : this.getMonth()+1, //month
-            'd+' : this.getDate(),    //day
-            'h+' : this.getHours(),   //hour
-            'm+' : this.getMinutes(), //minute
-            's+' : this.getSeconds(), //second
-            'q+' : Math.floor((this.getMonth()+3)/3),  //quarter
-            'S' : this.getMilliseconds() //millisecond
-        };
+    if(!Date.prototype.format) {
+        Date.prototype.format = function(format) {
+            var o = {
+                'M+' : this.getMonth()+1, //month
+                'd+' : this.getDate(),    //day
+                'h+' : this.getHours(),   //hour
+                'm+' : this.getMinutes(), //minute
+                's+' : this.getSeconds(), //second
+                'q+' : Math.floor((this.getMonth()+3)/3),  //quarter
+                'S' : this.getMilliseconds() //millisecond
+            };
 
-        if(/(y+)/.test(format)) {
-            format = format.replace(RegExp.$1,
-            (this.getFullYear()+'').substr(4 - RegExp.$1.length));
-        }
-        for(var k in o) {
-            if(new RegExp('('+ k +')').test(format)) {
+            if(/(y+)/.test(format)) {
                 format = format.replace(RegExp.$1,
-                        RegExp.$1.length === 1 ? o[k] :
-                        ('00' + o[k]).substr(('' + o[k]).length));
+                    (this.getFullYear()+'').substr(4 - RegExp.$1.length));
             }
-        }
-        return format;
-    };
+            for(var k in o) {
+                if(new RegExp('('+ k +')').test(format)) {
+                    format = format.replace(RegExp.$1,
+                            RegExp.$1.length === 1 ? o[k] :
+                            ('00' + o[k]).substr(('' + o[k]).length));
+                }
+            }
+            return format;
+        };
+    }
 });
