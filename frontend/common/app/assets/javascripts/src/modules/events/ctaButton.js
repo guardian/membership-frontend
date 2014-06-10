0da6f66945b159a4342acf651abd13b5d611966e
Ben Chidgey <ben@feedmypixel.com>
2014-06-10 09:42:41 +0100

    added SIF around ctaButton work

--- frontend/common/app/assets/javascripts/src/modules/events/ctaButton.js
+++ frontend/common/app/assets/javascripts/src/modules/events/ctaButton.js
@@ -1,31 +1,35 @@
 define([
     '$',
     'src/utils/user'
-], function($, user){
+], function ($, user) {
 
-    var config = {
-        classes: {
-            LOGGED_IN_CLASS: 'action--logged-in',
-            LOGGED_OUT_CLASS: 'action--logged-out'
-        },
-        text: {
-            LOGGED_IN_CTA_BUTTON: 'Book Event'
-        }
-    };
+    return (function (user) {
 
-    var init = function(){
-
-        var ctaButtons = document.querySelectorAll('.' + config.classes.LOGGED_OUT_CLASS);
-        var isUserLoggedIn = user.isLoggedIn();
-
-        if(ctaButtons.length && isUserLoggedIn){
-            for (var i = 0, ctaButtonsLength = ctaButtons.length; i < ctaButtonsLength; i++) {
-                $(ctaButtons[i]).toggleClass(config.classes.LOGGED_IN_CLASS).text(config.text.LOGGED_IN_CTA_BUTTON);
+        var config = {
+            classes: {
+                LOGGED_IN_CLASS: 'action--logged-in',
+                LOGGED_OUT_CLASS: 'action--logged-out'
+            },
+            text: {
+                LOGGED_IN_CTA_BUTTON: 'Book Event'
             }
-        }
-    };
+        };
 
-    return {
-        init: init
-    };
+        var init = function () {
+
+            var ctaButtons = document.querySelectorAll('.' + config.classes.LOGGED_OUT_CLASS);
+            var isUserLoggedIn = user.isLoggedIn();
+
+            if (ctaButtons.length && isUserLoggedIn) {
+                for (var i = 0, ctaButtonsLength = ctaButtons.length; i < ctaButtonsLength; i++) {
+                    $(ctaButtons[i]).toggleClass(config.classes.LOGGED_IN_CLASS).text(config.text.LOGGED_IN_CTA_BUTTON);
+                }
+            }
+        };
+
+        return {
+            init: init
+        };
+
+    })(user);
 });
