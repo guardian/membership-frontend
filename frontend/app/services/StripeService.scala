82494d5bd47bd20af2b6043c89cba52432bb318f
Ikenna Nwaiwu <ikenna.nwaiwu@guardian.co.uk>
2014-06-12 15:59:26 +0100

    Refactoring test

--- frontend/app/services/StripeService.scala
+++ frontend/app/services/StripeService.scala
@@ -23,35 +23,19 @@ trait StripeService {
   implicit val readsSubscriptionList = Json.reads[SubscriptionList]
   implicit val readsCustomer = Json.reads[Customer]
 
-  private def request(endpoint: String) ={
-    val r = WS.url(s"$apiURL/$endpoint").withHeaders(("Authorization", s"Bearer $apiSecret"))
-    println(r.url)
-    r
-
-  }
+  private def request(endpoint: String) =  WS.url(s"$apiURL/$endpoint").withHeaders(("Authorization", s"Bearer $apiSecret"))
 
   private def extract[A <: StripeObject](response: Response)(implicit reads: Reads[A]): A = {
-//    println("Body beofre extraction ******** " + response.body)
     response.json.asOpt[A].getOrElse {
       throw (response.json \ "error").asOpt[Error].getOrElse(Error("internal", "Unable to extract object"))
     }
   }
 
-  def post[A <: StripeObject](endpoint: String, data: Map[String, Seq[String]])(implicit reads: Reads[A]): Future[A] ={
-//    println("Posting data *** " + data)
-    val r:Future[A] = request(endpoint).post(data).map(extract[A])
-//    r.onComplete{
-//      case Success(a) => println(s"** post $endpoint \n Data:  $data" )
-//      case Failure(e) => println(s"** post failure $e" )
-//    }
-    r
-  }
+  def post[A <: StripeObject](endpoint: String, data: Map[String, Seq[String]])(implicit reads: Reads[A]): Future[A] =
+    request(endpoint).post(data).map(extract[A])
 
-  def get[A <: StripeObject](endpoint: String)(implicit reads: Reads[A]): Future[A] ={
-    val r = request(endpoint).get().map(extract[A])
-    r.onSuccess{case a => println(s"** get $endpoint")}
-    r
-  }
+  def get[A <: StripeObject](endpoint: String)(implicit reads: Reads[A]): Future[A] =
+    request(endpoint).get().map(extract[A])
 
     def createCharge(amount: Int, currency: String, card: String, description: String): Future[Charge] = {
       post[Charge]("charges", Map(
@@ -69,8 +53,9 @@ trait StripeService {
     def readCustomer(customerId: String): Future[Customer] =
       get[Customer](s"customers/$customerId")
 
-    def createSubscription(customerId: String, planId: String): Future[Subscription] =
+    def createSubscription(customerId: String, planId: String): Future[Subscription] = {
       post[Subscription](s"customers/$customerId/subscriptions", Map("plan" -> Seq(planId)))
+    }
 }
 
 object StripeService extends StripeService {
--- frontend/test/services/StripeServiceIntegrationTest.scala
+++ frontend/test/services/StripeServiceIntegrationTest.scala
@@ -1,6 +1,5 @@
 package services
 
-import org.specs2.mutable.BeforeAfter
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock._
@@ -9,52 +8,88 @@ import scala.io.Source
 import scala.concurrent.duration._
 import scala.concurrent._
 import play.api.test.PlaySpecification
-import model.Stripe.{SubscriptionList, CardList, Card}
+import model.Stripe._
+import org.specs2.specification.{Step, Fragments}
+import model.Stripe.SubscriptionList
+import model.Stripe.Card
+import model.Stripe.CardList
 
 
-class StripeServiceIntegrationTest extends PlaySpecification {
-
-  val Port = 8080
+class StripeServiceIntegrationTest extends BeforeAllAfterAll {
+  val Port = 9999
   val Host = "localhost"
-  val token = "***REMOVED***"
+  val stripeApiSecret = "***REMOVED***"
+  val cardToken = "tok_104Bpz2eZvKYlo2CRWVWL4Ou"
+  val wireMockServer = new WireMockServer(wireMockConfig().port(Port))
+  WireMock.configureFor(Host, Port)
 
-  trait StubServer extends BeforeAfter {
-    val wireMockServer = new WireMockServer(wireMockConfig().port(Port))
+  override protected def beforeAll(): Unit = wireMockServer.start()
 
-    def before = wireMockServer.start()
-
-    def after = wireMockServer.stop()
-  }
+  override protected def afterAll(): Unit = wireMockServer.stop()
 
   "SubscriptionService" should {
-    "create customers" in new StubServer {
-      val path = "/v1/customers"
 
-      stubFor(post(urlEqualTo(path))
-        .withRequestBody(WireMock.equalTo(s"card=$token"))
-        .willReturn(
-          aResponse()
-            .withStatus(200)
-            .withHeader("Content-Type", "application/json;charset=utf-8")
-            .withBody(loadFile("../customers.json"))))
+    "create customers" in {
+      val given = Given(
+        url = "/v1/customers",
+        body = s"card=$cardToken",
+        filePath = "../customers.json"
+      )
+      def when = TestStripeService.createCustomer(cardToken)
+      val expectResult = Customer("cus_4Bt317qP0aKefO", SubscriptionList(Nil), CardList(List(Card("Visa", "4242"))))
 
-      val customerFuture = TestStripeService.createCustomer(token)
+      executeTest(given)(when)(expectResult)
+    }
 
-      val customer = Await.result(customerFuture, 2 seconds)
-      customer.id mustEqual ("cus_4Bt317qP0aKefO")
-      customer.cards mustEqual (CardList(List(Card("Visa", "4242"))))
-      customer.subscriptions mustEqual (SubscriptionList(Nil))
+    "should create a subscription" in {
+      val given = Given(
+        url = s"/v1/customers/$stripeApiSecret/subscriptions",
+        body = "plan=Patron",
+        filePath = "../subscription.json"
+      )
+      def when = TestStripeService.createSubscription(stripeApiSecret, "Patron")
+      val expectResult = Subscription(id = "sub_4Bt3MJQXzF3RG9", start = 1402329978, current_period_end = 1404921978, Plan("Patron", "Patron plan", 6000))
+
+      executeTest(given)(when)(expectResult)
     }
   }
 
   def loadFile(fileName: String): String = Source.fromInputStream(getClass.getResourceAsStream(fileName)).mkString
 
+
+  object TestStripeService extends StripeService {
+    override protected val apiURL: String = s"http://localhost:$Port/v1"
+    override protected val apiSecret: String = "test_api_secret"
+  }
+
+  case class Given(url: String, body: String, filePath: String)
+
+  def executeTest(given: Given)(functionUnderTest: => Future[_])(expected: Any) = {
+    stubFor(post(urlMatching(given.url))
+      .withRequestBody(WireMock.equalTo(given.body))
+      .withHeader("Authorization", WireMock.matching("Bearer test_api_secret*"))
+      .willReturn(aResponse()
+      .withStatus(200)
+      .withHeader("Content-Type", "application/json;charset=utf-8")
+      .withBody((loadFile(given.filePath)
+      ))))
+
+    val resultFuture = functionUnderTest
+    val result = Await.result(resultFuture, 2 seconds)
+
+    result mustEqual expected
+  }
+
 }
 
 
-object TestStripeService extends StripeService {
-  override protected val apiURL: String = "http://localhost:8080/v1"
-  override protected val apiSecret: String = "test_api_secret"
+trait BeforeAllAfterAll extends PlaySpecification {
+  override def map(fragments: => Fragments) =
+    Step(beforeAll) ^ fragments ^ Step(afterAll)
+
+  protected def beforeAll()
+
+  protected def afterAll()
 }
 
 
