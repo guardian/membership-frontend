0d0247390b3f1ce2c6661c7439cbfa6c213a9ca3
Ben Chidgey <ben@feedmypixel.com>
2014-06-13 17:23:10 +0100

    added stripe messages

---
+++ frontend/common/app/assets/javascripts/config/stripeErrorMessages.js
@@ -0,0 +1,19 @@
+define({
+    invalid_request_error: {},
+    api_error: {
+        rate_limit: ''
+    },
+    card_error: {
+        incorrect_number: 'Sorry, the card number that you have entered is incorrect. Please check and retype.',
+        incorrect_cvc: 'Sorry, the security code that you have entered is incorrect. Please check and retype.',
+        invalid_number: 'Sorry, the card number that you have entered is incorrect. Please check and retype.',
+        invalid_expiry: 'Sorry, the expiry date that you have entered is invalid. Please check and re-enter.',
+        invalid_cvc: 'Sorry, the security code that you have entered is invalid. Please check and retype.',
+        expired_card: 'Sorry, this card has expired. Please try again with another card.',
+        card_declined: 'We\'re very sorry but your card was declined. Please try again with another card.',
+        processing_error: 'Sorry, we weren\'t able to process your payment this time around. Please try again.',
+        generic_payment: 'Sorry, there has been a problem processing your payment. Please try again later.',
+        generic_new_card: 'Sorry, there has been a problem updating your payment details. Please check and retype.',
+        client_validation: 'Sorry, we\'ve found some problems with your details. Please check and retype.'
+    }
+});
