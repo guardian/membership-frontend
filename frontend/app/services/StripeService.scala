fb86292377a1f16fbb8be3e8a083f7183c9f8033
Will Franklin <will.franklin@guardian.co.uk>
2014-06-16 11:04:58 +0100

    Override get/post direct

--- frontend/app/services/StripeService.scala
+++ frontend/app/services/StripeService.scala
@@ -10,9 +10,6 @@ import model.Stripe._
 import configuration.Config
 
 trait StripeService {
-  protected val apiURL: String
-  protected val apiSecret: String
-
   implicit val readsError = Json.reads[Error]
   implicit val readsCard = Json.reads[Card]
   implicit val readsCardList = Json.reads[CardList]
@@ -22,21 +19,13 @@ trait StripeService {
   implicit val readsSubscriptionList = Json.reads[SubscriptionList]
   implicit val readsCustomer = Json.reads[Customer]
 
-  private def headers = ("Authorization", s"Bearer $apiSecret")
-
+  def post[A <: StripeObject](endpoint: String, data: Map[String, Seq[String]])(implicit reads: Reads[A]): Future[A]
+  def get[A <: StripeObject](endpoint: String)(implicit reads: Reads[A]): Future[A]
 
   private def extract[A <: StripeObject](response: Response)(implicit reads: Reads[A]): A = {
     response.json.asOpt[A].getOrElse {
       throw (response.json \ "error").asOpt[Error].getOrElse(Error("internal", "Unable to extract object"))
     }
-  }
-
-  def post[A <: StripeObject](endpoint: String, data: Map[String, Seq[String]])(implicit reads: Reads[A]): Future[A] = {
-    httpPost(s"$apiURL/$endpoint", data, headers).map(extract[A])
-  }
-
-  def get[A <: StripeObject](endpoint: String)(implicit reads: Reads[A]): Future[A] = {
-    httpGet(s"$apiURL/$endpoint", headers).map(extract[A])
   }
 
   def createCharge(amount: Int, currency: String, card: String, description: String): Future[Charge] = {
@@ -58,17 +47,15 @@ trait StripeService {
   def createSubscription(customerId: String, planId: String): Future[Subscription] = {
     post[Subscription](s"customers/$customerId/subscriptions", Map("plan" -> Seq(planId)))
   }
-
-  def httpPost(url: String, data: Map[String, Seq[String]], header: (String, String)*): Future[Response] = {
-    WS.url(url).withHeaders(header: _*).post(data)
-  }
-
-  def httpGet(url: String, header: (String, String)*): Future[Response] = {
-    WS.url(url).withHeaders(header: _*).get
-  }
 }
 
 object StripeService extends StripeService {
-  protected val apiURL = Config.stripeApiURL
-  protected val apiSecret = Config.stripeApiSecret
+  val apiURL = Config.stripeApiURL
+  val apiAuthHeader = ("Authorization", s"Bearer ${Config.stripeApiSecret}")
+
+  def get[A <: StripeObject](endpoint: String)(implicit reads: Reads[A]): Future[A] =
+    WS.url(s"$apiURL/$endpoint").withHeaders(apiAuthHeader).get().map(extract[A])
+
+  def post[A <: StripeObject](endpoint: String, data: Map[String, Seq[String]])(implicit reads: Reads[A]): Future[A] =
+    WS.url(s"$apiURL/$endpoint").withHeaders(apiAuthHeader).post(data).map(extract[A])
 }
