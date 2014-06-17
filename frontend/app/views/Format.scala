27e0bc449fd20ca8fa44a182a4f081cd0e483cd3
Ben Chidgey <ben@feedmypixel.com>
2014-06-17 16:08:07 +0100

    changed entity to a £ and removed html wrapper

--- frontend/app/views/Format.scala
+++ frontend/app/views/Format.scala
@@ -1,12 +1,10 @@
 package views
 
-import play.api.templates.Html
-
 object Format {
-  def formatPounds(pounds: Int): Html = {
-    Html("&#163;" + pounds + "." + "00")
+  def formatPounds(pounds: Int): String = {
+    "£" + pounds + "." + "00"
   }
-  def formatPence(pence: Int): Html = {
-    Html("&#163;" + (pence / 100) + "." + ("%02d".format(pence % 100)))
+  def formatPence(pence: Int): String = {
+    "£" + (pence / 100) + "." + ("%02d".format(pence % 100))
   }
 }
