257ae18821e5c0fffd0c937ccca5447f989322a8
Ben Chidgey <ben@feedmypixel.com>
2014-06-18 10:52:20 +0100

    added specified decline_errors and generic decline_error

--- frontend/common/app/assets/javascripts/config/stripeErrorMessages.js
+++ frontend/common/app/assets/javascripts/config/stripeErrorMessages.js
@@ -11,7 +11,11 @@ define({
         invalid_cvc: 'Sorry, the security code that you have entered is invalid. Please check and retype.',
         expired_card: 'Sorry, this card has expired. Please try again with another card.',
         card_declined: {
-            card_not_supported: 'Sorry, this card is not accepted. Please use Visa, Mastercard or American Express'
+            generic_decline: 'We\'re sorry. Your card has been declined.',
+            card_not_supported:	'We\'re sorry. We can\'t take payment with this type of card. Please try again using Visa, Mastercard or American Express.',
+            currency_not_supported:	'We\'re sorry. You\'ve tried to pay using a currency we don\'t accept.',
+            card_not_supported: 'Sorry, this card is not accepted. Please use Visa, Mastercard or American Express',
+            try_again_later: 'We can\'t process your payment right now. Please try again later.'
         },
         processing_error: 'Sorry, we weren\'t able to process your payment this time around. Please try again.',
         client_validation: 'Sorry, we\'ve found some problems with your details. Please check and retype.'
