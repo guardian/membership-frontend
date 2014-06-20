46b72f7f2d9dbcea730ad7a02b829229290934fe
Ben Chidgey <ben@feedmypixel.com>
2014-06-20 12:17:05 +0100

    Tier controller tweaks

--- frontend/app/controllers/Tier.scala
+++ frontend/app/controllers/Tier.scala
@@ -1,9 +1,7 @@
 package controllers
 
 import play.api.mvc.Controller
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import actions.{AuthenticatedAction, MemberAction}
-import services.StripeService
 
 trait Tier extends Controller {
 
@@ -15,16 +13,14 @@ trait Tier extends Controller {
     Ok(views.html.tier.downgrade.confirm())
   }
 
-  def downgradeSummary() = MemberAction.async { implicit request =>
-    StripeService.Customer.read(request.member.customerId).map { customer =>
-      val response = for {
-        subscription <- customer.subscriptions.data.headOption
-        card <- customer.cards.data.headOption
-      } yield Ok(views.html.tier.downgrade.summary(subscription, card))
-
-      response.getOrElse(NotFound)
-    }
+  def downgradeSummary() = AuthenticatedAction { implicit request =>
+    Ok(views.html.tier.downgrade.summary())
   }
+
+  def downgradeTier() = AuthenticatedAction { implicit request =>
+    Redirect("/tier/downgrade/summary")
+  }
+
 }
 
 object Tier extends Tier
