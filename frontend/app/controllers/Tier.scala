f4f268b7c0f15863ee88420e7e2d58b1119c7aca
Ben Chidgey <ben@feedmypixel.com>
2014-06-23 10:00:33 +0100

    getting details from stripe for user

--- frontend/app/controllers/Tier.scala
+++ frontend/app/controllers/Tier.scala
@@ -1,7 +1,9 @@
 package controllers
 
 import play.api.mvc.Controller
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import actions.{AuthenticatedAction, MemberAction}
+import services.StripeService
 
 trait Tier extends Controller {
 
@@ -13,20 +15,34 @@ trait Tier extends Controller {
     Ok(views.html.tier.downgrade.confirm())
   }
 
-  def downgradeSummary() = AuthenticatedAction { implicit request =>
-    Ok(views.html.tier.downgrade.summary())
+  def downgradeSummary() = MemberAction.async { implicit request =>
+    StripeService.Customer.read(request.member.customerId).map { customer =>
+      val response = for {
+        subscription <- customer.subscriptions.data.headOption
+        card <- customer.cards.data.headOption
+      } yield Ok(views.html.tier.downgrade.summary(subscription, card))
+
+      response.getOrElse(NotFound)
+    }
   }
 
   def downgradeTier() = AuthenticatedAction { implicit request =>
-    Redirect("/tier/downgrade/summary")
+      Redirect("/tier/downgrade/summary")
   }
 
   def confirmCancel() = AuthenticatedAction { implicit request =>
     Ok(views.html.tier.cancel.confirm())
   }
 
-  def cancelSummary() = AuthenticatedAction { implicit request =>
-    Ok(views.html.tier.cancel.summary())
+  def cancelSummary() = MemberAction.async { implicit request =>
+    StripeService.Customer.read(request.member.customerId).map { customer =>
+      val response = for {
+        subscription <- customer.subscriptions.data.headOption
+        card <- customer.cards.data.headOption
+      } yield Ok(views.html.tier.cancel.summary(subscription, card))
+
+      response.getOrElse(NotFound)
+    }
   }
 
   def cancelTier() = AuthenticatedAction { implicit request =>
