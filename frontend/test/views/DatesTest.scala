3c086ddac777247bea17072dfbda0582e19d4ea2
Ben Chidgey <ben@feedmypixel.com>
2014-06-26 11:23:10 +0100

    made tests more concise

--- frontend/test/views/DatesTest.scala
+++ frontend/test/views/DatesTest.scala
@@ -6,46 +6,19 @@ class DatesTest extends Specification {
   "addSuffix" should {
 
     "append the correct suffix for 'th'" in {
-      Dates.suffix(4) mustEqual "th"
-      Dates.suffix(5) mustEqual "th"
-      Dates.suffix(6) mustEqual "th"
-      Dates.suffix(7) mustEqual "th"
-      Dates.suffix(8) mustEqual "th"
-      Dates.suffix(9) mustEqual "th"
-      Dates.suffix(10) mustEqual "th"
-      Dates.suffix(11) mustEqual "th"
-      Dates.suffix(12) mustEqual "th"
-      Dates.suffix(13) mustEqual "th"
-      Dates.suffix(14) mustEqual "th"
-      Dates.suffix(15) mustEqual "th"
-      Dates.suffix(16) mustEqual "th"
-      Dates.suffix(17) mustEqual "th"
-      Dates.suffix(18) mustEqual "th"
-      Dates.suffix(19) mustEqual "th"
-      Dates.suffix(20) mustEqual "th"
-      Dates.suffix(24) mustEqual "th"
-      Dates.suffix(25) mustEqual "th"
-      Dates.suffix(26) mustEqual "th"
-      Dates.suffix(27) mustEqual "th"
-      Dates.suffix(28) mustEqual "th"
-      Dates.suffix(29) mustEqual "th"
-      Dates.suffix(30) mustEqual "th"
+      forall((4 to 20) ++ (24 to 30)) ((num:Int) => Dates.suffix(num) mustEqual("th"))
     }
 
     "append the correct suffix for 'st" in {
-      Dates.suffix(1) mustEqual "st"
-      Dates.suffix(21) mustEqual "st"
-      Dates.suffix(31) mustEqual "st"
+      forall(Seq(1, 21, 31)) ((num:Int) => Dates.suffix(num) mustEqual("st"))
     }
 
     "append the correct suffix for 'nd" in {
-      Dates.suffix(2) mustEqual "nd"
-      Dates.suffix(22) mustEqual "nd"
+      forall(Seq(2, 22)) ((num:Int) => Dates.suffix(num) mustEqual("nd"))
     }
 
     "append the correct suffix for 'rd" in {
-      Dates.suffix(3) mustEqual "rd"
-      Dates.suffix(23) mustEqual "rd"
+      forall(Seq(3, 23)) ((num:Int) => Dates.suffix(num) mustEqual("rd"))
     }
   }
 }
