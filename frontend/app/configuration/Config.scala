86e168342eb59e3134bfd684eee5bd0c1647cb52
Ben Chidgey <ben@feedmypixel.com>
2014-06-27 16:44:27 +0100

    added lazy vals for titles being used across the site

--- frontend/app/configuration/Config.scala
+++ frontend/app/configuration/Config.scala
@@ -11,6 +11,20 @@ object Config {
 
   lazy val siteTitle = config.getString("site.title")
 
+  lazy val indexPageTitle = config.getString("index.page.title")
+  lazy val tierPageTitle = config.getString("tier.page.title")
+  lazy val joinerPageTitle = config.getString("joiner.page.title")
+  lazy val joinerPaymentPageTitle = config.getString("joiner.payment.page.title")
+  lazy val joinerThankyouPageTitle = config.getString("joiner.thankyou.page.title")
+  lazy val eventListPageTitle = config.getString("event.list.page.title")
+  lazy val eventDetailPageTitle = config.getString("event.detail.page.title")
+  lazy val tierChangePageTitle = config.getString("tier.change.page.title")
+  lazy val tierDowngradeConfirmPageTitle = config.getString("tier.downgrade.confirm.page.title")
+  lazy val tierDowngradeSummaryPageTitle = config.getString("tier.downgrade.summary.page.title")
+  lazy val tierCancelConfirmPageTitle = config.getString("tier.cancel.confirm.page.title")
+  lazy val tierCancelSummaryPageTitle = config.getString("tier.cancel.summary.page.title")
+  lazy val healthCheckPageTitle = config.getString("healthcheck.page.title")
+
   lazy val awsAccessKey = config.getString("aws.access.key")
   lazy val awsSecretKey = config.getString("aws.secret.key")
 
