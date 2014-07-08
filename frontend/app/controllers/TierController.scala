1a47f5dbee16a8382e03adedfacfdaee0708898c
Will Franklin <will.franklin@guardian.co.uk>
2014-07-08 12:04:22 +0100

    Refactored tier controller into traits

--- frontend/app/controllers/TierController.scala
+++ frontend/app/controllers/TierController.scala
@@ -18,7 +18,39 @@ case class AddressForm(street: String, postCode: String, city: String, country: 
 
 case class UpgradeTierForm(paymentType: String, stripeToken: String, deliveryAddress: AddressForm, billingAddress: AddressForm)
 
-trait TierController extends Controller {
+trait DowngradeTier {
+  self: TierController =>
+
+  def friendDowngrade() = MemberAction { implicit request =>
+    Ok(views.html.tier.downgrade.confirm())
+  }
+
+  def friendDowngradeConfirm() = MemberAction.async { implicit request => // POST
+    for {
+      customer <- StripeService.Customer.read(request.member.customerId)
+      cancelledOpt = customer.subscription.map { subscription =>
+        StripeService.Subscription.delete(customer.id, subscription.id)
+      }
+      cancelled <- Future.sequence(cancelledOpt.toSeq)
+    } yield {
+      cancelled.headOption.map(_ => Redirect("/tier/change/friend/summary")).getOrElse(NotFound)
+    }
+  }
+
+  def friendDowngradeSummary() = MemberAction.async { implicit request =>
+    StripeService.Customer.read(request.member.customerId).map { customer =>
+      val response = for {
+        subscription <- customer.subscription
+        card <- customer.card
+      } yield Ok(views.html.tier.downgrade.summary(subscription, card))
+
+      response.getOrElse(NotFound)
+    }
+  }
+}
+
+trait UpgradeTier {
+  self: TierController =>
 
   val upgradeTierForm: Form[UpgradeTierForm] = Form(
     mapping(
@@ -39,80 +71,25 @@ trait TierController extends Controller {
     )(UpgradeTierForm.apply)(UpgradeTierForm.unapply)
   )
 
-  def change() = MemberAction { implicit request =>
-    Ok(views.html.tier.change(request.member.tier))
+  def upgrade(tierStr: String) = MemberAction { implicit request =>
+    val tier = Tier.routeMap(tierStr)
+
+    if (request.member.tier < tier)
+      Ok(views.html.tier.upgrade.upgradeForm(tier))
+    else
+      NotFound
   }
 
-  def downgradeToFriend() = MemberAction { implicit request =>
-    Ok(views.html.tier.downgrade.confirm())
-  }
+  def upgradeConfirm(tierStr: String) = MemberAction.async { implicit request =>
+    val tier = Tier.routeMap(tierStr)
 
-  def confirmDowngradeToFriend() = MemberAction.async { implicit request => // POST
-    for {
-      customer <- StripeService.Customer.read(request.member.customerId)
-      cancelledOpt = customer.subscription.map { subscription =>
-        StripeService.Subscription.delete(customer.id, subscription.id)
-      }
-      cancelled <- Future.sequence(cancelledOpt.toSeq)
-    } yield {
-      cancelled.headOption.map(_ => Redirect("/tier/change/friend/summary")).getOrElse(NotFound)
-    }
-  }
-
-  def summaryFriend() = MemberAction.async { implicit request =>
-    StripeService.Customer.read(request.member.customerId).map { customer =>
-      val response = for {
-        subscription <- customer.subscription
-        card <- customer.card
-      } yield Ok(views.html.tier.downgrade.summary(subscription, card))
-
-      response.getOrElse(NotFound)
-    }
-  }
-
-  // Upgrade flow =====================================
-
-  def upgradeTo(tierName: String) = MemberAction { implicit request =>
-    val changingToTier = Tier.withName(tierName.capitalize)
-    request.member.tier match {
-      case Tier.Friend => Ok(views.html.tier.upgrade.upgradeForm(changingToTier))
-      case Tier.Partner => Ok(views.html.tier.upgrade.upgradeForm(changingToTier))
-      case _ => NotFound
-    }
-
-  }
-
-  def confirmUpgradeTo(tierName: String) = MemberAction.async { implicit request => // POST
-    val changingToTier = Tier.withName(tierName.capitalize)
-    if (changingToTier > request.member.tier) {
-      val formValues = upgradeTierForm.bindFromRequest
-      formValues.fold(_ => Future.successful(BadRequest), makePayment(changingToTier))
-    } else {
+    if (request.member.tier < tier)
+      upgradeTierForm.bindFromRequest.fold(_ => Future.successful(BadRequest), makePayment(tier))
+    else
       Future.successful(NotFound)
-    }
-  }
-
-  def confirmCancel() = AuthenticatedAction { implicit request =>
-    Ok(views.html.tier.cancel.confirm())
-  }
-
-  def cancelSummary() = MemberAction.async { implicit request =>
-    StripeService.Customer.read(request.member.customerId).map { customer =>
-      val response = for {
-        subscription <- customer.subscription
-        card <- customer.card
-      } yield Ok(views.html.tier.cancel.summary(subscription, card))
-
-      response.getOrElse(NotFound)
-    }
-  }
-
-  def cancelTier() = AuthenticatedAction { implicit request =>
-    Redirect("/tier/cancel/summary")
   }
 
   def makePayment(tier: Tier)(formData: UpgradeTierForm)(implicit request: MemberRequest[_]) = {
-
     val futureCustomer =
       if (request.member.customerId == Member.NO_CUSTOMER_ID)
         StripeService.Customer.create(request.user.getPrimaryEmailAddress, formData.stripeToken)
@@ -132,7 +109,36 @@ trait TierController extends Controller {
       MemberService.put(request.member.copy(tier = tier, customerId = customer.id))
       Ok("")
     }
+  }
+}
 
+trait CancelTier {
+  self: TierController =>
+
+  def cancelTier() = MemberAction { implicit request =>
+    Ok(views.html.tier.cancel.confirm())
+  }
+
+  def cancelTierConfirm() = MemberAction { implicit request =>
+    Redirect("/tier/cancel/summary")
+  }
+
+  def cancelTierSummary() = MemberAction.async { implicit request =>
+    StripeService.Customer.read(request.member.customerId).map { customer =>
+      val response = for {
+        subscription <- customer.subscription
+        card <- customer.card
+      } yield Ok(views.html.tier.cancel.summary(subscription, card))
+
+      response.getOrElse(NotFound)
+    }
+  }
+}
+
+trait TierController extends Controller with UpgradeTier with DowngradeTier with CancelTier {
+
+  def change() = MemberAction { implicit request =>
+    Ok(views.html.tier.change(request.member.tier))
   }
 }
 
