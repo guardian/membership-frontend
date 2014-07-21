3d297813f2e73857775a43e39a3de05170e33dcf
Will Franklin <will.franklin@guardian.co.uk>
2014-07-21 12:00:34 +0100

    Added basic 404 page

--- frontend/app/Global.scala
+++ frontend/app/Global.scala
@@ -1,10 +1,20 @@
+import scala.concurrent.Future
+
+import play.api.mvc.{Result, RequestHeader, WithFilters}
+import play.api.mvc.Results.NotFound
+import play.api.Application
+
+import controllers.NoCache
 import filters.CheckCacheHeadersFilter
-import play.api.mvc.WithFilters
 import services.EventbriteService
-import play.api.{ Play, Logger, Application, GlobalSettings }
+
 
 object Global extends WithFilters(CheckCacheHeadersFilter) {
   override def onStart(app: Application) {
     EventbriteService.start()
+  }
+
+  override def onHandlerNotFound(request: RequestHeader): Future[Result] = {
+    Future.successful(NoCache(NotFound(views.html.error404())))
   }
 }
