1d8579156d8240128907a43a20050ac66efcf63e
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-07-28 11:45:02 +0100

    Add assetMap so we can pull various files in easily

---
+++ frontend/app/views/support/Asset.scala
@@ -0,0 +1,15 @@
+package views.support
+
+import scala.io.Source
+
+import play.api.libs.json.{Json, JsObject}
+import collection.mutable.{ Map => MutableMap }
+
+object Asset {
+  lazy val map = {
+    val json = Json.parse(Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("assets.map")).mkString)
+    json.as[JsObject].fields.toMap.mapValues(_.as[String])
+  }
+
+  def at(path: String): String = "/assets/" + map.getOrElse(path, path)
+}
