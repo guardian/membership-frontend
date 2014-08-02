5d5f555668e549384b2af704038769108e732e13
Roberto Tyley <roberto.tyley@gmail.com>
2014-08-02 10:47:54 +0100

    Bind the Tier type for strongly-typed route path arguments
    Add a 'play.api.mvc.PathBindable' so that we don't have to parse the tier
    string every single time it's used as a path parameter!

--- frontend/app/controllers/Joiner.scala
+++ frontend/app/controllers/Joiner.scala
@@ -1,11 +1,9 @@
 package controllers
 
-import play.api.mvc.Controller
+import actions.{AuthenticatedAction, PaidMemberAction}
+import com.gu.membership.salesforce.Tier._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-
-import com.gu.membership.salesforce.Tier
-
-import actions.{PaidMemberAction, AuthenticatedAction}
+import play.api.mvc.Controller
 import services.{MemberRepository, StripeService}
 
 trait Joiner extends Controller {
@@ -14,16 +12,16 @@ trait Joiner extends Controller {
     Ok(views.html.joiner.tierList())
   }
 
-  def enterDetails(tierString: String) = AuthenticatedAction { implicit request =>
-    Tier.routeMap(tierString) match {
-      case Tier.Friend => Ok(views.html.joiner.detail.addressForm())
+  def enterDetails(tier: Tier) = AuthenticatedAction { implicit request =>
+    tier match {
+      case Friend => Ok(views.html.joiner.detail.addressForm())
       case paidTier => Ok(views.html.joiner.payment.paymentForm(paidTier))
     }
   }
 
   def joinFriend() = AuthenticatedAction.async { implicit request =>
     for {
-      member <- MemberRepository.upsert(request.user, "", Tier.Friend)
+      member <- MemberRepository.upsert(request.user, "", Friend)
     } yield Redirect(routes.Joiner.thankyouFriend())
   }
 
@@ -35,7 +33,7 @@ trait Joiner extends Controller {
     Ok(views.html.joiner.thankyou.friend())
   }
 
-  def thankyouPaid(tier: String) = PaidMemberAction.async { implicit request =>
+  def thankyouPaid(tier: Tier) = PaidMemberAction.async { implicit request =>
     StripeService.Customer.read(request.stripeCustomerId).map { customer =>
       val response = for {
         paymentDetails <- customer.paymentDetails
