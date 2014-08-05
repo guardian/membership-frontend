d9fbf277f8289d61e457f67e93a8da34a4d30916
Chris Finch <chris.finch@guardian.co.uk>
2014-08-05 10:26:49 +0100

    works for friend page too

--- frontend/app/controllers/Joiner.scala
+++ frontend/app/controllers/Joiner.scala
@@ -1,11 +1,11 @@
 package controllers
 
 import actions.{AuthenticatedAction, PaidMemberAction}
-import com.gu.membership.salesforce.Tier._
+import com.gu.membership.salesforce.Tier
 import configuration.Config
 import controllers.Joining._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.mvc.Controller
+import play.api.mvc.{Request, Controller}
 import services.{MemberService, StripeService, EventbriteService, MemberRepository}
 import model.Eventbrite._
 
@@ -17,20 +17,31 @@ trait Joiner extends Controller {
   val memberService: MemberService
   val eventService: EventbriteService
 
+  def getEbEventFromSession(request: Request[_]): Future[Option[EBEvent]] = {
+    val eventIdOpt = services.PreMembershipJoiningEventFromSessionExtractor.eventIdFrom(request)
+    Future.sequence(eventIdOpt.map(eventService.getEvent).toSeq).map(_.headOption)
+  }
+
+  def getEbIframeUrl(eventOpt: Option[EBEvent], discountOpt: Option[EBDiscount]): Option[String] = {
+    for (event <- eventOpt) yield {
+      Config.eventbriteApiIframeUrl ? ("eid" -> event.id) & ("discount" -> discountOpt.map(_.code))
+    }
+  }
+
   def tierList = CachedAction { implicit request =>
     Ok(views.html.joiner.tierList())
   }
 
-  def enterDetails(tier: Tier) = AuthenticatedAction { implicit request =>
+  def enterDetails(tier: Tier.Tier) = AuthenticatedAction { implicit request =>
     tier match {
-      case Friend => Ok(views.html.joiner.detail.addressForm())
+      case Tier.Friend => Ok(views.html.joiner.detail.addressForm())
       case paidTier => Ok(views.html.joiner.payment.paymentForm(paidTier))
     }
   }
 
   def joinFriend() = AuthenticatedAction.async { implicit request =>
     for {
-      member <- MemberRepository.upsert(request.user, "", Friend)
+      member <- MemberRepository.upsert(request.user, "", Tier.Friend)
     } yield Redirect(routes.Joiner.thankyouFriend())
   }
 
@@ -38,29 +49,23 @@ trait Joiner extends Controller {
     Ok(views.html.joiner.tier.patron())
   }
 
-  def thankyouFriend() = AuthenticatedAction { implicit request =>
-    Ok(views.html.joiner.thankyou.friend())
+  def thankyouFriend() = AuthenticatedAction.async { implicit request =>
+
+    for (event <- getEbEventFromSession(request)) yield {
+      Ok(views.html.joiner.thankyou.friend(getEbIframeUrl(event, None)))
+    }
   }
 
-  def thankyouPaid(tier: Tier) = PaidMemberAction.async { implicit request =>
-
-    val eventIdOpt = services.PreMembershipJoiningEventFromSessionExtractor.eventIdFrom(request)
-    val eventOpt = eventIdOpt.map(eventService.getEvent)
+  def thankyouPaid(tier: Tier.Tier) = PaidMemberAction.async { implicit request =>
 
     def getDiscount(eventOpt: Option[EBEvent]): Future[Option[EBDiscount]] = {
       val discountOpt = eventOpt.map(memberService.createEventDiscount(request.user.id, _))
       Future.sequence(discountOpt.toSeq).map(_.headOption.flatten)
     }
 
-    def getEbIframeUrl(eventOpt: Option[EBEvent], discountOpt: Option[EBDiscount]): Option[String] = {
-      for (event <- eventOpt) yield {
-        Config.eventbriteApiIframeUrl ? ("eid" -> event.id) & ("discount" -> discountOpt.map(_.code))
-      }
-    }
-
     for {
       customer <- StripeService.Customer.read(request.stripeCustomerId)
-      event <- Future.sequence(eventOpt.toSeq)
+      event <- getEbEventFromSession(request)
       discountOpt <- getDiscount(event.headOption)
     } yield {
       val response = for {
