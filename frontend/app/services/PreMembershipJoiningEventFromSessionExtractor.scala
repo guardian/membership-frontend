72e3d74958915c20b10782a13223a8c06cf4b18f
Will Franklin <will.franklin@guardian.co.uk>
2014-08-05 17:46:43 +0100

    Added some tests

--- frontend/app/services/PreMembershipJoiningEventFromSessionExtractor.scala
+++ frontend/app/services/PreMembershipJoiningEventFromSessionExtractor.scala
@@ -2,13 +2,22 @@ package services
 
 import play.api.mvc.RequestHeader
 
+object EventIdExtractor {
+  def apply(url: String): Option[String] = {
+    val parts = url.stripPrefix("/").split("/")
+    for {
+      page <- parts.headOption
+      if page == "event"
+      eventId <- parts.tail.headOption
+    } yield eventId
+  }
+}
 
 object PreMembershipJoiningEventFromSessionExtractor {
   def eventIdFrom(request: RequestHeader): Option[String] = {
     for {
       url <- request.session.get("preJoinReturnUrl")
-      parts = url.stripPrefix("/").split('/')
-      if parts(0) == "event"
-    }  yield parts.takeRight(1).head
+      eventId <- EventIdExtractor(url)
+    }  yield eventId
   }
 }
