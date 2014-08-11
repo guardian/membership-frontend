af33fa33c7c865da7e945aee4be7e526369089e4
Will Franklin <will.franklin@guardian.co.uk>
2014-08-11 17:30:44 +0100

    Use Zuora data in /user/me/details

--- frontend/app/controllers/User.scala
+++ frontend/app/controllers/User.scala
@@ -12,10 +12,13 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import com.gu.membership.salesforce.{FreeMember, PaidMember}
 
 import actions.{AjaxMemberAction, MemberRequest}
-import services.StripeService
+import services.{StripeService, SubscriptionService}
 import model.Stripe
 
 trait User extends Controller {
+  val standardFormat = ISODateTimeFormat.dateTime.withZoneUTC
+  implicit val writesInstant = Writes[Instant] { instant => JsString(instant.toString(standardFormat)) }
+
   def me = AjaxMemberAction { implicit request =>
     Cors(Ok(basicDetails(request)))
   }
@@ -23,9 +26,22 @@ trait User extends Controller {
   def meDetails = AjaxMemberAction.async { implicit request =>
     val futurePaymentDetails = request.member match {
       case paidMember: PaidMember =>
-        StripeService.Customer.read(paidMember.stripeCustomerId).map {
-          _.paymentDetails.fold(Json.obj())(extractPaymentDetails)
-        }
+        for {
+          customer <- StripeService.Customer.read(paidMember.stripeCustomerId)
+          invoice <- SubscriptionService.getInvoiceSummary(paidMember.salesforceAccountId)
+        } yield Json.obj(
+          "subscription" -> Json.obj(
+            "start" -> invoice.startDate,
+            "end" -> invoice.endDate,
+            "cancelledAt" -> false, // TODO
+            "plan" -> Json.obj(
+              "name" -> invoice.planName,
+              "amount" -> invoice.planAmount * 100,
+              "interval" -> (if (invoice.annual) "year" else "month")
+            ),
+            "card" -> Json.obj("last4" -> customer.card.last4, "type" -> customer.card.`type`)
+          )
+        )
 
       case member: FreeMember =>
         val paymentDetails = Json.obj(
@@ -43,35 +59,11 @@ trait User extends Controller {
   def basicDetails(request: MemberRequest[_]) = {
     val member = request.member
 
-    val standardFormat = ISODateTimeFormat.dateTime.withZoneUTC
-    implicit val writesDateTime = Writes[DateTime] { dt => JsString(dt.toString(standardFormat)) }
-
     Json.obj(
       "userId" -> member.identityId,
       "tier" -> member.tier.toString,
       "joinDate" -> member.joinDate,
       "optIn" -> member.optedIn
-    )
-  }
-
-  def extractPaymentDetails(paymentDetails: Stripe.PaymentDetails) = {
-    val standardFormat = ISODateTimeFormat.dateTime.withZoneUTC
-    implicit val writesInstant = Writes[Instant] { instant => JsString(instant.toString(standardFormat)) }
-
-    val Stripe.PaymentDetails(card, subscription) = paymentDetails
-
-    Json.obj(
-      "subscription" -> Json.obj(
-        "start" -> subscription.start,
-        "end" -> subscription.current_period_end,
-        "cancelledAt" -> subscription.canceled_at,
-        "plan" -> Json.obj(
-          "name" -> subscription.plan.name,
-          "amount" -> subscription.plan.amount,
-          "interval" -> subscription.plan.interval
-        ),
-        "card" -> Json.obj("last4" -> card.last4, "type" -> card.`type`)
-      )
     )
   }
 }
