86378eea2bcc6552187776e473d0248f27db7404
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-08-22 17:53:02 +0100

    Send gnm and third party preferences to salesforces on membership creation

--- frontend/app/forms/MemberForm.scala
+++ frontend/app/forms/MemberForm.scala
@@ -15,15 +15,18 @@ object MemberForm {
 
   case class PaymentForm(annual: Boolean, token: String)
 
+  case class MarketingChoicesForm(gnm: Boolean, thirdParty: Boolean)
+
   trait JoinForm {
     val name: NameForm
     val deliveryAddress: AddressForm
+    val marketingChoices: MarketingChoicesForm
   }
 
-  case class FriendJoinForm(name: NameForm, deliveryAddress: AddressForm) extends JoinForm
+  case class FriendJoinForm(name: NameForm, deliveryAddress: AddressForm, marketingChoices: MarketingChoicesForm) extends JoinForm
 
   case class PaidMemberJoinForm(tier: Tier, name: NameForm, payment: PaymentForm, deliveryAddress: AddressForm,
-                                billingAddress: Option[AddressForm]) extends JoinForm
+                                billingAddress: Option[AddressForm], marketingChoices: MarketingChoicesForm) extends JoinForm
 
   case class PaidMemberChangeForm(payment: PaymentForm, deliveryAddress: AddressForm,
                                   billingAddress: Option[AddressForm])
@@ -51,6 +54,11 @@ object MemberForm {
     "last" -> nonEmptyText
   )(NameForm.apply)(NameForm.unapply)
 
+  val marketingChoicesMapping: Mapping[MarketingChoicesForm] = mapping(
+    "gnnMarketing" -> boolean,
+    "thirdParty" -> boolean
+  )(MarketingChoicesForm.apply)(MarketingChoicesForm.unapply)
+
   val paymentMapping: Mapping[PaymentForm] = mapping(
     "annual" -> boolean,
     "token" -> nonEmptyText
@@ -59,7 +67,8 @@ object MemberForm {
   val friendJoinForm: Form[FriendJoinForm] = Form(
     mapping(
       "name" -> nameMapping,
-      "deliveryAddress" -> friendAddressMapping
+      "deliveryAddress" -> friendAddressMapping,
+      "marketingChoices" -> marketingChoicesMapping
     )(FriendJoinForm.apply)(FriendJoinForm.unapply)
   )
 
@@ -69,7 +78,8 @@ object MemberForm {
       "name" -> nameMapping,
       "payment" -> paymentMapping,
       "deliveryAddress" -> paidAddressMapping,
-      "billingAddress" -> optional(paidAddressMapping)
+      "billingAddress" -> optional(paidAddressMapping),
+      "marketingChoices" -> marketingChoicesMapping
     )(PaidMemberJoinForm.apply)(PaidMemberJoinForm.unapply)
   )
 
