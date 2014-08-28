c0d92337946d39aa141532fc173394019be01afa
Ben Chidgey <ben@feedmypixel.com>
2014-08-28 11:57:50 +0100

    split out the pricing for the benefits

--- frontend/app/model/Benefits.scala
+++ frontend/app/model/Benefits.scala
@@ -4,14 +4,15 @@ import com.gu.membership.salesforce.Tier
 
 object Benefits {
 
-  case class Benefits(leadin: String, list: Seq[(String, String)], priceText: String, cta: String)
+  case class Benefits(leadin: String, list: Seq[(String, String)], priceMonth: Option[Int],
+                      priceYear: Option[Int], saving: Option[Int], cta: String)
 
   val friendBenefits = Benefits("As a friend:", Seq(
     "Book tickets" -> "Book tickets to Guardian Live events",
     "Digital Digest email" -> "Receive weekly updates on the upcoming programme",
     "Exclusive offers" -> "Discounts and special offers from Guardian Discover",
     "Video highlights" -> "Watch highlights of selected Guardian Live events"
-  ), "£(Free)", "Become a Friend")
+  ), None, None, None, "Become a Friend")
 
   val partnerBenefits = Benefits("All the benefits of a Friend plus:", Seq(
     "20% Discount" -> "20% discount on Guardian Live tickets",
@@ -19,13 +20,13 @@ object Benefits {
     "Live stream events" -> "Watch live streams of Flagship events",
     "Membership card" -> "Unlock additional experiences",
     "Early Booking" -> "Additional priority ticket booking (one week before friends)"
-  ), "£15 per month | £135 per year (save £45)", "Become a Partner")
+  ), Some(15), Some(135), Some(45), "Become a Partner");
 
   val patronBenefits = Benefits("All benefits of a partner plus:", Seq(
     "Early Booking" -> "Additional priority ticket booking (one week before Partners)",
     "Unique experiences" -> "Get closer to the Guardian’s journalism and better understand the impact of our campaigns",
     "Complimentary items" -> "Thank you for your support"
-  ), "£15 per month | £135 per year (save £45)", "Become a Partner")
+  ), Some(60), Some(540), Some(180), "Become a Partner")
 
   val details = Map(
     Tier.Friend -> friendBenefits,
