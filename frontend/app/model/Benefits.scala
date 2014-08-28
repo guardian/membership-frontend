b877e2f4bedf8d54ffc1656a56dfdfd3aba165db
Ben Chidgey <ben@feedmypixel.com>
2014-08-28 16:08:00 +0100

    rework of benifits model to include a pricing case class

--- frontend/app/model/Benefits.scala
+++ frontend/app/model/Benefits.scala
@@ -4,15 +4,18 @@ import com.gu.membership.salesforce.Tier
 
 object Benefits {
 
-  case class Benefits(leadin: String, list: Seq[(String, String)], priceMonth: Option[Int],
-                      priceYear: Option[Int], saving: Option[Int], cta: String)
+  case class Benefits(leadin: String, list: Seq[(String, String)], pricing: Option[Pricing], cta: String)
+
+  case class Pricing(yearly: Int, monthly: Int) {
+    lazy val yearlySaving = (12 * monthly) - yearly
+  }
 
   val friendBenefits = Benefits("As a friend:", Seq(
     "Book tickets" -> "Book tickets to Guardian Live events",
     "Digital Digest email" -> "Receive weekly updates on the upcoming programme",
     "Exclusive offers" -> "Discounts and special offers from Guardian Discover",
     "Video highlights" -> "Watch highlights of selected Guardian Live events"
-  ), None, None, None, "Become a Friend")
+  ), None, "Become a Friend")
 
   val partnerBenefits = Benefits("All the benefits of a Friend plus:", Seq(
     "20% Discount" -> "20% discount on Guardian Live tickets",
@@ -20,13 +23,13 @@ object Benefits {
     "Live stream events" -> "Watch live streams of Flagship events",
     "Membership card" -> "Unlock additional experiences",
     "Early Booking" -> "Additional priority ticket booking (one week before friends)"
-  ), Some(15), Some(135), Some(45), "Become a Partner");
+  ), Some(Pricing(135, 15)), "Become a Partner");
 
   val patronBenefits = Benefits("All benefits of a partner plus:", Seq(
     "Early Booking" -> "Additional priority ticket booking (one week before Partners)",
     "Unique experiences" -> "Get closer to the Guardian’s journalism and better understand the impact of our campaigns",
     "Complimentary items" -> "Thank you for your support"
-  ), Some(60), Some(540), Some(180), "Become a Partner")
+  ), Some(Pricing(540, 60)), "Become a Partner")
 
   val details = Map(
     Tier.Friend -> friendBenefits,
