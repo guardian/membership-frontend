69293a9a49f5d4e355033f8027774db95eec1e3f
James Oram <james.oram@tromba.io>
2014-09-03 10:33:10 +0100

    wip

--- functional-tests/src/test/scala/MembershipChangeTierTests.scala
+++ functional-tests/src/test/scala/MembershipChangeTierTests.scala
@@ -7,55 +7,68 @@ class MembershipChangeTierTests extends BaseMembershipTest {
 
   info("Tests for changing tier")
 
-  feature("A user can downgrade") {
-
-    scenarioWeb("30. A Partner can downgrade to a Friend") {
-      implicit driver =>
-      given {
-        MembershipSteps().IAmLoggedInAsAPartner
-      }
-      .when {
-        _.IChooseToBecomeAFriend
-      }
-      .then {
-        _.IAmAFriend
-      }
-    }
-
-    scenarioWeb("31. A Patron can downgrade to a Friend") {
-      implicit driver =>
-        given {
-          MembershipSteps().IAmLoggedInAsAPatron
-        }
-        .when {
-          _.IChooseToBecomeAFriend
-        }
-        .then {
-          _.IAmAFriend
-        }
-     }
-   }
-
-  feature("A user can upgrade") {
-
-    scenarioWeb("34. A friend can upgrade to a partner") {
-      implicit driver =>
-        given {
-          MembershipSteps().IAmLoggedInAsAFriend
-        }
-        .when {
-          _.IChooseToBecomeAPartner
-        }
-        .then {
-          _.IAmAPartner
-        }
-    }
-
-    // TODO this functionality is not implemented yet
-//    scenarioWeb("35. A Partner can upgrade to a Patron") {
+//  feature("A user can downgrade") {
+//
+//    scenarioWeb("30. A Partner can downgrade to a Friend") {
+//      implicit driver =>
+//      given {
+//        MembershipSteps().IAmLoggedInAsAPartner
+//      }
+//      .when {
+//        _.IChooseToBecomeAFriend
+//      }
+//      .then {
+//        _.IAmAFriend
+//      }
+//    }
+//
+//    scenarioWeb("31. A Patron can downgrade to a Friend") {
 //      implicit driver =>
 //        given {
-//          MembershipSteps().IAmLoggedInAsAPartner
+//          MembershipSteps().IAmLoggedInAsAPatron
+//        }
+//        .when {
+//          _.IChooseToBecomeAFriend
+//        }
+//        .then {
+//          _.IAmAFriend
+//        }
+//     }
+//   }
+//
+//  feature("A user can upgrade") {
+//
+//    scenarioWeb("34. A friend can upgrade to a partner") {
+//      implicit driver =>
+//        given {
+//          MembershipSteps().IAmLoggedInAsAFriend
+//        }
+//        .when {
+//          _.IChooseToBecomeAPartner
+//        }
+//        .then {
+//          _.IAmAPartner
+//        }
+//    }
+//
+//    // TODO this functionality is not implemented yet
+////    scenarioWeb("35. A Partner can upgrade to a Patron") {
+////      implicit driver =>
+////        given {
+////          MembershipSteps().IAmLoggedInAsAPartner
+////        }
+////        .when {
+////          _.IChooseToBecomeAPatron
+////        }
+////        .then {
+////          _.IAmAPatron
+////        }
+////    }
+//
+//    scenarioWeb("36. A Friend can upgrade to a Patron") {
+//      implicit driver =>
+//        given {
+//          MembershipSteps().IAmLoggedInAsAFriend
 //        }
 //        .when {
 //          _.IChooseToBecomeAPatron
@@ -64,20 +77,7 @@ class MembershipChangeTierTests extends BaseMembershipTest {
 //          _.IAmAPatron
 //        }
 //    }
-
-    scenarioWeb("36. A Friend can upgrade to a Patron") {
-      implicit driver =>
-        given {
-          MembershipSteps().IAmLoggedInAsAFriend
-        }
-        .when {
-          _.IChooseToBecomeAPatron
-        }
-        .then {
-          _.IAmAPatron
-        }
-    }
-  }
+//  }
 
   // cancel membership
   scenarioWeb("37. A Friend can cancel membership") {
