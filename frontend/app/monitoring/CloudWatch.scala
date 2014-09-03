9dbc4ed83082990b2bbd742299050b20358fe307
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-09-03 14:47:03 +0100

    Moved IdentityApiMetrics to it's own file

--- frontend/app/monitoring/CloudWatch.scala
+++ frontend/app/monitoring/CloudWatch.scala
@@ -55,28 +55,3 @@ trait CloudWatch {
   def put(namespace: String, metrics: Map[String, Double]): Unit =
     put(namespace, metrics, Seq(stageDimension))
 }
-
-object IdentityApiMetrics extends CloudWatch {
-
-  def putPasswordsExistsResponse(status: Int) {
-    putStatus("identity-get-user-password-exists-response", status)
-  }
-
-  def putUserDetailsResponse(status: Int) {
-    putStatus("identity-get-user-details-response", status)
-  }
-
-  def putUpdateUserDetailsResponse(status : Int) {
-    putStatus("identity-update-user-details-response", status)
-  }
-
-  def putPasswordUpdateResponse(status: Int) {
-    putStatus("identity-password-update-response", status)
-  }
-
-  private def putStatus(name: String, status: Int) {
-    val statusClass = status / 100
-    val metrics = Map(s"$name-${statusClass}XX" -> 1.00)
-    put("Identity API", metrics)
-  }
-}
