abba7d77ac012e08fc6f3045f71847e673a158ec
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-09-05 17:15:40 +0100

    Update to mem-common library

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val identityModel = "com.gu.identity" %% "identity-model" % identity
   val awsSdk="com.amazonaws" % "aws-java-sdk" % "1.8.9.1"
   val scalaUri = "com.netaporter" %% "scala-uri" % "0.4.1"
-  val membershipCommon = "com.gu" %% "membership-common" % "0.17"
+  val membershipCommon = "com.gu" %% "membership-common" % "0.17.1"
   val playWS = PlayImport.ws
   val playFilters = PlayImport.filters
 
