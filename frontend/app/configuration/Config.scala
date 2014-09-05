799681bc5cde1c865dd1b2b51239285574abd065
Will Franklin <will.franklin@guardian.co.uk>
2014-09-05 11:03:37 +0100

    Move the Stripe public key to a config file

--- frontend/app/configuration/Config.scala
+++ frontend/app/configuration/Config.scala
@@ -42,7 +42,8 @@ object Config {
   val eventOrderingJsonUrl = config.getString("event.ordering.json")
 
   val stripeApiURL = config.getString("stripe.api.url")
-  val stripeApiSecret = config.getString("stripe.api.secret")
+  val stripeApiKeySecret = config.getString("stripe.api.key.secret")
+  val stripeApiKeyPublic = config.getString("stripe.api.key.public")
 
   val salesforceConsumerKey = config.getString("salesforce.consumer.key")
   val salesforceConsumerSecret = config.getString("salesforce.consumer.secret")
