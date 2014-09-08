65768b2c62006198a8820408f702c8892761613b
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-09-08 15:31:47 +0100

    Inline one time use methods.

--- frontend/app/monitoring/ZuoraMetrics.scala
+++ frontend/app/monitoring/ZuoraMetrics.scala
@@ -3,7 +3,7 @@ package monitoring
 import com.gu.monitoring.{StatusMetrics, AuthenticationMetrics}
 
 object ZuoraMetrics extends Metrics with StatusMetrics with AuthenticationMetrics {
-  override val service = "Zuroa"
+  val service = "Zuora"
 
   def recordError {
     put("error-count", 1)
