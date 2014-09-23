a5a1e6ab7f8278719b879a8deae0f19a049d4fc9
Will Franklin <will.franklin@guardian.co.uk>
2014-09-23 17:37:13 +0100

    Use ISO 3166 alpha-2 code as value in country fields

--- frontend/test/forms/MemberFormTest.scala
+++ frontend/test/forms/MemberFormTest.scala
@@ -24,57 +24,57 @@ class MemberFormTest extends Specification {
 
   "AddressForm" should {
     "always require a country and postcode" in {
-      friendAddressForm.bind(address("a", "b", "c", "d", "N1 9GU", "United Kingdom")).hasErrors must beFalse
+      friendAddressForm.bind(address("a", "b", "c", "d", "N1 9GU", "GB")).hasErrors must beFalse
       friendAddressForm.bind(address("a", "b", "c", "d", "", "")).hasErrors must beTrue
 
-      paidAddressForm.bind(address("a", "b", "c", "d", "N1 9GU", "United Kingdom")).hasErrors must beFalse
+      paidAddressForm.bind(address("a", "b", "c", "d", "N1 9GU", "GB")).hasErrors must beFalse
       paidAddressForm.bind(address("a", "b", "c", "d", "", "")).hasErrors must beTrue
     }
 
     "also require line one and town for paid members" in {
-      paidAddressForm.bind(address("a", "b", "c", "d", "N1 9GU", "United Kingdom")).hasErrors must beFalse
-      paidAddressForm.bind(address("", "b", "", "d", "N1 9GU", "United Kingdom")).hasErrors must beTrue
+      paidAddressForm.bind(address("a", "b", "c", "d", "N1 9GU", "GB")).hasErrors must beFalse
+      paidAddressForm.bind(address("", "b", "", "d", "N1 9GU", "GB")).hasErrors must beTrue
     }
 
     "allow empty state for friends for any country except US/Canada" in {
-      val form = friendAddressForm.bind(address("Kings Place", "90 York Way", "London", "", "N1 9GU", "United Kingdom"))
+      val form = friendAddressForm.bind(address("Kings Place", "90 York Way", "London", "", "N1 9GU", "GB"))
       form.hasErrors must beFalse
     }
 
     "allow empty state for paid members for any country except US/Canada" in {
-      val form = paidAddressForm.bind(address("Kings Place", "90 York Way", "London", "", "N1 9GU", "United Kingdom"))
+      val form = paidAddressForm.bind(address("Kings Place", "90 York Way", "London", "", "N1 9GU", "GB"))
       form.hasErrors must beFalse
     }
 
     "require a valid state for friends when country is US" in {
-      val badState = friendAddressForm.bind(address("The Guardian", "536 Broadway", "New York", "Greater York", "10012", "United States"))
+      val badState = friendAddressForm.bind(address("The Guardian", "536 Broadway", "New York", "Greater York", "10012", "US"))
       badState.hasErrors must beTrue
 
-      val goodState = friendAddressForm.bind(address("The Guardian", "536 Broadway", "New York", "New York", "10012", "United States"))
+      val goodState = friendAddressForm.bind(address("The Guardian", "536 Broadway", "New York", "New York", "10012", "US"))
       goodState.hasErrors must beFalse
     }
 
     "require a valid province for friends when country is Canada" in {
-      val badProvince = friendAddressForm.bind(address("80 Elgin St", "", "Ottawa", "Oldfoundland", "K1P 5K7", "Canada"))
+      val badProvince = friendAddressForm.bind(address("80 Elgin St", "", "Ottawa", "Oldfoundland", "K1P 5K7", "CA"))
       badProvince.hasErrors must beTrue
 
-      val goodProvince = friendAddressForm.bind(address("80 Elgin St", "", "Ottawa", "Ontario", "K1P 5K7", "Canada"))
+      val goodProvince = friendAddressForm.bind(address("80 Elgin St", "", "Ottawa", "Ontario", "K1P 5K7", "CA"))
       goodProvince.hasErrors must beFalse
     }
 
     "require a valid state for paid members when country is US" in {
-      val badState = paidAddressForm.bind(address("Kings Place", "90 York Way", "London", "Greater London", "N1 9GU", "United States"))
+      val badState = paidAddressForm.bind(address("Kings Place", "90 York Way", "London", "Greater London", "N1 9GU", "US"))
       badState.hasErrors must beTrue
 
-      val goodState = paidAddressForm.bind(address("The Guardian", "536 Broadway", "New York", "New York", "10012", "United States"))
+      val goodState = paidAddressForm.bind(address("The Guardian", "536 Broadway", "New York", "New York", "10012", "US"))
       goodState.hasErrors must beFalse
     }
 
     "require a valid province for paid members when country is Canada" in {
-      val badProvince = paidAddressForm.bind(address("80 Elgin St", "", "Ottawa", "Oldfoundland", "K1P 5K7", "Canada"))
+      val badProvince = paidAddressForm.bind(address("80 Elgin St", "", "Ottawa", "Oldfoundland", "K1P 5K7", "CA"))
       badProvince.hasErrors must beTrue
 
-      val goodProvince = paidAddressForm.bind(address("80 Elgin St", "", "Ottawa", "Ontario", "K1P 5K7", "Canada"))
+      val goodProvince = paidAddressForm.bind(address("80 Elgin St", "", "Ottawa", "Ontario", "K1P 5K7", "CA"))
       goodProvince.hasErrors must beFalse
     }
   }
