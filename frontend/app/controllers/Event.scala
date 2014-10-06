ee4cfdd747e65073e6c85df3f4a1f8b586cd5b6c
Ben Chidgey <ben@feedmypixel.com>
2014-10-06 15:43:22 +0100

    added thankyou action method

--- frontend/app/controllers/Event.scala
+++ frontend/app/controllers/Event.scala
@@ -74,11 +74,11 @@ trait Event extends Controller {
     }.getOrElse(Future.successful(NotFound))
   }
 
-  def thankyou(id: String) = NoCacheAction.async { implicit request =>
+  def thankyou(id: String) = MemberAction.async { implicit request =>
     eventService.getEvent(id).map {
       event =>
         Timing.record(EventbriteMetrics, "user-returned-from-eventbrite") {
-          Future.successful(Ok(views.html.event.thankyou(event)))
+          Future.successful(Ok(views.html.event.thankyou(request.member.firstName.getOrElse(""), event)))
         }
     }.getOrElse(Future.successful(NotFound))
   }
