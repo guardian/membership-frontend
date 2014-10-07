b4140f57dd9180751a50a1d06a1fd02fc77a30eb
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-10-07 17:11:47 +0100

    Send 404 for assets that do not exist. This should reduce out 500 error count: https://gupage.pagerduty.com/incidents/PBRQ6RC

--- frontend/app/controllers/CachedAssets.scala
+++ frontend/app/controllers/CachedAssets.scala
@@ -1,14 +1,20 @@
 package controllers
-import scala.concurrent.ExecutionContext.Implicits.global
 
+import play.Logger
 import play.api.mvc.{Action, Controller}
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object CachedAssets extends Controller {
 
   def at(path: String, file: String, aggressiveCaching: Boolean = false) = Action.async { request =>
-    controllers.Assets.at(path, file, aggressiveCaching).apply(request).map { result =>
+    controllers.Assets.at(path, file, aggressiveCaching).apply(request).recover {
+      case e: RuntimeException => {
+        Logger.warn(s"Asset run time exception for path $path $file. Does this file exist?", e)
+        Cached(NotFound)
+      }
+    }.map { result =>
       if (result.header.headers.contains(CACHE_CONTROL)) result else Cached(result)
     }
-
   }
 }
