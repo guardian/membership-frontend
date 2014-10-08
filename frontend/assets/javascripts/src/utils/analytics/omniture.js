ee16ac139ffcc96849022bf7c16984157cf728e8
Ben Chidgey <ben@feedmypixel.com>
2014-10-08 17:52:44 +0100

    remove the omniture debug in dev

--- frontend/assets/javascripts/src/utils/analytics/omniture.js
+++ frontend/assets/javascripts/src/utils/analytics/omniture.js
@@ -16,10 +16,6 @@ define([
         var referrerArray = document.referrer.split('/');
         var referrerDomain;
 
-        if (guardian && guardian.isDev) {
-            s.debugTracking = 'YES';
-        }
-
         if (referrerArray.length > 2) {
             referrerDomain = referrerArray[2];
             if (referrerDomain !== document.location.host) {
