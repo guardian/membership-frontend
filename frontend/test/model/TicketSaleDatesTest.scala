ac7efefe5d689b5d1e908c22ba46342174f5f20f
Roberto Tyley <roberto.tyley@theguardian.com>
2014-10-24 15:25:19 +0100

    Better advance ticket sale dates (day+ distinctions)
    After conversation with the Guardian Live Events team about how they've had
    set the ticket-sale-start just to get round short lead times, we've agreed
    with Dale to adjust the rules to cope better with those situations.
    
    Regarding the test event data from PROD, you can grab a data file using the
    same query as the site:
    
    $ curl "https://www.eventbriteapi.com/v3/users/me/owned_events/?token=XXXX&page=1"
    
    (Note that this gives full pagination info, unlike the /v3/events call
    I was using for test data before - panic over)
    
    
    These are the original requirements from Dale's email - note that only
    the rules that support 1-day-or-greater distinction between tiers are
    currently implemented, because we need to tweak the UI to display *times*
    as well as dates when the distinction is less.
    
    ---------- Forwarded message ----------
    From: Dale Kirsop <dale.kirsop@theguardian.com>
    Date: 22 October 2014 18:51
    Subject: Re: Membership Ticket Sale Dates (causing errors) - and advance purchase for Patrons & Partners
    To: Roberto Tyley <roberto.tyley@theguardian.com>
    Cc: Natalie Scriven <natalie.scriven@theguardian.com>, Michael Harris <michael.harris@guardian.co.uk>, Madeleine Williams <madeleine.williams@theguardian.com>, Membership Dev <membership.dev@guardian.co.uk>
    
    ...
    
    less that 4 hours lead time: everybody gets tickets immediately from ticket-sale-start
    4+ hours - 48 hours lead time : Patron gets 30 minute lead, Partner gets 20 minute lead = 30 mins before general release
    48 hours - 7 days lead time : Patron gets 4 hour lead, Partner gets 2 hour lead = 4 hours before general release
    7 days - 2 weeks lead time : Patron gets 2 day lead, Partner gets 1 day lead = 2 days before general release
    2 weeks - 6 weeks lead time: Patron gets 5 day lead, Partner gets 3 day lead = 5 days before general release
    6+ weeks lead time : Patron gets 2 week lead, Partner gets 1 week lead = 2 week lead

--- frontend/test/model/TicketSaleDatesTest.scala
+++ frontend/test/model/TicketSaleDatesTest.scala
@@ -1,10 +1,9 @@
 package model
 
 import com.github.nscala_time.time.Imports.{richDateTime, _}
-import com.gu.membership.salesforce.Tier
-import model.Eventbrite.{EBEvent, EBTickets}
+import com.gu.membership.salesforce.Tier.{Friend, Partner, Patron}
+import model.Eventbrite.{EBEvent, EBResponse, EBTickets}
 import model.EventbriteDeserializer._
-import org.joda.time.Instant
 import org.specs2.mutable.Specification
 import org.specs2.time.NoTimeConversions
 import utils.Resource
@@ -12,40 +11,51 @@ import utils.Resource
 class TicketSaleDatesTest extends Specification with NoTimeConversions {
 
   val eventDate = new DateTime(2014, 6, 1, 0, 0)
-  val testEvent = EventbriteTestObjects.eventWithName().copy(created = (eventDate - 1.month).toInstant)
+  val testEvent = EventbriteTestObjects.eventWithName().copy(created = (eventDate - 2.months).toInstant)
 
   "Ticket Sales Dates" should {
 
-    "all be the sale-start date if sale-start is less than 2 weeks from Event start" in {
-      val saleStart = (testEvent.start - 13.days).toInstant
+    "give general availability immediately if there's very little time until the event " in {
+      val saleStart = (testEvent.start - 2.hours).toInstant
 
-      val datesByTier = TicketSaleDates.datesFor(testEvent, EBTickets(sales_start = Some(saleStart)))
+      val datesByTier = TicketSaleDates.datesFor(testEvent, EBTickets(sales_start = Some(saleStart))).datesByTier
 
-      datesByTier.values.toSet mustEqual Set(saleStart)
-      datesByTier.keys.toSet mustEqual Set(Tier.Partner, Tier.Patron, Tier.Friend)
+      datesByTier(Friend) must be(saleStart)
+      datesByTier(Partner) must be(saleStart)
+      datesByTier(Patron) must be(saleStart)
     }
 
-    "give patrons and partners preferential times if sale-start is 2 or more weeks from Event start" in {
-      val saleStart = testEvent.start - 2.weeks
+    "give patrons and partners advance tickets if there's enough lead time" in {
+      val saleStart = (testEvent.start - 7.weeks).toInstant
 
-      val datesByTier = TicketSaleDates.datesFor(testEvent, EBTickets(sales_start = Some(saleStart.toInstant)))
+      val datesByTier = TicketSaleDates.datesFor(testEvent, EBTickets(sales_start = Some(saleStart))).datesByTier
 
-      datesByTier must havePairs(
-        Tier.Friend -> (saleStart + 2.week).toInstant,
-        Tier.Partner -> (saleStart + 1.week).toInstant,
-        Tier.Patron -> (saleStart).toInstant
-      )
+      datesByTier(Patron) must be_==(saleStart)
+      datesByTier(Patron) must be_<=(datesByTier(Partner))
+      datesByTier(Partner) must be_<=(datesByTier(Friend))
+      datesByTier(Friend) must be_<=(testEvent.start)
+      (datesByTier(Patron) to datesByTier(Friend)).duration must be_>=(7.days.standardDuration)
     }
 
     "not die if sales_start is missing" in {
       val event = Resource.getJson("model/eventbrite/event.long-lead-time.missing-sales-start.json").as[EBEvent]
       val created = event.created.toDateTime
 
-      TicketSaleDates.datesFor(event, event.ticket_classes.head) must havePairs(
-        Tier.Friend -> (created + 2.week).toInstant,
-        Tier.Partner -> (created + 1.week).toInstant,
-        Tier.Patron -> created.toInstant
-      )
+      TicketSaleDates.datesFor(event, event.ticket_classes.head).memberAdvanceTicketSales must beSome
+    }
+
+    "be sensible given a recent snapshot of PROD Eventbrite events" in {
+      val eventsWithTickets =
+        Resource.getJson("model/eventbrite/owned-events.2014-10-24.PROD.page-1.json").as[EBResponse[EBEvent]].data.filter(_.ticket_classes.nonEmpty)
+
+      forall(eventsWithTickets) { event: EBEvent =>
+        val ticketSaleDates = TicketSaleDates.datesFor(event, event.ticket_classes.head)
+        val datesByTier = ticketSaleDates.datesByTier
+
+        datesByTier(Patron) must be_<=(datesByTier(Partner))
+        datesByTier(Partner) must be_<=(datesByTier(Friend))
+        datesByTier(Friend) must be_<=(event.start)
+      }
     }
   }
 }
