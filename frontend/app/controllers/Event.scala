888ca4527da5d5d502b1de980cbc273d62afa497
Marc Hibbins <marc.hibbins@guardian.co.uk>
2014-11-03 16:01:16 +0000

    Use event blub for event page meta description

--- frontend/app/controllers/Event.scala
+++ frontend/app/controllers/Event.scala
@@ -41,7 +41,7 @@ trait Event extends Controller {
       val pageInfo = PageInfo(
         event.name.text,
         request.path,
-        Some(event.venue.name.getOrElse("") + ", " + event.eventAddressLine + " - " + prettyDateWithTime(event.start)),
+        event.description.map(_.blurb),
         Some(eventImageFullPath(event.id))
       )
       Ok(views.html.event.page(event, pageInfo))
