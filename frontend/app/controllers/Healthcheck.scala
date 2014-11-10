c27ec00061ce1e7d06e581572c19705556b7ef58
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-11-10 15:22:40 +0000

    Add masterclass list to the healthcheck

--- frontend/app/controllers/Healthcheck.scala
+++ frontend/app/controllers/Healthcheck.scala
@@ -1,12 +1,12 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
-import services.{GuardianLiveEventService, EventbriteService}
+import services.{GuardianLiveEventService, MasterclassDataService}
 
 object Healthcheck extends Controller {
 
   def healthcheck() = Action {
-    Cached(1)(if (GuardianLiveEventService.events.nonEmpty) Ok("OK") else ServiceUnavailable("Service Unavailable"))
+    Cached(1)(if (GuardianLiveEventService.events.nonEmpty && MasterclassDataService.masterclassData.nonEmpty) Ok("OK") else ServiceUnavailable("Service Unavailable"))
   }
 
 }
