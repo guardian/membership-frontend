bd28ed9abfc9736cb3051fd35dc8b23b6c51a5eb
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-11-11 16:23:38 +0000

    Healthcheck on MasterclassEventService and not MasterclassDataService

--- frontend/app/controllers/Healthcheck.scala
+++ frontend/app/controllers/Healthcheck.scala
@@ -1,14 +1,14 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
-import services.{GuardianLiveEventService, MasterclassDataService}
+import services.{MasterclassEventService, GuardianLiveEventService, MasterclassDataService}
 import com.gu.monitoring.CloudWatchHealth
 
 object Healthcheck extends Controller {
 
   def healthcheck() = Action {
     Cached(1)(if (GuardianLiveEventService.events.nonEmpty &&
-                  MasterclassDataService.masterclassData.nonEmpty &&
+                  MasterclassEventService.events.nonEmpty &&
                   CloudWatchHealth.hasPushedMetricSuccessfully) Ok("OK")
     else ServiceUnavailable("Service Unavailable"))
   }
