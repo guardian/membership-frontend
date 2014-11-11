be4364b1a3ed1a7a5ff32e971dbcbd7f7d6a7981
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-11-11 12:05:24 +0000

    Ensure we have pushed to Cloudwatch successfully. This relies on a service already pushing a metric - we have services such as poll against Eventbrite which should track the metric straight away.

--- frontend/app/controllers/Healthcheck.scala
+++ frontend/app/controllers/Healthcheck.scala
@@ -2,11 +2,15 @@ package controllers
 
 import play.api.mvc.{Action, Controller}
 import services.{GuardianLiveEventService, MasterclassDataService}
+import com.gu.monitoring.CloudWatchHealth
 
 object Healthcheck extends Controller {
 
   def healthcheck() = Action {
-    Cached(1)(if (GuardianLiveEventService.events.nonEmpty && MasterclassDataService.masterclassData.nonEmpty) Ok("OK") else ServiceUnavailable("Service Unavailable"))
+    Cached(1)(if (GuardianLiveEventService.events.nonEmpty &&
+                  MasterclassDataService.masterclassData.nonEmpty &&
+                  CloudWatchHealth.hasPushedMetricSuccessfully) Ok("OK")
+    else ServiceUnavailable("Service Unavailable"))
   }
 
 }
