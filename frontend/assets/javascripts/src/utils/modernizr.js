39ec248ccace1ffe72f1ab858cee8b504974b6d0
Will Franklin <will.franklin@guardian.co.uk>
2014-11-19 17:44:36 +0000

    Remove SVG test from modernizr

--- frontend/assets/javascripts/src/utils/modernizr.js
+++ frontend/assets/javascripts/src/utils/modernizr.js
@@ -1,145 +1,160 @@
 /* jshint ignore:start */
 /* Modernizr 2.8.3 (Custom Build) | MIT & BSD
- * Build: http://modernizr.com/download/#-flexbox-flexboxlegacy-svg-cssclasses-testprop-testallprops-domprefixes
+ * Build: http://modernizr.com/download/#-flexbox-flexboxlegacy-cssclasses-testprop-testallprops-domprefixes
  */
 ;
 
-window.Modernizr = (function(window, document, undefined) {
+
+
+window.Modernizr = (function( window, document, undefined ) {
 
     var version = '2.8.3',
-        Modernizr = {},
-        enableClasses = true,
-        docElement = document.documentElement,
-        mod = 'modernizr',
-        modElem = document.createElement(mod),
-        mStyle = modElem.style,
-        inputElem,
-        toString = {}.toString,
-        omPrefixes = 'Webkit Moz O ms',
-        cssomPrefixes = omPrefixes.split(' '),
-        domPrefixes = omPrefixes.toLowerCase().split(' '),
-        ns = {
-            'svg': 'http://www.w3.org/2000/svg'
-        },
-        tests = {},
-        inputs = {},
-        attrs = {},
-        classes = [],
-        slice = classes.slice,
-        featureName,
-        _hasOwnProperty = ({}).hasOwnProperty,
-        hasOwnProp;
 
-    if (!is(_hasOwnProperty, 'undefined') && !is(_hasOwnProperty.call, 'undefined')) {
-        hasOwnProp = function(object, property) {
-            return _hasOwnProperty.call(object, property);
-        };
-    } else {
-        hasOwnProp = function(object, property) {
-            return ((property in object) && is(object.constructor.prototype[property], 'undefined'));
-        };
+    Modernizr = {},
+
+    enableClasses = true,
+
+    docElement = document.documentElement,
+
+    mod = 'modernizr',
+    modElem = document.createElement(mod),
+    mStyle = modElem.style,
+
+    inputElem  ,
+
+
+    toString = {}.toString,    omPrefixes = 'Webkit Moz O ms',
+
+    cssomPrefixes = omPrefixes.split(' '),
+
+    domPrefixes = omPrefixes.toLowerCase().split(' '),
+
+
+    tests = {},
+    inputs = {},
+    attrs = {},
+
+    classes = [],
+
+    slice = classes.slice,
+
+    featureName,
+
+
+
+    _hasOwnProperty = ({}).hasOwnProperty, hasOwnProp;
+
+    if ( !is(_hasOwnProperty, 'undefined') && !is(_hasOwnProperty.call, 'undefined') ) {
+      hasOwnProp = function (object, property) {
+        return _hasOwnProperty.call(object, property);
+      };
     }
+    else {
+      hasOwnProp = function (object, property) {
+        return ((property in object) && is(object.constructor.prototype[property], 'undefined'));
+      };
+    }
+
 
     if (!Function.prototype.bind) {
-        Function.prototype.bind = function bind(that) {
+      Function.prototype.bind = function bind(that) {
 
-            var target = this;
+        var target = this;
 
-            if (typeof target != "function") {
-                throw new TypeError();
+        if (typeof target != "function") {
+            throw new TypeError();
+        }
+
+        var args = slice.call(arguments, 1),
+            bound = function () {
+
+            if (this instanceof bound) {
+
+              var F = function(){};
+              F.prototype = target.prototype;
+              var self = new F();
+
+              var result = target.apply(
+                  self,
+                  args.concat(slice.call(arguments))
+              );
+              if (Object(result) === result) {
+                  return result;
+              }
+              return self;
+
+            } else {
+
+              return target.apply(
+                  that,
+                  args.concat(slice.call(arguments))
+              );
+
             }
 
-            var args = slice.call(arguments, 1),
-                bound = function() {
-
-                    if (this instanceof bound) {
-
-                        var F = function() {};
-                        F.prototype = target.prototype;
-                        var self = new F();
-
-                        var result = target.apply(
-                            self,
-                            args.concat(slice.call(arguments))
-                        );
-                        if (Object(result) === result) {
-                            return result;
-                        }
-                        return self;
-
-                    } else {
-
-                        return target.apply(
-                            that,
-                            args.concat(slice.call(arguments))
-                        );
-
-                    }
-
-                };
-
-            return bound;
         };
+
+        return bound;
+      };
     }
 
-    function setCss(str) {
+    function setCss( str ) {
         mStyle.cssText = str;
     }
 
-    function setCssAll(str1, str2) {
-        return setCss(prefixes.join(str1 + ';') + (str2 || ''));
+    function setCssAll( str1, str2 ) {
+        return setCss(prefixes.join(str1 + ';') + ( str2 || '' ));
     }
 
-    function is(obj, type) {
+    function is( obj, type ) {
         return typeof obj === type;
     }
 
-    function contains(str, substr) {
+    function contains( str, substr ) {
         return !!~('' + str).indexOf(substr);
     }
 
-    function testProps(props, prefixed) {
-        for (var i in props) {
+    function testProps( props, prefixed ) {
+        for ( var i in props ) {
             var prop = props[i];
-            if (!contains(prop, "-") && mStyle[prop] !== undefined) {
+            if ( !contains(prop, "-") && mStyle[prop] !== undefined ) {
                 return prefixed == 'pfx' ? prop : true;
             }
         }
         return false;
     }
 
-    function testDOMProps(props, obj, elem) {
-        for (var i in props) {
+    function testDOMProps( props, obj, elem ) {
+        for ( var i in props ) {
             var item = obj[props[i]];
-            if (item !== undefined) {
+            if ( item !== undefined) {
 
-                if (elem === false) return props[i];
+                            if (elem === false) return props[i];
 
-                if (is(item, 'function')) {
-                    return item.bind(elem || obj);
+                            if (is(item, 'function')){
+                                return item.bind(elem || obj);
                 }
 
-                return item;
+                            return item;
             }
         }
         return false;
     }
 
-    function testPropsAll(prop, prefixed, elem) {
+    function testPropsAll( prop, prefixed, elem ) {
 
-        var ucProp = prop.charAt(0).toUpperCase() + prop.slice(1),
-            props = (prop + ' ' + cssomPrefixes.join(ucProp + ' ') + ucProp).split(' ');
+        var ucProp  = prop.charAt(0).toUpperCase() + prop.slice(1),
+            props   = (prop + ' ' + cssomPrefixes.join(ucProp + ' ') + ucProp).split(' ');
 
-        if (is(prefixed, "string") || is(prefixed, "undefined")) {
-            return testProps(props, prefixed);
+            if(is(prefixed, "string") || is(prefixed, "undefined")) {
+          return testProps(props, prefixed);
 
-        } else {
-            props = (prop + ' ' + (domPrefixes).join(ucProp + ' ') + ucProp).split(' ');
-            return testDOMProps(props, prefixed, elem);
+            } else {
+          props = (prop + ' ' + (domPrefixes).join(ucProp + ' ') + ucProp).split(' ');
+          return testDOMProps(props, prefixed, elem);
         }
-    }
-    tests['flexbox'] = function() {
-        return testPropsAll('flexWrap');
+    }    tests['flexbox'] = function() {
+      return testPropsAll('flexWrap');
     };
 
 
@@ -147,61 +162,70 @@ window.Modernizr = (function(window, document, undefined) {
         return testPropsAll('boxDirection');
     };
 
-    tests['svg'] = function() {
-        return !!document.createElementNS && !!document.createElementNS(ns.svg, 'svg').createSVGRect;
-    };
-    for (var feature in tests) {
-        if (hasOwnProp(tests, feature)) {
-            featureName = feature.toLowerCase();
+
+
+    for ( var feature in tests ) {
+        if ( hasOwnProp(tests, feature) ) {
+                                    featureName  = feature.toLowerCase();
             Modernizr[featureName] = tests[feature]();
 
             classes.push((Modernizr[featureName] ? '' : 'no-') + featureName);
         }
     }
 
-    Modernizr.addTest = function(feature, test) {
-        if (typeof feature == 'object') {
-            for (var key in feature) {
-                if (hasOwnProp(feature, key)) {
-                    Modernizr.addTest(key, feature[key]);
-                }
-            }
-        } else {
 
-            feature = feature.toLowerCase();
 
-            if (Modernizr[feature] !== undefined) {
-                return Modernizr;
-            }
+     Modernizr.addTest = function ( feature, test ) {
+       if ( typeof feature == 'object' ) {
+         for ( var key in feature ) {
+           if ( hasOwnProp( feature, key ) ) {
+             Modernizr.addTest( key, feature[ key ] );
+           }
+         }
+       } else {
 
-            test = typeof test == 'function' ? test() : test;
+         feature = feature.toLowerCase();
 
-            if (typeof enableClasses !== "undefined" && enableClasses) {
-                docElement.className += ' ' + (test ? '' : 'no-') + feature;
-            }
-            Modernizr[feature] = test;
+         if ( Modernizr[feature] !== undefined ) {
+                                              return Modernizr;
+         }
 
-        }
+         test = typeof test == 'function' ? test() : test;
 
-        return Modernizr;
-    };
+         if (typeof enableClasses !== "undefined" && enableClasses) {
+           docElement.className += ' ' + (test ? '' : 'no-') + feature;
+         }
+         Modernizr[feature] = test;
+
+       }
+
+       return Modernizr;
+     };
+
 
     setCss('');
     modElem = inputElem = null;
 
-    Modernizr._version = version;
-    Modernizr._domPrefixes = domPrefixes;
-    Modernizr._cssomPrefixes = cssomPrefixes;
 
-    Modernizr.testProp = function(prop) {
+    Modernizr._version      = version;
+
+    Modernizr._domPrefixes  = domPrefixes;
+    Modernizr._cssomPrefixes  = cssomPrefixes;
+
+
+
+    Modernizr.testProp      = function(prop){
         return testProps([prop]);
     };
 
-    Modernizr.testAllProps = testPropsAll;
+    Modernizr.testAllProps  = testPropsAll;
+
     docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1$2') +
-    (enableClasses ? ' js ' + classes.join(' ') : '');
+
+                                                    (enableClasses ? ' js ' + classes.join(' ') : '');
 
     return Modernizr;
 
-})(this, this.document);;
+})(this, this.document);
+;
 /* jshint ignore:end */
