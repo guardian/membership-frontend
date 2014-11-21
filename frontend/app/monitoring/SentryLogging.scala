a1dd1adc10c8f13d49c2c3c35813dbe145df091a
Roberto Tyley <roberto.tyley@theguardian.com>
2014-11-21 16:02:53 +0000

    Add Sentry logging of server-side errors with raven-logback library
    The credentials and url of the Sentry server are all stored in the
    "sentry.dsn" property, which I've put in /PROD/membership-keys.conf (but
    not the DEV keys- because we don't want that spamming Sentry, like I did
    while testing).

---
+++ frontend/app/monitoring/SentryLogging.scala
@@ -0,0 +1,37 @@
+package monitoring
+
+import ch.qos.logback.classic.filter.ThresholdFilter
+import ch.qos.logback.classic.{Logger, LoggerContext}
+import configuration.Config
+import net.kencochrane.raven.RavenFactory
+import net.kencochrane.raven.logback.SentryAppender
+import org.slf4j.Logger.ROOT_LOGGER_NAME
+import org.slf4j.LoggerFactory
+
+import scala.util.{Failure, Success}
+
+object SentryLogging {
+
+  def init() {
+    Config.sentryDsn match {
+      case Failure(ex) =>
+        play.api.Logger.warn("No Sentry logging configured (OK for dev)", ex)
+      case Success(dsn) =>
+        play.api.Logger.info(s"Initialising Sentry logging for ${dsn.getHost}")
+        val buildInfo: Map[String, Any] = app.BuildInfo.toMap
+        val tags = Map("stage" -> Config.stage) ++ buildInfo
+        val tagsString = tags.map { case (key, value) => s"$key:$value"}.mkString(",")
+
+        val filter = new ThresholdFilter { setLevel("ERROR") }
+        filter.start() // OMG WHY IS THIS NECESSARY LOGBACK?
+
+        val sentryAppender = new SentryAppender(RavenFactory.ravenInstance(dsn)) {
+          addFilter(filter)
+          setTags(tagsString)
+          setContext(LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext])
+        }
+        sentryAppender.start()
+        LoggerFactory.getLogger(ROOT_LOGGER_NAME).asInstanceOf[Logger].addAppender(sentryAppender)
+    }
+  }
+}
