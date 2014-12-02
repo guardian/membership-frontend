d84666a6895be482079abaca22b0b8654a4a65b3
Will Franklin <will.franklin@guardian.co.uk>
2014-12-02 14:25:44 +0000

    Test zuora ping in healthcheck

--- frontend/app/controllers/Healthcheck.scala
+++ frontend/app/controllers/Healthcheck.scala
@@ -1,15 +1,21 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
-import services.{MasterclassEventService, GuardianLiveEventService, MasterclassDataService}
+import services.{TouchpointBackend, GuardianLiveEventService}
 import com.gu.monitoring.CloudWatchHealth
+import com.github.nscala_time.time.Imports._
 
 object Healthcheck extends Controller {
 
   def healthcheck() = Action {
-    Cached(1)(if (GuardianLiveEventService.events.nonEmpty &&
-                  CloudWatchHealth.hasPushedMetricSuccessfully) Ok("OK")
-    else ServiceUnavailable("Service Unavailable"))
+    Cached(1) {
+      val zuoraLastPing = TouchpointBackend.Normal.zuoraService.pingTask.get()
+
+      if (GuardianLiveEventService.events.nonEmpty &&
+        CloudWatchHealth.hasPushedMetricSuccessfully &&
+        zuoraLastPing > DateTime.now - 2.minutes) Ok("OK")
+      else ServiceUnavailable("Service Unavailable")
+    }
   }
 
 }
