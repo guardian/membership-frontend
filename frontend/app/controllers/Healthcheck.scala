7856199153d7743ba52b78a396a06465bed37bbc
Will Franklin <will.franklin@guardian.co.uk>
2014-12-03 11:57:26 +0000

    Add logging for healthcheck tests

--- frontend/app/controllers/Healthcheck.scala
+++ frontend/app/controllers/Healthcheck.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.Logger
 import play.api.mvc.{Action, Controller}
 import services.{TouchpointBackend, GuardianLiveEventService}
 import com.gu.monitoring.CloudWatchHealth
@@ -7,13 +8,34 @@ import com.github.nscala_time.time.Imports._
 
 object Healthcheck extends Controller {
 
+  val tests = Map(
+    "Events" -> GuardianLiveEventService.events.nonEmpty _,
+    "CloudWatch" -> CloudWatchHealth.hasPushedMetricSuccessfully _,
+    "Zuora" -> (() => {
+      val lastPing = TouchpointBackend.Normal.zuoraService.pingTask.get()
+      val delta = (lastPing to DateTime.now).duration
+      if (delta > 2.minutes) {
+        Logger.error(s"Zuora has not responded for ${delta.toStandardSeconds.getSeconds} seconds")
+        false
+      } else {
+        true
+      }
+    })
+  )
+
   def healthcheck() = Action {
     Cached(1) {
       val zuoraLastPing = TouchpointBackend.Normal.zuoraService.pingTask.get()
 
-      if (GuardianLiveEventService.events.nonEmpty &&
-        CloudWatchHealth.hasPushedMetricSuccessfully &&
-        zuoraLastPing > DateTime.now - 2.minutes) Ok("OK")
+      val serviceOk = tests.forall { case (name, fn) =>
+          val result = fn()
+          if (!result) {
+            Logger.error(s"$name test failed, health check will fail")
+          }
+          result
+      }
+
+      if (serviceOk) Ok("OK")
       else ServiceUnavailable("Service Unavailable")
     }
   }
