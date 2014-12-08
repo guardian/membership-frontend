3d3d45dbca6345c9ea2be55c4af6d3e6219de250
Matt Andrews <matthew.andrews@guardian.co.uk>
2014-12-08 11:27:57 +0000

    Use an optional data attribute to toggle elements on pageload (and update styleguide to reflect this)

--- frontend/assets/javascripts/src/modules/events/toggle.js
+++ frontend/assets/javascripts/src/modules/events/toggle.js
@@ -1,18 +1,19 @@
 // *** generic toggle button component ***
 //
 // usage:
-//     <button class="js-toggle action action--toggle" data-toggle="foo" data-toggle-label="Less foo">Show more foo</button>
-//     <div id="foo" class="js-toggle-elm" style="display: none;">all the foo (initially hidden)</div>
+//     <button class="js-toggle" data-toggle="js-foo" data-toggle-label="Less foo">More foo</button>
+//     <div id="js-foo" data-toggle-hidden>all the foo (initially hidden)</div>
 // notes:
 //     * data-toggle-label is optional.
-//     * js-toggle-elm should be hidden initially (to allow inversion)
+//     * data-toggle-hidden should be added to toggle elements which should be hidden on pageload
 
 define(['$', 'bean', 'src/utils/analytics/ga'], function ($, bean, googleAnalytics) {
 
     var TOGGLE_BTN_SELECTOR = '.js-toggle',
         TOGGLE_DATA_ELM     = 'toggle',
         TOGGLE_DATA_LABEL   = 'toggle-label',
-        TOGGLE_CLASS        = 'is-toggled';
+        TOGGLE_CLASS        = 'is-toggled',
+        ELEMENTS_TO_TOGGLE  = '[data-toggle-hidden]';
 
     var toggleElm = function ($elem) {
         return function () {
@@ -39,6 +40,11 @@ define(['$', 'bean', 'src/utils/analytics/ga'], function ($, bean, googleAnalyti
         googleAnalytics.trackEvent('Toggle element', id, (hasToggled ? 'Show' : 'Hide'));
     };
 
+     var hideToggleElements = function () {
+        var toggleContainers = document.querySelectorAll(ELEMENTS_TO_TOGGLE);
+        $(toggleContainers).hide();
+    };
+
     var bindToggles = function () {
         var $toggles = $(TOGGLE_BTN_SELECTOR);
         $toggles.each(function (elem) {
@@ -47,6 +53,7 @@ define(['$', 'bean', 'src/utils/analytics/ga'], function ($, bean, googleAnalyti
     };
 
     function init() {
+        hideToggleElements();
         bindToggles();
     }
 
