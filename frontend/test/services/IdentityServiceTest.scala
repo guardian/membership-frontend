2d3ac91bc4f93bf1d5b93ea01a753fd241dc7f28
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-12-10 17:26:55 +0000

    Unit test for join paid in IdentityService

--- frontend/test/services/IdentityServiceTest.scala
+++ frontend/test/services/IdentityServiceTest.scala
@@ -1,8 +1,9 @@
 package services
 
+import com.gu.membership.salesforce.Tier
 import com.gu.membership.zuora.{Address, Countries}
 import controllers.IdentityRequest
-import forms.MemberForm.{FriendJoinForm, MarketingChoicesForm, NameForm}
+import forms.MemberForm._
 import model.IdMinimalUser
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
@@ -43,6 +44,27 @@ class IdentityServiceTest extends Specification with Mockito{
       val expectedJson = Resource.getJson(s"model/identity/update-friend.json").as[JsObject]
 
       identityService.updateUserFieldsBasedOnJoining(user, friendForm, identityRequest)
+      there was one(identityAPI).post("user/4444", expectedJson, headers, trackingParameters, "update-user")
+    }
+
+    "post json for updating users details on joining paid tier" in {
+      val identityAPI = mock[IdentityApi]
+
+      val identityService = new IdentityService(identityAPI)
+
+      val paidForm = PaidMemberJoinForm(
+        Tier.Partner,
+        NameForm("Joe", "Bloggs"),
+        PaymentForm(true, "token"),
+        Address("line one", "line 2", "town", "country", "postcode", Countries.UK),
+        Some(Address("line one", "line 2", "town", "country", "postcode", Countries.UK)),
+        MarketingChoicesForm(Some(false), Some(false)),
+        None
+      )
+
+      val expectedJson = Resource.getJson(s"model/identity/update-paid.json").as[JsObject]
+
+      identityService.updateUserFieldsBasedOnJoining(user, paidForm, identityRequest)
       there was one(identityAPI).post("user/4444", expectedJson, headers, trackingParameters, "update-user")
     }
   }
