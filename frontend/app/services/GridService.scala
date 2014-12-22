61b48b0fd17fb8b4ba474840afc229e0b5854bbc
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-12-22 15:06:48 +0000

    Get crop from the url.

--- frontend/app/services/GridService.scala
+++ frontend/app/services/GridService.scala
@@ -1,6 +1,8 @@
 package services
 
+import com.netaporter.uri.Uri
 import configuration.Config
+import com.netaporter.uri.Uri.parse
 
 case class GridConfig(url: String, apiUrl: String, key: String)
 
@@ -9,6 +11,11 @@ class GridService {
   def isUrlCorrectFormat(url: String) = url.startsWith(Config.gridConfig.url)
 
   def getApiUrl(url: String) = url.replace(Config.gridConfig.url, Config.gridConfig.apiUrl)
+
+  def getCropRequested(urlString: String) = {
+    val uri = parse(urlString)
+    uri.query.param("crop")
+  }
 
   //store the crop requested
 
--- frontend/test/services/GridServiceTest.scala
+++ frontend/test/services/GridServiceTest.scala
@@ -7,7 +7,7 @@ class GridServiceTest extends Specification {
   val service = new GridService
 
   val validGridUrl = "https://media.test.dev-gutools.co.uk/images/aef2fb1db22f7cd20683548719a2849b3c9962ec?crop=0_19_480_288"
-  val invalidGridUrl = "https://incorrect-format.media.test.dev-gutools.co.uk/images/aef2fb1db22f7cd20683548719a2849b3c9962ec?crop=0_19_480_288\""
+  val invalidGridUrl = "https://incorrect-format.media.test.dev-gutools.co.uk/images/aef2fb1db22f7cd20683548719a2849b3c9962ec?crop=0_19_480_288"
 
   "GridService" should {
     "confirm the url supplied in Eventbrite is from the Grid" in {
@@ -25,5 +25,13 @@ class GridServiceTest extends Specification {
 
   "must not substitute a Grid url with it's API equivalent" in {
       service.getApiUrl(invalidGridUrl) mustEqual(invalidGridUrl)
+  }
+
+  "must return the crop parameters from Grid url" in {
+    service.getCropRequested(validGridUrl) mustEqual(Some("0_19_480_288"))
+  }
+
+  "must return None if no parameter is passed into url" in {
+    service.getCropRequested("https://media.test.dev-gutools.co.uk/images/fsifjsifjsi") mustEqual(None)
   }
 }
