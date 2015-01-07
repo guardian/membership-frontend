51128cb568732fcb3fa11debcf58f87a0b7948bb
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-01-07 18:17:35 +0000

    Remove brackets for Will

--- frontend/test/model/GuLiveEventTest.scala
+++ frontend/test/model/GuLiveEventTest.scala
@@ -19,43 +19,43 @@ class GuLiveEventTest extends PlaySpecification with Mockito {
       val image = EventImage(gridResponse.data.exports(0).assets, gridResponse.data.metadata)
       val guEvent = GuLiveEvent(event, Some(image))
 
-      guEvent.imgUrl mustEqual ("https://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/294_26_1584_950/{width}.jpg")
-      guEvent.imageMetadata.flatMap(_.description) mustEqual(Some("It's Chris!"))
-      guEvent.imageMetadata.flatMap(_.source) mustEqual(None)
-      guEvent.availableWidths mustEqual(List(1000, 500))
-      guEvent.socialImgUrl mustEqual("http://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/294_26_1584_950/1000.jpg")
+      guEvent.imgUrl mustEqual "https://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/294_26_1584_950/{width}.jpg"
+      guEvent.imageMetadata.flatMap(_.description) mustEqual Some("It's Chris!")
+      guEvent.imageMetadata.flatMap(_.source) mustEqual None
+      guEvent.availableWidths mustEqual List(1000, 500)
+      guEvent.socialImgUrl mustEqual "http://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/294_26_1584_950/1000.jpg"
     }
 
     "use file url, metadata, socialUrl for image when no secure url is present" in {
       val image = EventImage(gridResponse.data.exports(1).assets, gridResponse.data.metadata)
       val guEvent = GuLiveEvent(event, Some(image))
 
-      guEvent.imgUrl mustEqual ("http://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/0_130_1703_1022/{width}.jpg")
-      guEvent.imageMetadata.flatMap(_.description) mustEqual(Some("It's Chris!"))
-      guEvent.imageMetadata.flatMap(_.source) mustEqual(None)
-      guEvent.availableWidths mustEqual(List(1000, 500, 140))
-      guEvent.socialImgUrl mustEqual("http://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/0_130_1703_1022/1000.jpg")
+      guEvent.imgUrl mustEqual "http://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/0_130_1703_1022/{width}.jpg"
+      guEvent.imageMetadata.flatMap(_.description) mustEqual Some("It's Chris!")
+      guEvent.imageMetadata.flatMap(_.source) mustEqual None
+      guEvent.availableWidths mustEqual List(1000, 500, 140)
+      guEvent.socialImgUrl mustEqual "http://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/0_130_1703_1022/1000.jpg"
     }
 
     "use fallback image when no image is found from the Grid" in {
       val guEvent = GuLiveEvent(event, None)
 
-      guEvent.imgUrl mustEqual(Config.gridConfig.fallbackImageUrl)
-      guEvent.imageMetadata.flatMap(_.description) mustEqual(None)
-      guEvent.imageMetadata.flatMap(_.source) mustEqual(None)
-      guEvent.availableWidths mustEqual(List.empty)
-      guEvent.socialImgUrl mustEqual(Config.gridConfig.fallbackImageUrl)
+      guEvent.imgUrl mustEqual Config.gridConfig.fallbackImageUrl
+      guEvent.imageMetadata.flatMap(_.description) mustEqual None
+      guEvent.imageMetadata.flatMap(_.source) mustEqual None
+      guEvent.availableWidths mustEqual List.empty
+      guEvent.socialImgUrl mustEqual Config.gridConfig.fallbackImageUrl
     }
 
     "use fallback image when assets in export is an empty list from the Grid" in {
       val image = EventImage(Nil, Metadata(None, None, None, None))
       val guEvent = GuLiveEvent(event, Some(image))
 
-      guEvent.imgUrl mustEqual(Config.gridConfig.fallbackImageUrl)
-      guEvent.imageMetadata.flatMap(_.description) mustEqual(None)
-      guEvent.imageMetadata.flatMap(_.source) mustEqual(None)
-      guEvent.availableWidths mustEqual(List.empty)
-      guEvent.socialImgUrl mustEqual(Config.gridConfig.fallbackImageUrl)
+      guEvent.imgUrl mustEqual Config.gridConfig.fallbackImageUrl
+      guEvent.imageMetadata.flatMap(_.description) mustEqual None
+      guEvent.imageMetadata.flatMap(_.source) mustEqual None
+      guEvent.availableWidths mustEqual List.empty
+      guEvent.socialImgUrl mustEqual Config.gridConfig.fallbackImageUrl
     }
   }
 }
