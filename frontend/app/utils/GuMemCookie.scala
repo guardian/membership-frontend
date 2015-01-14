2714e4df2b50cebad64b5d001361f14d4ab0f588
David Rapson <david.rapson@guardian.co.uk>
2015-01-14 16:55:58 +0000

    Extract base64encode Json method, use encodeBase64URLSafeString

---
+++ frontend/app/utils/GuMemCookie.scala
@@ -0,0 +1,12 @@
+package utils
+
+import org.apache.commons.codec.binary.Base64
+import play.api.libs.json.{JsObject, Json}
+
+object GuMemCookie {
+
+  def encodeUserJson(json: JsObject): String = {
+    Base64.encodeBase64URLSafeString(Json.stringify(json).getBytes)
+  }
+
+}
