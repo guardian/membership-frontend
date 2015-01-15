336217d85cc22b2125188e746b2cda5ed8ab2bdf
Will Franklin <will.franklin@guardian.co.uk>
2015-01-15 11:22:00 +0000

    Upgrade to Scala 2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,12 @@ import play._
 
 object Dependencies {
 
-  val identity = "3.42"
-
   //libraries
   val sentryRavenLogback = "net.kencochrane.raven" % "raven-logback" % "6.0.0"
-  val identityCookie = "com.gu.identity" %% "identity-cookie" % identity
+  val identityCookie = "com.gu.identity" %% "identity-cookie" % "3.44"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.4"
-  val scalaUri = "com.netaporter" %% "scala-uri" % "0.4.1"
-  val membershipCommon = "com.gu" %% "membership-common" % "0.44"
+  val scalaUri = "com.netaporter" %% "scala-uri" % "0.4.4"
+  val membershipCommon = "com.gu" %% "membership-common" % "0.45"
   val playGoogleAuth = "com.gu" %% "play-googleauth" % "0.1.10"
   val contentAPI = "com.gu" %% "content-api-client" % "3.5"
   val playWS = PlayImport.ws
