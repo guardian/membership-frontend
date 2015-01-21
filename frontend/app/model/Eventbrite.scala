7926b160321baf248b8918fef27147679d2374f1
Ben Chidgey <ben@feedmypixel.com>
2015-01-21 17:32:45 +0000

    logic moved from Event view support to EBEvent

--- frontend/app/model/Eventbrite.scala
+++ frontend/app/model/Eventbrite.scala
@@ -120,6 +120,9 @@ object Eventbrite {
     val generalReleaseTicket = ticket_classes.find(!_.isHidden)
     val memberTickets = ticket_classes.filter { t => t.isHidden && t.name.toLowerCase.startsWith("guardian member")}
     val hasMemberTicket = memberTickets.nonEmpty
+    val hasFreeGeneralReleaseTicket = generalReleaseTicket.exists(_.free)
+    val isFree = isSoldThruEventbrite && hasFreeGeneralReleaseTicket
+    val possiblyMissingDiscount = !hasFreeGeneralReleaseTicket && !hasMemberTicket
 
     val mainImageUrl: Option[String] = description.flatMap(desc => "<!--\\s*main-image: (.*?)\\s*-->".r.findFirstMatchIn(desc.html).map(_.group(1)) )
 
