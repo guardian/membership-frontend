f20a5ea9e490750858781e74f84e80875a765b1f
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-01-27 16:47:47 +0000

    Move the call to get guardian content to it's own trait. This is in preparation for adding another call to CAPI.

---
+++ frontend/app/services/GuardianContent.scala
@@ -0,0 +1,37 @@
+package services
+
+import com.gu.contentapi.client.{GuardianContentClient, GuardianContentApiError}
+import com.gu.contentapi.client.model.ItemResponse
+import configuration.Config
+import monitoring.ContentApiMetrics
+import org.joda.time.DateTime
+import play.api.Logger
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.concurrent.Future
+import scala.util.Failure
+
+case class ContentAPIPagination(currentPage: Int, pages: Int) {
+  lazy val nextPageOpt = Some(currentPage + 1).filter(_ <= pages)
+}
+
+trait GuardianContent {
+
+  val contentApi = new GuardianContentClient(Config.contentApiKey)
+
+  def masterclasses(page: Int): Future[ItemResponse] = {
+    val date = new DateTime(2014, 1, 1, 0, 0)
+    contentApi.item.itemId("guardian-masterclasses")
+      .fromDate(date)
+      .pageSize(100)
+      .page(page)
+      .showReferences("eventbrite")
+      .showFields("body")
+      .showElements("image")
+      .response.andThen {
+      case Failure(GuardianContentApiError(status, message)) =>
+        ContentApiMetrics.putResponseCode(status, "GET content")
+        Logger.error(s"Error response from Content API $status")
+    }
+  }
+}
--- frontend/app/services/MasterclassDataService.scala
+++ frontend/app/services/MasterclassDataService.scala
@@ -1,39 +1,29 @@
 package services
 
 import akka.agent.Agent
-import com.gu.contentapi.client.model.{Asset, Content, ItemResponse}
-import com.gu.contentapi.client.{GuardianContentApiError, GuardianContentClient}
-import configuration.Config
-import monitoring.ContentApiMetrics
-import org.joda.time.DateTime
-import play.api.Logger
+import com.gu.contentapi.client.model.{Asset, Content}
 import play.api.libs.iteratee.{Enumerator, Iteratee}
 import utils.ScheduledTask
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.util.Failure
 import scala.util.matching.Regex
 
 case class MasterclassData(eventId: String, webUrl: String, images: List[Asset])
 
 case class MasterclassResponse[T](pagination: ContentAPIPagination, data: Seq[T])
 
-case class ContentAPIPagination(currentPage: Int, pages: Int) {
-  lazy val nextPageOpt = Some(currentPage + 1).filter(_ <= pages)
-}
 
-trait MasterclassDataService {
+trait MasterclassDataService extends GuardianContent {
 
-  val contentApi = new GuardianContentClient(Config.contentApiKey)
   lazy val content = Agent[Seq[MasterclassData]](Seq.empty)
 
   protected def getAllContent: Future[Seq[MasterclassData]] = {
     val enumerator = Enumerator.unfoldM(Option(1)) {
       _.map { nextPage =>
         for {
-          response <- getContent(nextPage)
+          response <- masterclasses(nextPage)
         } yield {
           val masterclassData = response.results.flatMap(MasterclassDataExtractor.extractEventbriteInformation)
           val pagination = ContentAPIPagination(response.currentPage.getOrElse(0), response.pages.getOrElse(0))
@@ -44,22 +34,6 @@ trait MasterclassDataService {
     }
 
     enumerator(Iteratee.consume()).flatMap(_.run)
-  }
-
-  def getContent(page: Int): Future[ItemResponse] = {
-    val date = new DateTime(2014, 1, 1, 0, 0)
-    contentApi.item.itemId("guardian-masterclasses")
-      .fromDate(date)
-      .pageSize(100)
-      .page(page)
-      .showReferences("eventbrite")
-      .showFields("body")
-      .showElements("image")
-      .response.andThen {
-      case Failure(GuardianContentApiError(status, message)) =>
-        ContentApiMetrics.putResponseCode(status, "GET content")
-        Logger.error(s"Error response from Content API $status")
-    }
   }
 }
 
