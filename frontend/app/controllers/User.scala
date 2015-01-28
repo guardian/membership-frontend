a3d2e58e377ebc767820907342ad2070a42110e8
Ben Chidgey <ben@feedmypixel.com>
2015-01-28 13:19:17 +0000

    whitespace to be done in another pr

--- frontend/app/controllers/User.scala
+++ frontend/app/controllers/User.scala
@@ -19,6 +19,8 @@ trait User extends Controller {
     Ok(json).withCookies(Cookie("GU_MEM", GuMemCookie.encodeUserJson(json), secure = true, httpOnly = false))
   }
 
+
+
   def meDetails = AjaxMemberAction.async { implicit request =>
     def futureCardDetails = request.member match {
       case paidMember: PaidMember =>
