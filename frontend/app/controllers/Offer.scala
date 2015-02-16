18666df98664fe1b00ff52e38163a50a41a11267
Ben Chidgey <ben@feedmypixel.com>
2015-02-16 14:52:56 +0000

    moved subscriber ation to staff oauth until this is realeased

--- frontend/app/controllers/Offer.scala
+++ frontend/app/controllers/Offer.scala
@@ -1,9 +1,16 @@
 package controllers
 
+import actions.Functions._
+import configuration.Config
 import play.api.mvc.Controller
 
 trait Offer extends Controller {
-  def subscriber = NoCacheAction { implicit request =>
+  val permanentStaffGroups = Config.staffAuthorisedEmailGroups
+  val AuthorisedStaff = GoogleAuthenticatedStaffAction andThen isInAuthorisedGroupGoogleAuthReq(
+    permanentStaffGroups, views.html.fragments.oauth.staffWrongGroup())
+
+  // TODO move this to CachedAction once this work is ready to go into the wild
+  def subscriber = AuthorisedStaff { implicit request =>
     Ok(views.html.offer.subscriber())
   }
 }
