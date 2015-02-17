a9a4acc87bf16b3f31ae2732a99942ccded053eb
Matt Andrews <matthew.andrews@guardian.co.uk>
2015-02-17 12:28:46 +0000

    add masterclass overview page too

--- frontend/app/controllers/Staff.scala
+++ frontend/app/controllers/Staff.scala
@@ -3,19 +3,24 @@ package controllers
 import actions.Functions._
 import configuration.Config
 import play.api.mvc.Controller
-import services.GuardianLiveEventService
+import services.{GuardianLiveEventService, MasterclassEventService}
 
 import scala.concurrent.Future
 
 trait Staff extends Controller {
   val permanentStaffGroups = Config.staffAuthorisedEmailGroups
   val guLiveEvents = GuardianLiveEventService
+  val masterclassEvents = MasterclassEventService
 
   val AuthorisedStaff = GoogleAuthenticatedStaffAction andThen isInAuthorisedGroupGoogleAuthReq(
     permanentStaffGroups, views.html.fragments.oauth.staffWrongGroup())
 
   def eventOverview = AuthorisedStaff { implicit request =>
      Ok(views.html.staff.eventOverview(guLiveEvents.events, guLiveEvents.eventsDraft))
+  }
+
+  def masterclassOverview = AuthorisedStaff { implicit request =>
+     Ok(views.html.staff.eventOverview(masterclassEvents.events, masterclassEvents.eventsDraft))
   }
 }
 
