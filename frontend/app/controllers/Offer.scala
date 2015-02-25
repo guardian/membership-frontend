64915aef8531ab20858a67c1119f225bc04ca5da
Matt Andrews <matthew.andrews@guardian.co.uk>
2015-02-25 14:34:54 +0000

    WIP template for CAPI data

--- frontend/app/controllers/Offer.scala
+++ frontend/app/controllers/Offer.scala
@@ -3,15 +3,21 @@ package controllers
 import actions.Functions._
 import configuration.Config
 import play.api.mvc.Controller
+import services.GuardianContentService
 
 trait Offer extends Controller {
   val permanentStaffGroups = Config.staffAuthorisedEmailGroups
   val AuthorisedStaff = GoogleAuthenticatedStaffAction andThen isInAuthorisedGroupGoogleAuthReq(
     permanentStaffGroups, views.html.fragments.oauth.staffWrongGroup())
 
+  val contentApiService = GuardianContentService
+
   // TODO move this to CachedAction once this work is ready to go into the wild
   def subscriber = AuthorisedStaff { implicit request =>
     Ok(views.html.offer.subscriber())
+  }
+  def offersAndComps = CachedAction { implicit request =>
+    Ok(views.html.offer.offersandcomps(contentApiService.membersOnlyContent))
   }
 }
 
