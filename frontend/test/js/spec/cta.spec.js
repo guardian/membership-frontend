6586acf04fb37bab4b436a921ec1c8982b7cfdae
Ben Chidgey <ben@feedmypixel.com>
2015-03-03 11:36:31 +0000

    rework of tests

--- frontend/test/js/spec/cta.spec.js
+++ frontend/test/js/spec/cta.spec.js
@@ -1,47 +1,68 @@
+/**
+ * Rules for the CTA
+ * - If event is not on sale to anyone return 'unavailable' status object
+ * - If event is within priority booking mode and user is not logged-in return 'join' status object
+ * - If event is within priority booking mode, and user is logged-in user with a tier sale start date in the future return 'upgrade' status object
+ * - If event is within priority booking mode, and user is logged-in user with a tier sale start date in the past return empty status object
+ * - If event is on sale to everyone, and user is logged-in with or without a tier return empty status object
+ */
 define(['src/modules/events/cta'], function (cta) {
 
-    describe('newStatus', function () {
+    describe('Cta tests', function () {
 
-        it('be unavailable if off-sale to everyone' , function () {
-            var preSaleStart = new Date('1 Jan 2199');
-            var preSaleEnd = new Date('1 Jan 2199');
+        describe('getCtaStatus', function () {
 
-            expect(cta.newStatus(preSaleStart, preSaleEnd)).toEqual('unavailable');
-            expect(cta.newStatus(preSaleStart, preSaleEnd), new Date('1 Feb 2015')).toEqual('unavailable');
-            expect(cta.newStatus(preSaleStart, preSaleEnd), new Date('31 Dec 2198')).toEqual('unavailable');
-            expect(cta.newStatus(preSaleStart, preSaleEnd), false).toEqual('unavailable');
+            it('Event is not on sale to anyone' , function () {
+                var startDate = new Date('1 Jan 2198');
+                var endDate = new Date('1 Jan 2199');
+                var patronSaleStart = new Date(startDate.getTime());
+
+                it('with no user', function () {
+                    expect(cta.getCtaStatus(startDate, endDate, null).id).toEqual('unavailable');
+                });
+
+                it('with patron tier', function () {
+                    expect(cta.getCtaStatus(startDate, endDate, patronSaleStart).id).toEqual('unavailable');
+                });
+            });
+
+            describe('Event is within priority booking mode', function () {
+                var startDate = new Date('1 Jan 2015');
+                var endDate = new Date('1 Jan 2199');
+                var friendSaleStart = new Date(endDate.getTime());
+                var patronSaleStart = new Date(startDate.getTime());
+
+                it('with no user', function () {
+                    expect(cta.getCtaStatus(startDate, endDate, null).id).toEqual('join');
+                });
+
+                it('with friend tier', function () {
+                    expect(cta.getCtaStatus(startDate, endDate, friendSaleStart).id).toEqual('upgrade');
+                });
+
+                it('with patron tier', function () {
+                    expect(cta.getCtaStatus(startDate, endDate, patronSaleStart).id).toBeUndefined();
+                });
+            });
+
+            describe('Event is on sale to everyone', function () {
+                var startDate = new Date('1 Jan 2015');
+                var endDate = new Date('1 Feb 2015');
+                var friendSaleStart = new Date(endDate.getTime());
+                var patronSaleStart = new Date(startDate.getTime());
+
+                it('with no user', function () {
+                    expect(cta.getCtaStatus(startDate, endDate, null).id).toBeUndefined();
+                });
+
+                it('with friend tier', function () {
+                    expect(cta.getCtaStatus(startDate, endDate, friendSaleStart).id).toBeUndefined();
+                });
+
+                it('with patron tier', function () {
+                    expect(cta.getCtaStatus(startDate, endDate, patronSaleStart).id).toBeUndefined();
+                });
+            });
         });
-
-        describe('Event in pre-sale mode', function () {
-            it('should prompt to become a member if pre-sale but logged-out' , function () {
-                var preSaleStart = new Date('1 Jan 2015');
-                var preSaleEnd = new Date('1 Jan 2199');
-
-                expect(cta.newStatus(preSaleStart, preSaleEnd)).toEqual('join');
-            });
-            it('should prompt to upgrade if the users tier cannot buy tickets yet' , function () {
-
-                var preSaleStart = new Date('1 Jan 2015');
-                var preSaleEnd = new Date('1 Jan 2199');
-                var tierDate = new Date('31 Dec 2198');
-
-                expect(cta.newStatus(preSaleStart, preSaleEnd, tierDate)).toEqual('upgrade');
-            });
-            it('should be bookable if the users tier can buy tickets' , function () {
-
-                var preSaleStart = new Date('1 Jan 2015');
-                var preSaleEnd = new Date('1 Jan 2199');
-                var tierDate = new Date('1 Feb 2015');
-
-                /**
-                 * Should return false as bookable is the default action,
-                 * so don't do anything.
-                 */
-                expect(cta.newStatus(preSaleStart, preSaleEnd, tierDate)).toEqual(false);
-            });
-        });
-
     });
-
 });
-
