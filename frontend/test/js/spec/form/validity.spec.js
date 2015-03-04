183defb79619ca62c9eab6ff2d8c11551e4ec9e0
Ben Chidgey <ben@feedmypixel.com>
2015-03-04 18:07:24 +0000

    added validity tests

---
+++ frontend/test/js/spec/form/validity.spec.js
@@ -0,0 +1,116 @@
+define([
+    'stripe',
+    'src/modules/form/validation/validity'
+], function (stripe, validity) {
+
+    var elem = document.createElement('input');
+    var pattern = /\s\w+\d{2}/;
+    var lengthElem;
+    var requiredElem;
+    var patternElem;
+    var validationProfileElem;
+
+    describe('Test Validity method', function () {
+
+        describe('No Validation', function () {
+
+            it('simple input', function () {
+                var standardElem = elem.cloneNode();
+
+                expect(validity.testValidity(standardElem)).toBe(true);
+            });
+
+            it('disabled input', function () {
+                var disabledElem = elem.cloneNode();
+                disabledElem.disabled = true;
+
+                expect(validity.testValidity(disabledElem)).toBe(true);
+            });
+        });
+
+        describe('Required', function () {
+
+            beforeEach(function () {
+                requiredElem = elem.cloneNode();
+                requiredElem.setAttribute('required', 'required');
+            });
+
+            it('empty value', function () {
+                expect(validity.testValidity(requiredElem)).toBe(false);
+            });
+
+            it('with value', function () {
+                requiredElem.value = 'Henry Dorsett Case'
+
+                expect(validity.testValidity(requiredElem)).toBe(true);
+            });
+        });
+
+        describe('Length', function () {
+
+            beforeEach(function () {
+                lengthElem = elem.cloneNode();
+                lengthElem.setAttribute('minlength', 5);
+                lengthElem.setAttribute('maxlength', 10);
+            });
+
+            it('between min and max', function () {
+                lengthElem.value = 'Armitage';
+
+                expect(validity.testValidity(lengthElem)).toBe(true);
+            });
+
+            it('below min', function () {
+                lengthElem.value = 'Case';
+
+                expect(validity.testValidity(lengthElem)).toBe(false);
+            });
+
+            it('above max', function () {
+                lengthElem.value = 'Dixie Flatline';
+
+                expect(validity.testValidity(lengthElem)).toBe(false);
+            });
+        });
+
+        describe('Pattern', function () {
+
+            beforeEach(function () {
+                patternElem = elem.cloneNode();
+                patternElem.setAttribute('pattern', pattern.source);
+            });
+
+            it('match', function () {
+                patternElem.value = 'Peter Riviera56';
+
+                expect(validity.testValidity(patternElem)).toBe(true);
+            });
+
+            it('no match', function () {
+                patternElem.value = 'Peter Riviera';
+
+                expect(validity.testValidity(patternElem)).toBe(false);
+            });
+        });
+
+        describe('Validation profile', function () {
+
+            beforeEach(function () {
+                validationProfileElem = elem.cloneNode();
+            });
+
+            it('does not exist', function () {
+                validationProfileElem.setAttribute('data-validation', 'notAValidationProfile');
+
+                expect(validity.testValidity(validationProfileElem)).toBe(false);
+            });
+
+            it('exists', function () {
+                spyOn(stripe.card, 'validateCardNumber').and.returnValue(true);
+                validationProfileElem.setAttribute('data-validation', 'validCreditCardNumber');
+
+                expect(validity.testValidity(validationProfileElem)).toBe(true);
+            });
+        });
+    });
+});
