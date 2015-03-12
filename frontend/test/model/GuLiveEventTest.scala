f5d7e2f9084ccc93467fc0dcbe59e8066db1c0f5
David Rapson <david.rapson@guardian.co.uk>
2015-03-12 14:21:49 +0000

    Update tests

--- frontend/test/model/GuLiveEventTest.scala
+++ frontend/test/model/GuLiveEventTest.scala
@@ -15,24 +15,16 @@ class GuLiveEventTest extends PlaySpecification with Mockito {
   val gridResponse = grid.as[GridResult]
 
   "GuLiveEventTest" should {
-    "contain secure url, metadata and socialUrl for image" in {
-      val image = EventImage(gridResponse.data.exports.get(0).assets, gridResponse.data.metadata)
-      val guEvent = GuLiveEvent(event, Some(image), None)
 
-      guEvent.imageMetadata.flatMap(_.description) mustEqual Some("It's Chris!")
-      guEvent.imageMetadata.map(_.photographer) mustEqual Some("Joe Bloggs/Guardian Images")
-      guEvent.socialImgUrl.get mustEqual "https://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/294_26_1584_950/1000.jpg"
-    }
+    "contain metadata and socialUrl for an event image" in {
 
-    "use file url, metadata, socialUrl for image when no secure url is present" in {
       val image = EventImage(gridResponse.data.exports.get(1).assets, gridResponse.data.metadata)
       val guEvent = GuLiveEvent(event, Some(image), None)
 
       guEvent.imageMetadata.flatMap(_.description) mustEqual Some("It's Chris!")
       guEvent.imageMetadata.map(_.photographer) mustEqual Some("Joe Bloggs/Guardian Images")
 
-      guEvent.socialImgUrl.get mustEqual "http://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/0_130_1703_1022/1000.jpg"
+      guEvent.socialImgUrl.get mustEqual "http://some-media-thing/aede0da05506d0d8cb993558b7eb9ad1d2d3e675/0_130_1703_1022/500.jpg"
     }
-
   }
 }
