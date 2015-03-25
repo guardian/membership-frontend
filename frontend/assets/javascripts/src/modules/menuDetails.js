be7e0e066aa273999ed6aaf5e51b2312302afa48
Ben Chidgey <ben@feedmypixel.com>
2015-03-25 17:49:41 +0000

    split out identity menu detail population

---
+++ frontend/assets/javascripts/src/modules/menuDetails.js
@@ -0,0 +1,49 @@
+define(['src/utils/user'], function (userUtil) {
+
+    var MENU_CTA_TEXT_ELEM = document.querySelector('.js-identity-menu-cta-text');
+    var MENU_EDIT_PROFILE_ELEM = document.querySelector('.js-identity-menu-edit-profile');
+    var MENU_COMMENT_ACTIVITY_ELEM = document.querySelector('.js-identity-menu-comment-activity');
+    var HEADER_JOIN_US_CTA_ELEM = document.querySelector('.js-header-join-us-cta');
+
+    function init() {
+        var identityUser = userUtil.getUserFromCookie();
+
+        if (identityUser) {
+            hideIdentityCtaText();
+            appendUserIdToCommentActivityLink(identityUser.id);
+
+            userUtil.getMemberDetail(function (memberDetail) {
+                if (memberDetail && memberDetail.tier) {
+                    updateEditProfileLink();
+                }
+            });
+        } else {
+            showJoinUsCta();
+        }
+    }
+
+    function hideIdentityCtaText() {
+        MENU_CTA_TEXT_ELEM.textContent = 'You are signed in as';
+        MENU_CTA_TEXT_ELEM.classList.add('is-visually-hidden');
+    }
+
+    function updateEditProfileLink() {
+        MENU_EDIT_PROFILE_ELEM.setAttribute('href',
+            MENU_EDIT_PROFILE_ELEM.getAttribute('data-member-href')
+        );
+    }
+
+    function appendUserIdToCommentActivityLink(id) {
+        MENU_COMMENT_ACTIVITY_ELEM.setAttribute('href',
+                MENU_COMMENT_ACTIVITY_ELEM.getAttribute('href') + id
+        );
+    }
+
+    function showJoinUsCta() {
+        HEADER_JOIN_US_CTA_ELEM.classList.remove('is-hidden');
+    }
+
+    return {
+        init: init
+    };
+});
