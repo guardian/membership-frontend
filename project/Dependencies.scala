10833a7e6cb4164879525a3ae5b299e1df0d191f
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2015-04-10 15:00:57 +0100

    Updated dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val identityCookie = "com.gu.identity" %% "identity-cookie" % "3.44"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.5"
   val scalaUri = "com.netaporter" %% "scala-uri" % "0.4.6"
-  val membershipCommon = "com.gu" %% "membership-common" % "0.61" //todo update dep
+  val membershipCommon = "com.gu" %% "membership-common" % "0.63"
   val playGoogleAuth = "com.gu" %% "play-googleauth" % "0.1.11"
   val contentAPI = "com.gu" %% "content-api-client" % "5.2"
   val playWS = PlayImport.ws
