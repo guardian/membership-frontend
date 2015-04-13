5e819f5041a6f3146d5fc1808494c74048505302
David Rapson <david.rapson@guardian.co.uk>
2015-04-13 12:21:50 +0100

    Use different placeholder for supporter video

--- frontend/app/configuration/Videos.scala
+++ frontend/app/configuration/Videos.scala
@@ -22,7 +22,7 @@ object Videos {
       ResponsiveImageGroup(
         altText=Some("If you read the Guardian, join the Guardian"),
         availableImages=ResponsiveImageGenerator(
-          id="9c47cb6060bbbd7b5eed5c87fd2bbbdeb1585a11/0_0_2280_1368",
+          id="76c306a5c0fed576a7fa3fa18c246ca52f671ad3/0_0_2280_1368",
           sizes=List(1000, 500)
         )
       )
