b11bfc13d26122d29f8e413585649cdedf8cb93c
Dominic Kendrick <dominic.kendrick@guardian.co.uk>
2015-04-17 12:27:27 +0100

    updating dependencies to lateset versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,8 @@ import play._
 
 object Dependencies {
 
+  //versions
+  val awsClientVersion = "1.9.30"
   //libraries
   val sentryRavenLogback = "net.kencochrane.raven" % "raven-logback" % "6.0.0"
   val identityCookie = "com.gu.identity" %% "identity-cookie" % "3.44"
@@ -10,20 +12,19 @@ object Dependencies {
   val scalaUri = "com.netaporter" %% "scala-uri" % "0.4.6"
   val membershipCommon = "com.gu" %% "membership-common" % "0.63"
   val playGoogleAuth = "com.gu" %% "play-googleauth" % "0.1.11"
-  val googleApiClient = "com.google.api-client" % "google-api-client" % "1.19.1"
-  val googleAdminService = "com.google.apis" % "google-api-services-admin" % "directory_v1-rev32-1.16.0-rc"
+  val googleAdminService = "com.google.apis" % "google-api-services-admin-directory" % "directory_v1-rev53-1.20.0"
   val contentAPI = "com.gu" %% "content-api-client" % "5.2"
   val playWS = PlayImport.ws
   val playCache = PlayImport.cache
   val playFilters = PlayImport.filters
-  val awsSimpleEmail = "com.amazonaws" % "aws-java-sdk-ses" % "1.9.24"
+  val awsSimpleEmail = "com.amazonaws" % "aws-java-sdk-ses" % awsClientVersion
   val snowPlow = "com.snowplowanalytics" % "snowplow-java-tracker" % "0.5.2-SNAPSHOT"
   val bCrypt = "com.github.t3hnar" %% "scala-bcrypt" % "2.4"
-  val s3 =  "com.amazonaws" % "aws-java-sdk-s3" % "1.9.30"
+  val s3 =  "com.amazonaws" % "aws-java-sdk-s3" % awsClientVersion
 
   //projects
 
-  val frontendDependencies = Seq(identityCookie, googleApiClient, googleAdminService, playGoogleAuth, identityTestUsers, scalaUri, membershipCommon,
+  val frontendDependencies = Seq(identityCookie, googleAdminService, playGoogleAuth, identityTestUsers, scalaUri, membershipCommon,
     contentAPI, playWS, playCache, playFilters,sentryRavenLogback, awsSimpleEmail, snowPlow, bCrypt, s3)
 
 }
