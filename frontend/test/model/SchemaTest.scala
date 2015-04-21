b1576c94de841b8489a1671b464cfa649dfa4508
David Rapson <david.rapson@guardian.co.uk>
2015-04-21 10:10:05 +0100

    Add tests for JSON LD

---
+++ frontend/test/model/SchemaTest.scala
@@ -0,0 +1,25 @@
+package model
+
+import model.RichEvent.GuLiveEvent
+import play.api.test.PlaySpecification
+import play.api.libs.json.{Json}
+import model.EventbriteTestObjects._
+
+class SchemaTest extends PlaySpecification {
+
+  "EventSchema" should {
+
+    "generate JSON-LD schema data for an event" in {
+
+      val event = GuLiveEvent(eventWithName("Test Event"), None, None)
+      val json = Json.toJson(event.schema)
+
+      (json \ "@context").as[String] === "http://schema.org"
+      (json \ "@type").as[String] === "Event"
+      (json \ "name").as[String] === "Test Event"
+      (json \ "description").as[String] === "Event Description"
+    }
+
+  }
+
+}
