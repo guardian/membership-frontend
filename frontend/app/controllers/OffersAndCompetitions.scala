023a10a1d11a2b494bc8ab7707b21c7b4f09122c
David Rapson <david.rapson@guardian.co.uk>
2015-05-07 11:07:31 +0100

    Filter offers content, make page public

--- frontend/app/controllers/OffersAndCompetitions.scala
+++ frontend/app/controllers/OffersAndCompetitions.scala
@@ -4,11 +4,17 @@ import model.ContentItemOffer
 import play.api.mvc.Controller
 import services.GuardianContentService
 
-
 trait OffersAndCompetitions extends Controller {
-  // TODO move this to CachedAction once this work is ready to go into the wild
-  def list = GoogleAuthenticatedStaffAction { implicit request =>
+  def list = CachedAction { implicit request =>
+    /**
+     * Only show items that:
+     * - Don't have the membershipAccess field set
+     * - Have an available image
+     */
     val results = GuardianContentService.offersAndCompetitionsContent.map(ContentItemOffer)
+      .filter { item =>
+        item.content.fields.map(_("membershipAccess")).isEmpty
+      }.filter(_.imgOpt.nonEmpty)
     Ok(views.html.offer.offersandcomps(results, "Sorry, no matching items were found."))
   }
 }
--- frontend/app/model/RichContent.scala
+++ frontend/app/model/RichContent.scala
@@ -9,10 +9,10 @@ abstract class RichContent(content: Content) {
 case class ContentItem(content: Content) extends RichContent(content)
 
 case class ContentItemOffer(content: Content) extends RichContent(content) {
-  val tagTitleOpt = content.tags.find { tag =>
+  val tagTitleOpt = content.tags.map { tag =>
     tag.id.toLowerCase match {
-      case "membership/membership-offers" | "membership/membership-competitions" => true
-      case _ => false
+      case "membership/membership-offers" | "membership/membership-competitions" => Some(tag.webTitle.replace("Membership ", ""))
+      case _ => None
     }
   }
 }
