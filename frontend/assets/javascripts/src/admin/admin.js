82195912839755efcb2115db07abe00b7bda837e
Matt Andrews <matthew.andrews@guardian.co.uk>
2015-05-18 16:11:53 +0100

    add basic tab module

--- frontend/assets/javascripts/src/admin/admin.js
+++ frontend/assets/javascripts/src/admin/admin.js
@@ -1,1 +1,34 @@
-// hello, world!
+/*
+ * basic tab module
+ *
+ */
+var ACTIVE_TAB_CLASS    = 'tabs__tab--active';
+var ACTIVE_PANE_CLASS   = 'tabs__content--active';
+var PANE_DATA_ATTR      = 'data-tab-pane';
+var TAB_HOLDER          = 'js-tabs';
+var TAB_ITEM            = 'js-tabs__tab';
+
+var tabSets = document.querySelectorAll('.' + TAB_HOLDER);
+[].forEach.call(tabSets, function(tabSet) {
+    var tabs = tabSet.querySelectorAll('.' + TAB_ITEM);
+    var paneHolder = document.getElementById(tabSet.getAttribute(PANE_DATA_ATTR));
+
+    [].forEach.call(tabs, function(tab) {
+        tab.addEventListener('click', function(e) {
+
+            var prevActiveTab = tabSet.querySelector('.' + ACTIVE_TAB_CLASS);
+            if (prevActiveTab) { prevActiveTab.classList.remove(ACTIVE_TAB_CLASS); }
+
+            var prevActivePane = paneHolder.querySelector('.' + ACTIVE_PANE_CLASS);
+            if (prevActivePane) { prevActivePane.classList.remove(ACTIVE_PANE_CLASS); }
+
+            tab.classList.toggle(ACTIVE_TAB_CLASS);
+            var pane = tab.getAttribute('href').replace('#', '');
+            if (pane) {
+                document.getElementById(pane).classList.toggle(ACTIVE_PANE_CLASS);
+            }
+
+            e.preventDefault();
+        });
+    });
+});
