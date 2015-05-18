19cc98640796c9b598ff4f6324704f3a4cd2f417
David Rapson <david.rapson@guardian.co.uk>
2015-05-18 10:28:44 +0100

    Use guardian.co.uk address for new test users group

--- frontend/app/controllers/Testing.scala
+++ frontend/app/controllers/Testing.scala
@@ -15,7 +15,7 @@ object Testing extends Controller with LazyLogging {
   val AuthorisedTester = GoogleAuthenticatedStaffAction andThen isInAuthorisedGroupGoogleAuthReq(
     Set(
       "membership.dev@guardian.co.uk",
-      "membership.testusers@theguardian.com",
+      "membership.testusers@guardian.co.uk",
       "touchpoint@guardian.co.uk",
       "crm@guardian.co.uk"
     ),
