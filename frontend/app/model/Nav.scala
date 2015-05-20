4dca8c8ac0e0121855622f31a9059941eed04f06
David Rapson <david.rapson@guardian.co.uk>
2015-05-20 16:22:53 +0100

    Refactor secondary nav, allow for private items

--- frontend/app/model/Nav.scala
+++ frontend/app/model/Nav.scala
@@ -1,19 +1,18 @@
 package model
 
-import configuration.Config
-
 object Nav {
 
   case class NavItem(
     id: String,
     href: String,
     title: String,
+    isPrivate: Boolean = false,
     subNav: Seq[NavItem] = Nil
   )
 
   def fetchNav(url: String) = {
     val navigationURLs = navigation.find { navItem =>
-      // we should return parent item if URL is a subnav
+      // Return the parent item if URL is a subnav
       navItem.href == url || navItem.subNav.map(_.href).contains(url)
     }
     navigationURLs
@@ -21,6 +20,11 @@ object Nav {
 
   val navigation = List(
     NavItem("events", "/events", "Events"),
+    NavItem("whats-on", "/whats-on", "What's On", isPrivate = true,
+      subNav = List(
+        NavItem("calendar", "/whats-on/calendar", "Calendar")
+      )
+    ),
     NavItem("masterclasses", "/masterclasses", "Masterclasses"),
     NavItem("offers", "/offers-competitions", "Offers"),
     NavItem("about", "/about", "About membership"),
