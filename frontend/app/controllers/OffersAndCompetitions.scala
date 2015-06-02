bb8e1aa56001aacb2633473098c17621316d9637
David Rapson <david.rapson@guardian.co.uk>
2015-06-02 17:53:34 +0100

    Filter out offers and competitions which start with EXPIRED

--- frontend/app/controllers/OffersAndCompetitions.scala
+++ frontend/app/controllers/OffersAndCompetitions.scala
@@ -6,10 +6,12 @@ import model.ContentItemOffer
 
 trait OffersAndCompetitions extends Controller {
   def list = CachedAction { implicit request =>
+
     val results = GuardianContentService.offersAndCompetitionsContent.map(ContentItemOffer)
-      .filter { item =>
-        item.content.fields.map(_("membershipAccess")).isEmpty
-      }.filter(_.imgOpt.nonEmpty)
+      .filter(_.content.fields.map(_("membershipAccess")).isEmpty)
+      .filter(!_.content.webTitle.startsWith("EXPIRED"))
+      .filter(_.imgOpt.nonEmpty)
+
     Ok(views.html.offer.offersandcomps(results))
   }
 }
