bfb320a15774dc13bb5dec96afb773d19529f2da
David Rapson <david.rapson@guardian.co.uk>
2015-06-05 16:09:49 +0100

    Refactor Benefits for Supporter launch

--- frontend/app/model/Benefits.scala
+++ frontend/app/model/Benefits.scala
@@ -15,60 +15,84 @@ object Benefits {
     desc: String
   )
 
-  case class BenefitItem(title: String, description: String, identifier: String, icon: String)
+  case class BenefitItem(identifier: String, title: String, description: String, icon: String)
 
   val allBenefits = Seq(
-    BenefitItem("Book tickets", "Book tickets to Guardian Live events", "book_tickets", "benefit-booking"),
-    BenefitItem("Membership email updates", "Receive regular updates from the membership community", "digital_digest", "benefit-digest"),
-    BenefitItem("Video highlights", "Watch highlights of selected Guardian Live events", "video_highlights", "benefit-video"),
-    BenefitItem("Early booking", "Early ticket booking on Guardian Live Events (before Friends)", "early_booking", "benefit-priority-booking"),
-    BenefitItem("50% off live events", "50% discount on Guardian Live events until 30 June (usually 20% discount)", "discount", "benefit-live-discount"),
-    BenefitItem("20% off masterclasses", "20% discount on Guardian Masterclasses", "discount_masterclasses", "benefit-masterclasses-discount"),
-    BenefitItem("Membership card", "Membership card and annual gift", "membership_card", "benefit-card"),
-    BenefitItem("+1 guest", "Bring a guest to Guardian Live with the same discount and priority booking advantages", "plus_1_guest", "benefit-plus1"),
-    BenefitItem("Live stream events", "Watch live streams of flagship Guardian Live Membership events", "live_stream", "benefit-stream"),
-    BenefitItem("Priority booking", "Additional priority ticket booking on Guardian Live Events (before Partners)", "priority_booking", "benefit-priority-booking"),
-    BenefitItem("Special thank-yous", "The occasional unique gift to thank you for your support", "complim_items", "benefit-gifts"),
-    BenefitItem("Unique experiences", "Get behind the scenes of our journalism", "unique_experiences", "benefit-experiences")
+    BenefitItem("book_tickets", "Book tickets", "Book tickets to Guardian Live events", "benefit-booking"),
+    BenefitItem("digital_digest", "Membership email updates", "Receive regular updates from the membership community", "benefit-digest"),
+    BenefitItem("video_highlights", "Video highlights", "Watch highlights of selected Guardian Live events", "benefit-video"),
+    BenefitItem("early_booking", "Early booking", "Early ticket booking on Guardian Live Events (before Friends and Supporters)", "benefit-priority-booking"),
+    BenefitItem("discount", "50% off live events", "50% discount on Guardian Live events until 30 June (usually 20% discount)", "benefit-live-discount"),
+    BenefitItem("discount_masterclasses", "20% off masterclasses", "20% discount on Guardian Masterclasses", "benefit-masterclasses-discount"),
+    BenefitItem("membership_card", "Membership card", "Membership card and annual gift", "benefit-card"),
+    BenefitItem("plus_1_guest", "+1 guest", "Bring a guest to Guardian Live with the same discount and priority booking advantages", "benefit-plus1"),
+    BenefitItem("live_stream", "Live stream events", "Watch live streams of flagship Guardian Live Membership events", "benefit-stream"),
+    BenefitItem("priority_booking", "Priority booking", "Additional priority ticket booking on Guardian Live Events (before Partners)", "benefit-priority-booking"),
+    BenefitItem("complim_items", "Special thank-yous", "The occasional unique gift to thank you for your support", "benefit-gifts"),
+    BenefitItem("unique_experiences", "Unique experiences", "Get behind the scenes of our journalism", "benefit-experiences")
   )
 
   case class Pricing(yearly: Int, monthly: Int) {
-    lazy val yearlyMonthlyCost = (12 * monthly)
+    lazy val yearlyMonthlyCost = 12 * monthly
     lazy val yearlySaving = yearlyMonthlyCost - yearly
     lazy val yearlyWith6MonthSaving = yearly / 2f
     lazy val hasYearlySaving = yearlySaving > 0
   }
 
-  def benefitsFilter(identifiers: String*) = identifiers.flatMap { id =>
+  private def benefitsFilter(identifiers: String*) = identifiers.flatMap { id =>
     allBenefits.find(_.identifier == id)
   }
 
-  val friendsWithBenefits = benefitsFilter("book_tickets", "digital_digest", "video_highlights")
+  private def uniqueBenefits(benefits: Seq[BenefitItem], exclude: Seq[BenefitItem]) = {
+    benefits.filterNot(exclude.toSet)
+  }
 
-  val supporterWithBenefits = benefitsFilter("membership_card", "live_stream", "digital_digest", "book_tickets")
+  val friendBenefitsList = benefitsFilter(
+    "book_tickets",
+    "digital_digest",
+    "video_highlights"
+  )
+  val supporterBenefitsList = benefitsFilter(
+    "membership_card",
+    "live_stream",
+    "video_highlights",
+    "digital_digest",
+    "book_tickets"
+  )
+  val partnerBenefitsList = benefitsFilter(
+    "early_booking",
+    "plus_1_guest",
+    "live_stream",
+    "discount",
+    "discount_masterclasses",
+    "membership_card",
+    "digital_digest",
+    "video_highlights",
+    "book_tickets"
+  )
+  val patronBenefitsList = benefitsFilter(
+    "discount",
+    "discount_masterclasses",
+    "priority_booking",
+    "complim_items",
+    "unique_experiences",
+    "plus_1_guest",
+    "membership_card",
+    "live_stream",
+    "digital_digest",
+    "video_highlights"
+  )
 
-  val partnerWithBenefits = benefitsFilter("early_booking", "plus_1_guest", "live_stream", "discount", "discount_masterclasses",
-    "membership_card", "digital_digest", "video_highlights", "book_tickets")
+  val friendBenefits = Benefits("Benefits", friendBenefitsList,
+    None, "Become a Friend", "Become a friend to book tickets to Guardian Live events and access the Guardian members area.")
 
-  val patronWithBenefits = benefitsFilter("discount", "discount_masterclasses", "priority_booking",
-    "complim_items", "unique_experiences", "plus_1_guest", "membership_card",
-    "live_stream", "digital_digest", "video_highlights")
+  val supporterBenefits = Benefits("Benefits", supporterBenefitsList,
+    Some(Pricing(60, 5)), "Become a Supporter", "Supporters keep our journalism fearless, open and free from interference.")
 
-  var partnerWithBenefitsLimited = benefitsFilter("discount", "discount_masterclasses", "early_booking",
-    "plus_1_guest", "live_stream", "membership_card")
-
-  var patronWithBenefitsLimited = benefitsFilter("priority_booking", "complim_items", "unique_experiences")
-
-  val friendBenefits = Benefits("Benefits", friendsWithBenefits,
-    None, "Become a Friend", "Stay up to date and book tickets to Guardian Live events")
-
-  val supporterBenefits = Benefits("Benefits", supporterWithBenefits,
-    Some(Pricing(60, 5)), "Become a Supporter", "Stay up to date and book tickets to Guardian Live events")
-
-  val partnerBenefits = Benefits("Friend benefits, plus…", partnerWithBenefits,
+  val partnerBenefits = Benefits("Supporter benefits, plus…", partnerBenefitsList,
     Some(Pricing(135, 15)), "Become a Partner", "Support the Guardian and experience it brought to life, with early booking and discounted tickets")
 
-  val patronBenefits = Benefits("Partner benefits, plus…", patronWithBenefits,
+  val patronBenefits = Benefits("Partner benefits, plus…", patronBenefitsList,
     Some(Pricing(540, 60)), "Become a Patron", "Defend the Guardian’s independence and promote the open exchange of ideas, with a backstage pass to the Guardian")
 
   def details(tier: Tier) = tier match {
@@ -79,9 +103,10 @@ object Benefits {
   }
 
   def detailsLimited(tier: Tier) = tier match {
-    case Tier.Friend => friendsWithBenefits
-    case Tier.Supporter => supporterWithBenefits
-    case Tier.Partner => partnerWithBenefitsLimited
-    case Tier.Patron => patronWithBenefitsLimited
+    case Tier.Friend => friendBenefitsList
+    case Tier.Supporter => supporterBenefitsList
+    case Tier.Partner => uniqueBenefits(partnerBenefitsList, supporterBenefitsList)
+    case Tier.Patron => uniqueBenefits(patronBenefitsList, partnerBenefitsList)
   }
+
 }
