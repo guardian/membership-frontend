b5f560f8f06c47762fcf91be4c29b2712bd6c517
David Rapson <david.rapson@guardian.co.uk>
2015-06-17 14:33:24 +0100

    Update funcitonal test for Join page

--- functional-tests/src/main/scala/com/gu/membership/pages/JoinPage.scala
+++ functional-tests/src/main/scala/com/gu/membership/pages/JoinPage.scala
@@ -2,15 +2,10 @@ package com.gu.membership.pages
 
 import org.openqa.selenium.{By, WebDriver}
 
-/**
- * Created by jao on 29/05/2014.
- */
 class JoinPage(driver: WebDriver) extends BaseMembershipPage(driver) {
 
-  private def becomeAFriendLink = driver.findElement(By.cssSelector(".qa-package-friend"))
-
+  private def becomeAFriendLink = driver.findElement(By.cssSelector(".qa-friend-join"))
   private def becomeAPartnerLink = driver.findElement(By.cssSelector(".qa-package-partner"))
-
   private def becomeAPatronLink = driver.findElement(By.cssSelector(".qa-package-patron"))
 
   def clickBecomeAFriend = {
