190b9305d74fd1cd27647128afc28c52a56b8242
David Rapson <david.rapson@guardian.co.uk>
2015-07-30 17:20:51 +0100

    PR: Crossbrowser and coding style fixes
    - Model header and brand logos using SVGImage, fixes IE display issues.
    - Some small coding style fixes

--- frontend/app/model/SVG.scala
+++ frontend/app/model/SVG.scala
@@ -13,4 +13,22 @@ object SVG {
     val src = Asset.at(path)
   }
 
+  object Logos {
+    val membershipLogo = SVGImage(
+      "Guardian Membership",
+      "images/logos/membership-logo.svg",
+      width=240, height=83
+    )
+    val guardianLive = SVGImage(
+      "Guardian Live",
+      "images/providers/guardian-live.svg",
+      width=62, height=90
+    )
+    val guardianMasterclasses = SVGImage(
+      "Guardian Masterclasses",
+      "images/providers/masterclasses.svg",
+      width=242, height=88
+    )
+  }
+
 }
