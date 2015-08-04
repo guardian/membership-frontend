bd4bc880e62542115ab38292762938054f4691c2
David Rapson <david.rapson@guardian.co.uk>
2015-08-04 16:41:07 +0100

    Move CSP out of def context

--- frontend/app/controllers/Security.scala
+++ frontend/app/controllers/Security.scala
@@ -12,43 +12,43 @@ import scala.concurrent.Future
 
 object Security extends Controller with LazyLogging {
 
+  /**
+   * See: https://developer.mozilla.org/en-US/docs/Web/Security/CSP/Using_CSP_violation_reports
+   * Report format:
+   *
+   *  {
+   *    "csp-report": {
+   *      "document-uri": "http://example.com/signup.html",
+   *      "referrer": "",
+   *      "blocked-uri": "http://example.com/css/style.css",
+   *      "violated-directive": "style-src cdn.example.com",
+   *      "original-policy": "default-src 'none'; style-src cdn.example.com; report-uri /_/csp-reports"
+   *    }
+   *  }
+   */
+  case class CSPReport(
+    documentUri: String,
+    blockedUri: String,
+    violatedDirective: String,
+    referrer: Option[String]
+  ) {
+    val message = s"""
+      |CSP warning:
+      |Document URI: $documentUri
+      |Blocked URI: $blockedUri
+      |Violated Directive: $violatedDirective
+      |Referrer: $referrer
+      |""".stripMargin
+  }
+
+  implicit val reads: Reads[CSPReport] = (
+    (JsPath \ "csp-report" \ "document-uri").read[String] and
+    (JsPath \ "csp-report" \ "blocked-uri").read[String] and
+    (JsPath \ "csp-report" \ "violated-directive").read[String] and
+    (JsPath \ "csp-report" \ "referrer").readNullable[String]
+  )(CSPReport.apply _)
+
   def cspReport = NoCacheAction.async(parse.tolerantJson(maxLength = 4096)) { implicit request =>
-
-    /**
-     * See: https://developer.mozilla.org/en-US/docs/Web/Security/CSP/Using_CSP_violation_reports
-     * Report format:
-     *
-     *  {
-     *    "csp-report": {
-     *      "document-uri": "http://example.com/signup.html",
-     *      "referrer": "",
-     *      "blocked-uri": "http://example.com/css/style.css",
-     *      "violated-directive": "style-src cdn.example.com",
-     *      "original-policy": "default-src 'none'; style-src cdn.example.com; report-uri /_/csp-reports"
-     *    }
-     *  }
-     */
-    case class CSPReport(
-      documentUri: String,
-      blockedUri: String,
-      violatedDirective: String,
-      referrer: Option[String]
-    ) {
-      val message = s"""
-        |CSP warning:
-        |Document URI: $documentUri
-        |Blocked URI: $blockedUri
-        |Violated Directive: $violatedDirective
-        |Referrer: $referrer
-        |""".stripMargin
-    }
-
-    implicit val reads: Reads[CSPReport] = (
-      (JsPath \ "csp-report" \ "document-uri").read[String] and
-      (JsPath \ "csp-report" \ "blocked-uri").read[String] and
-      (JsPath \ "csp-report" \ "violated-directive").read[String] and
-      (JsPath \ "csp-report" \ "referrer").readNullable[String]
-    )(CSPReport.apply _)
 
     val report = request.body.as[CSPReport]
 
