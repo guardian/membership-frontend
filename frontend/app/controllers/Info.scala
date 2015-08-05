e8b5bbdb570fdf74bd3e4a1e640daa75599dd483
David Rapson <david.rapson@guardian.co.uk>
2015-08-05 16:23:09 +0100

    Supporter USA, update lead image

--- frontend/app/controllers/Info.scala
+++ frontend/app/controllers/Info.scala
@@ -107,9 +107,12 @@ trait Info extends Controller {
     val pageImages = Seq(
       ResponsiveImageGroup(
         name=Some("intro"),
-        altText=Some("Michael Brown vigil"),
+        altText=Some("""People take part in a demonstration in Times Square, New York,
+          |following Monday's grand jury decision in the shooting of Michael Brown in
+          |Ferguson, Missouri, November 27, 2014.
+        """.stripMargin),
         availableImages=ResponsiveImageGenerator(
-          id="0a6d77047c2b5d6f82b6674a3054286e872c9fab/0_331_5760_3457",
+          id="8eea3b3bd80eb2f8826b1cef75799d27a11e56e5/293_409_3206_1924",
           sizes=List(1000,500)
         )
       ),
@@ -123,7 +126,7 @@ trait Info extends Controller {
       ),
       ResponsiveImageGroup(
         name=Some("join"),
-        altText=Some("Kath Viner, editor-in-chief of the Guardian"),
+        altText=Some("Katharine Viner, editor-in-chief of the Guardian"),
         availableImages=ResponsiveImageGenerator(
           id="e2e62954254813fd6781952a56f18bf20343ed0a/0_0_2000_1200",
           sizes=List(1000, 500)
