235ebeb217f4c5059a7155a6a878f08988035638
Andrea Fiore <andrea.fiore@composable.io>
2015-08-13 11:35:52 +0100

    start defining some json readers for Zuora REST API

--- frontend/app/services/zuora/ZuoraRestService.scala
+++ frontend/app/services/zuora/ZuoraRestService.scala
@@ -4,9 +4,9 @@ import com.gu.lib.okhttpscala._
 import com.gu.membership.util.Timing
 import com.gu.membership.zuora.ZuoraApiConfig
 import com.gu.monitoring.{AuthenticationMetrics, StatusMetrics}
-import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.{Response, OkHttpClient}
 import com.squareup.okhttp.Request.Builder
-import model.Zuora.Feature
+import model.Zuora.{Subscription, Feature}
 import monitoring.TouchpointBackendMetrics
 import play.api.libs.concurrent.Execution.Implicits._
 
@@ -16,6 +16,21 @@ import play.api.libs.json.Reads._
 import play.api.libs.functional.syntax._
 
 import scala.util.{Failure, Success, Try}
+
+object ZuoraRestService {
+  sealed trait RestResponse[+T] {
+    def isSuccess: Boolean
+  }
+  case class RestSuccess[T](value: T) extends RestResponse[T] {
+    override def isSuccess = true
+  }
+
+  case class RestError(processId: String, reasons: Seq[ErrorMsg]) extends RestResponse[Nothing] {
+    override def isSuccess = false
+  }
+
+  case class ErrorMsg(code: String, msg: String)
+}
 
 class ZuoraRestService(config: ZuoraApiConfig) {
 
@@ -32,12 +47,14 @@ class ZuoraRestService(config: ZuoraApiConfig) {
     }
   }
 
-  private def get(uri: String) = client.execute(new Builder()
-    .addHeader("apiAccessKeyId", config.username)
-    .addHeader("apiSecretAccessKey", config.password)
-    .addHeader("Accept", "application/json")
-    .url(s"${config.url}/$uri")
-    .get().build())
+  def subscriptionsByAccount(accountKey: String): Future[Seq[Subscription]] =
+    Timing.record(metrics, "subscriptionsByAccount") {
+      get(s"subscriptions/accounts/$accountKey").map { response =>
+        metrics.putResponseCode(response.code, "GET")
+        None
+        ???
+      }
+    }
 
   /**
    * @see https://knowledgecenter.zuora.com/BC_Developers/REST_API/B_REST_API_reference/Subscriptions/4_Get_subscriptions_by_account
@@ -56,14 +73,37 @@ class ZuoraRestService(config: ZuoraApiConfig) {
         }
       }
     }
+
+
+  private def get(uri: String): Future[Response] = client.execute(new Builder()
+    .addHeader("apiAccessKeyId", config.username)
+    .addHeader("apiSecretAccessKey", config.password)
+    .addHeader("Accept", "application/json")
+    .url(s"${config.url}/$uri")
+    .get().build())
 }
 
 object ZuoraRestResponseReaders {
+  import ZuoraRestService._
+
+  implicit val errorMsgReads: Reads[ErrorMsg] = Json.reads[ErrorMsg]
+
+  def parseResponse[T](body: String)(implicit ev: Reads[T]): RestResponse[T] = {
+    val json = Json.parse(body)
+    val isSuccess = (json \ "success").as[Boolean]
+    if (isSuccess) RestSuccess(json.as[T]) else json.as[RestError]
+  }
+
+  implicit val errorReads: Reads[RestError] =
+    ((JsPath \ "processId").read[String] and
+      (JsPath \ "errors").read[Seq[ErrorMsg]])(RestError.apply _)
 
   implicit val featureReads: Reads[Feature] = (
     (JsPath \ "id").read[String] and
       (JsPath \ "featureCode").read[String]
     )(Feature.apply _)
+
+  implicit val subscriptionReads: Reads[Subscription] = Json.reads[Subscription]
 
   def extractFeatures(responseBody: String): Seq[Feature] =
     (Json.parse(responseBody) \\ "subscriptionProductFeatures")
--- frontend/test/services/zuora/ZuoraRestResponseReadersTest.scala
+++ frontend/test/services/zuora/ZuoraRestResponseReadersTest.scala
@@ -4,6 +4,69 @@ import model.Zuora.Feature
 import org.specs2.mutable.Specification
 
 class ZuoraRestResponseReadersTest extends Specification {
+  "parseResponde" should {
+    "return a RestSuccess object when the response is successful" in {
+      """
+        |{
+        |  "success": true,
+        |  "nextPage": "https://api.zuora.com/rest/v1/subscriptions/accounts/A00001115?page=2&pageSize=10",
+        |  "subscriptions": [
+        |    {
+        |      "termStartDate": "2013-02-01",
+        |      "subscriptionNumber": "A-S00001081",
+        |      "accountNumber": "A00001115",
+        |      "totalContractedValue": 400.0,
+        |      "accountName": "subscribeCallYan_1",
+        |      "ratePlans": [
+        |        {
+        |          "ratePlanName": "QSF_Tier",
+        |          "productName": "Recurring Charge",
+        |          "ratePlanCharges": [
+        |            {
+        |              "name": "TieredPrice",
+        |              "listPriceBase": "Per_Month",
+        |              "billingPeriod": "Quarter",
+        |              "model": "Tiered",
+        |              "type": "Recurring",
+        |              "pricingSummary": "0 to 10 ONE_DOWN: USD10/ONE_DOWN;  10.1 to 20 ONE_DOWN: USD20/ONE_DOWN;  20.1 to 30 ONE_DOWN: USD30/ONE_DOWN;  30.1 to 40 ONE_DOWN: USD40/ONE_DOWN;  40.1 ONE_DOWN or more: USD50/ONE_DOWN",
+        |              "quantity": 10.0,
+        |              "billingDay": "DefaultFromCustomer",
+        |              "id": "2c92c8f83dc4f752013dc72c24d9016c"
+        |            },
+        |            {
+        |              "name": "TieredPrice Prepayment Charge",
+        |              "billingPeriod": null,
+        |              "model": "FlatFee",
+        |              "type": "OneTime",
+        |              "pricingSummary": "USD200",
+        |              "quantity": null,
+        |              "billingDay": "DefaultFromCustomer",
+        |              "id": "2c92c8f83dc4f752013dc72c252f0171"
+        |            }
+        |          ],
+        |          "id": "subscription-id"
+        |        }
+        |      ],
+        |      "status": "Active",
+        |      "renewalTerm": 3,
+        |      "accountId": "2c92a0f9391832b10139183e277a0042",
+        |      "autoRenew": true,
+        |      "termType": "TERMED",
+        |      "termEndDate": "2014-02-01",
+        |      "contractEffectiveDate": "2013-02-01",
+        |      "notes": "Test POST subscription from z-ruby-sdk",
+        |      "initialTerm": 12,
+        |      "subscriptionStartDate": "2013-02-01",
+        |      "id": "2c92c8f83dc4f752013dc72c24ee016d"
+        |    },
+        |  ]
+        |}
+        |
+      """.stripMargin
+
+    }
+
+  }
   "extractFeatures" should {
 
     "correctly parse subscriptions" in {
