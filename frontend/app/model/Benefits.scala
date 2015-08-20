989bb4fc41b5d1fd5a3ce208ad0b3e123bcb8034
David Rapson <david.rapson@guardian.co.uk>
2015-08-20 17:37:24 +0100

    Simplify Benfits model

--- frontend/app/model/Benefits.scala
+++ frontend/app/model/Benefits.scala
@@ -15,7 +15,7 @@ object Benefits {
     desc: String
   )
 
-  case class BenefitItem(identifier: String, title: String, description: String, icon: String)
+  case class BenefitItem(identifier: String, title: String)
 
   case class ComparisonItem(
     description: String,
@@ -24,17 +24,14 @@ object Benefits {
   )
 
   val allBenefits = Seq(
-    BenefitItem("book_tickets", "Book tickets", "Book tickets to Guardian Live events", "benefit-booking"),
-    BenefitItem("digital_digest", "Membership email updates", "Receive regular updates from the membership community", "benefit-digest"),
-    BenefitItem("video_highlights", "Video highlights", "Watch highlights of selected Guardian Live events", "benefit-video"),
-    BenefitItem("priority_booking", "Priority booking", "Priority booking on Guardian Live Events (before Friends and Supporters)", "benefit-priority-booking"),
-    BenefitItem("discount", "20% off live events", "20% discount on Guardian Live tickets", "benefit-live-discount"),
-    BenefitItem("discount_masterclasses", "20% off masterclasses", "20% discount on Guardian Masterclasses", "benefit-masterclasses-discount"),
-    BenefitItem("membership_card", "Membership card", "Membership card and annual gift", "benefit-card"),
-    BenefitItem("plus_1_guest", "+1 guest", "Bring a guest to Guardian Live with the same discount and priority booking advantages", "benefit-plus1"),
-    BenefitItem("live_stream", "Live stream events", "Watch live streams of flagship Guardian Live Membership events", "benefit-stream"),
-    BenefitItem("complim_items", "Special thank-yous", "The occasional unique gift to thank you for your support", "benefit-gifts"),
-    BenefitItem("unique_experiences", "Unique experiences", "Get behind the scenes of our journalism", "benefit-experiences")
+    BenefitItem("welcome_pack", "Welcome pack, card and gift"),
+    BenefitItem("book_tickets", "Access to tickets"),
+    BenefitItem("live_stream", "Access to live stream"),
+    BenefitItem("email_updates", "Regular events email"),
+    BenefitItem("offers_competitions", "Offers and competitions"),
+    BenefitItem("priority_booking", "48hrs priority booking"),
+    BenefitItem("discount", "20% discount for you and a guest"),
+    BenefitItem("unique_experiences", "Exclusive behind-the-scenes functions")
   )
 
   case class Pricing(
@@ -58,57 +55,64 @@ object Benefits {
 
   val friendBenefitsList = benefitsFilter(
     "book_tickets",
-    "digital_digest",
-    "video_highlights"
+    "offers_competitions",
+    "email_updates"
   )
   val supporterBenefitsList = benefitsFilter(
-    "membership_card",
+    "welcome_pack",
     "live_stream",
-    "video_highlights",
-    "digital_digest",
-    "book_tickets"
+    "book_tickets",
+    "offers_competitions",
+    "email_updates"
   )
   val partnerBenefitsList = benefitsFilter(
     "priority_booking",
-    "plus_1_guest",
-    "live_stream",
     "discount",
-    "discount_masterclasses",
-    "membership_card",
-    "digital_digest",
-    "video_highlights"
+    "welcome_pack",
+    "live_stream",
+    "offers_competitions",
+    "email_updates"
   )
   val patronBenefitsList = benefitsFilter(
-    "complim_items",
-    "unique_experiences"
-  ) ++ partnerBenefitsList
+    "unique_experiences",
+    "priority_booking",
+    "discount",
+    "welcome_pack",
+    "live_stream",
+    "offers_competitions",
+    "email_updates"
+  )
 
   val friendBenefits = Benefits("Benefits",
     friendBenefitsList,
     None,
     "Become a Friend",
-    "Receive regular updates from the membership community, book tickets to Guardian Live events and access the Guardian members area"
+    """Receive regular updates from the membership community,
+      | book tickets to Guardian Live events and access the Guardian members area""".stripMargin
   )
   val supporterBenefits = Benefits(
     "Benefits",
     supporterBenefitsList,
     Some(Pricing(50, 5, Some("1 year membership, 2 months free"))),
     "Become a Supporter",
-    "Supporters keep our journalism fearless, open and free from interference."
+    """Support our journalism and keep theguardian.com free of charge.
+    | Get access to tickets and to the live broadcast of events""".stripMargin
   )
   val partnerBenefits = Benefits(
     "Supporter benefits, plus…",
     partnerBenefitsList,
     Some(Pricing(135, 15, Some("1 year membership, 3 months free"))),
     "Become a Partner",
-    "Support the Guardian and experience it brought to life, with priority booking and discounted tickets"
+    """Get priority booking and 20% discount on Guardian Live,
+      | Guardian Local and most Guardian Masterclasses.""".stripMargin
   )
   val patronBenefits = Benefits(
     "Partner benefits, plus…",
     patronBenefitsList,
     Some(Pricing(540, 60, Some("1 year membership, 3 months free"))),
     "Become a Patron",
-    "Defend the Guardian’s independence and promote the open exchange of ideas, with a backstage pass to the Guardian"
+    """Show deep support for keeping the Guardian free, open and independent.
+    | Get invited to a small number of exclusive, behind-the-scenes functions""".stripMargin
   )
 
   def details(tier: Tier) = tier match {
