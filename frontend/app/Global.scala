fe9ec210e8b40c250f6029243f185b1fd9f711e0
David Rapson <david.rapson@guardian.co.uk>
2015-09-03 17:59:46 +0100

    Remove CSP header
    We've been running a report-only Content-Security-Policy header on
    Membership for the last few weeks. Unfortunately it's pretty hard to
    meaningfully determine a safe-whitelist for the policy as the
    signal-to-noise ratio on reports is pretty low. We could monitor this
    in a smarter way over time (i.e. not using Sentry) but at our current
    scale I don't think we get enough benefit out of CSP to be worth the
    extra effort. Sadly removing this for now.

--- frontend/app/Global.scala
+++ frontend/app/Global.scala
@@ -5,7 +5,7 @@ import play.api.mvc.WithFilters
 import play.filters.csrf._
 import services._
 
-object Global extends WithFilters(RedirectMembersFilter, CheckCacheHeadersFilter, CSRFFilter(), Gzipper, AddCSPHeader, AddEC2InstanceHeader) {
+object Global extends WithFilters(RedirectMembersFilter, CheckCacheHeadersFilter, CSRFFilter(), Gzipper, AddEC2InstanceHeader) {
   override def onStart(app: Application) {
     SentryLogging.init()
 
