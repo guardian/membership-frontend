2af06fccab74395c1efc0bfe75a3de6f8b643197
Joseph Smith <joseph.smith@guardian.co.uk>
2015-09-25 11:59:22 +0100

    Change video placeholder image to match new video

--- frontend/app/configuration/Videos.scala
+++ frontend/app/configuration/Videos.scala
@@ -5,8 +5,9 @@ import model.{Video, ResponsiveImageGroup, ResponsiveImageGenerator}
 
 object Videos {
 
-  private val videoPlaceholder = ResponsiveImageGenerator(
-    id="267569fecb462c61718f7e8cf50a8995ebddee5d/0_0_2280_1368",
+  private val whatIsMembershipPlaceholder = ResponsiveImageGenerator(
+    // ff2fdc0b1cd540445ab0c57b3efe8f08eba2e213
+    id="0f0b186b9acd3c32ae47e445e07fe5b128250d7f/0_0_1800_1080",
     sizes=List(1000, 500)
   )
 
@@ -15,9 +16,14 @@ object Videos {
     posterImage=Some(
       ResponsiveImageGroup(
         altText=Some("What is Guardian Members?"),
-        availableImages=videoPlaceholder
+        availableImages=whatIsMembershipPlaceholder
       )
     )
+  )
+
+  private val supportersPlaceholder = ResponsiveImageGenerator(
+    id="267569fecb462c61718f7e8cf50a8995ebddee5d/0_0_2280_1368",
+    sizes=List(1000, 500)
   )
 
   val supporters = Video(
@@ -25,7 +31,7 @@ object Videos {
     posterImage=Some(
       ResponsiveImageGroup(
         altText=Some("If you read the Guardian, join the Guardian"),
-        availableImages=videoPlaceholder
+        availableImages=supportersPlaceholder
       )
     )
   )
