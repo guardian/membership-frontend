1b5ac215c93c5f1d3db59f664e9033892108d437
tverran <tom.verran@guardian.co.uk>
2015-10-07 16:27:00 +0100

    Log raven API calls made on dev machines to the console

--- frontend/assets/javascripts/src/modules/raven.js
+++ frontend/assets/javascripts/src/modules/raven.js
@@ -15,15 +15,20 @@ define(['src/utils/user', 'raven'], function (user) {
          * Set up Raven, which speaks to Sentry to track errors
          */
         Raven.config(dsn, {
-            whitelistUrls: [ /membership\.theguardian\.com/ ],
+            whitelistUrls: [ /membership\.theguardian\.com/, /mem\.thegulocal\.com/, /localhost/ ],
             tags: tags,
             ignoreErrors: [ /duplicate define: jquery/ ],
-            ignoreUrls: [ /platform\.twitter\.com/ ]
+            ignoreUrls: [ /platform\.twitter\.com/ ],
+            shouldSendCallback: function(data) {
+                if(window.guardian.isDev) {
+                    console.log('Raven', data);
+                }
+                return !window.guardian.isDev;
+            }
         }).install();
     }
 
     return {
         init: init
     };
-
 });
