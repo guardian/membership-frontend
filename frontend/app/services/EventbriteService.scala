196d9d24fcfff530e5c52e76bd182d64d79ba459
Chris J Owen <chris.j.owen@guardian.co.uk>
2015-10-26 11:59:37 +0000

    Events for non members show prices with fees if passed forward

--- frontend/app/services/EventbriteService.scala
+++ frontend/app/services/EventbriteService.scala
@@ -162,7 +162,8 @@ object MasterclassEventsProvider {
     _.internalTicketing.exists(_.memberDiscountOpt.exists(!_.isSoldOut))
 }
 
-object MasterclassEventService extends EventbriteService {
+object
+MasterclassEventService extends EventbriteService {
   import MasterclassEventsProvider._
 
   val apiToken = Config.eventbriteMasterclassesApiToken
