fdc6597552f7152eca56b47a597a2862cdcc0978
Chris J Owen <chris.j.owen@guardian.co.uk>
2015-12-07 17:25:39 +0000

    Homepage restyling

--- frontend/assets/javascripts/src/modules/comparisonTable.js
+++ frontend/assets/javascripts/src/modules/comparisonTable.js
@@ -18,39 +18,31 @@ define([
         }
 
         addHoverListeners();
-        addClickListeners();
     }
 
     function tierSelector(tier) {
-        return '[data-tier="'+tier+'"]';
+        return '[data-tier="' + tier + '"]';
     }
 
     // We have to do this in JavaScript rather than using a CSS :hover class
     // because we have no single container which holds a tier column.
     // Instead they are split between rows, so we use data-tier to group them together.
     function addHoverListeners() {
-        bean.on(COMPARISON_TABLE, 'mouseenter', CLICKABLE, function(e) {
+        bean.on(COMPARISON_TABLE, 'mouseenter', CLICKABLE, function (e) {
             var tier = $(e.currentTarget).data('tier');
             var $elemsInThisTier = $(CLICKABLE + tierSelector(tier));
+            var $elemsInOtherTiers = $(CLICKABLE + ':not(' + tierSelector(tier) + ')');
 
             $elemsInThisTier.addClass(HOVER_CLASS);
+            $elemsInThisTier.addClass(ACTIVE_CLASS);
+            $elemsInOtherTiers.removeClass(ACTIVE_CLASS);
         });
 
-        bean.on(COMPARISON_TABLE, 'mouseleave', CLICKABLE, function() {
+        bean.on(COMPARISON_TABLE, 'mouseleave', CLICKABLE, function () {
             $(HOVERED).removeClass(HOVER_CLASS);
         });
     }
 
-    function addClickListeners() {
-        bean.on(COMPARISON_TABLE, 'click', CLICKABLE, function(e) {
-            var tier = $(e.currentTarget).data('tier');
-            var $elemsInThisTier = $(CLICKABLE + tierSelector(tier));
-            var $elemsInOtherTiers = $(CLICKABLE+':not('+tierSelector(tier)+')');
-
-            $elemsInThisTier.addClass(ACTIVE_CLASS);
-            $elemsInOtherTiers.removeClass(ACTIVE_CLASS);
-        });
-    }
 
     return {
         init: init
