b8adcf18555e5147c68ef4effb471688c7fb4e9c
Mario Galic <mario.galic@guardian.co.uk>
2015-12-08 17:19:30 +0000

    Remove old functional tests

--- functional-tests/src/main/scala/com/gu/membership/pages/JoinFlowChooseTierPage.scala
+++
@@ -1,5 +0,0 @@
-package com.gu.membership.pages
-
-import org.openqa.selenium.WebDriver
-
-class JoinFlowChooseTierPage(driver: WebDriver) extends LandingPage(driver) {}
--- functional-tests/src/main/scala/com/gu/membership/pages/PaymentPage.scala
+++
@@ -1,8 +0,0 @@
-package com.gu.membership.pages
-
-import org.openqa.selenium.WebDriver
-
-class PaymentPage(driver: WebDriver) extends BaseMembershipPage(driver) {
-
-  val cardWidget = new CreditCardWidget(driver)
-}
