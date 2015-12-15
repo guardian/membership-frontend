2c1c58aaa7803a3ed0463210ed05acadf4e01952
Thomas Franquelin <ostapneko@gmail.com>
2015-12-15 10:22:23 +0000

    Remove obsolete update card endpoint

--- frontend/app/controllers/Subscription.scala
+++ frontend/app/controllers/Subscription.scala
@@ -1,7 +1,5 @@
 package controllers
 
-import com.gu.membership.stripe.Stripe
-import com.gu.membership.stripe.Stripe.Serializer._
 import model.FreeEventTickets
 import play.api.data.Forms._
 import play.api.data._
@@ -9,20 +7,7 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json.Json
 import play.api.mvc._
 
-import scala.concurrent.Future
-
 trait Subscription extends Controller with MemberServiceProvider {
-  def updateCard() = AjaxPaidMemberAction.async { implicit request =>
-    updateForm.bindFromRequest
-      .fold(_ => Future.successful(BadRequest), stripeToken =>
-        for {
-          card <- memberService.updateDefaultCard(request.member, stripeToken)
-        } yield Ok(Json.obj("last4" -> card.last4, "cardType" -> card.`type`, "type" -> card.`type`))
-      ).recover {
-        case error: Stripe.Error => Forbidden(Json.toJson(error))
-      }
-  }
-
   def remainingTickets() = AjaxPaidMemberAction.async { implicit request =>
     for {
       subscription <- memberService.currentSubscription(request.member)
@@ -34,8 +19,6 @@ trait Subscription extends Controller with MemberServiceProvider {
       ))
     }
   }
-
-  def updateCardPreflight() = Cors.andThen(CachedAction) { Ok.withHeaders(ACCESS_CONTROL_ALLOW_HEADERS -> "Csrf-Token") }
 
   private val updateForm = Form { single("stripeToken" -> nonEmptyText) }
 }
