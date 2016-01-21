ae5692a24d3e7372310f524e4cc7476653e7b0d9
Paul Brown <paul.brown@guardian.co.uk>
2016-01-21 12:48:51 +0000

    Fixing bug where analytics did not work on Firefox, because navigator.doNotTrack === 'unspecified' so prefixing ! returned true rather than false. Chrome returns 'null' for that value. Also fixed that IE and Safari use window.doNotTrack rather than navigator.

--- frontend/assets/javascripts/src/modules/analytics/setup.js
+++ frontend/assets/javascripts/src/modules/analytics/setup.js
@@ -18,9 +18,19 @@ define([
 ) {
     'use strict';
 
+    /*
+     Re: https://bugzilla.mozilla.org/show_bug.cgi?id=1023920#c2
+
+     The landscape at the moment is:
+
+     On navigator [Firefox, Chrome, Opera]
+     On window [IE, Safari]
+     */
+    var isDNT = navigator.doNotTrack == '1' || window.doNotTrack == '1';
+
     var analyticsEnabled = (
         guardian.analyticsEnabled &&
-        !navigator.doNotTrack &&
+        !isDNT &&
         !cookie.getCookie('ANALYTICS_OFF_KEY')
     );
 
