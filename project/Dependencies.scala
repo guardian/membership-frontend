addedea3ad905ad07c312f225256a29576453333
Roberto Tyley <roberto.tyley@theguardian.com>
2016-02-04 16:50:10 +0000

    Bump to latest version of mem-common, and associated versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,13 +4,13 @@ import play.sbt.PlayImport
 object Dependencies {
 
   //versions
-  val awsClientVersion = "1.9.30"
+  val awsClientVersion = "1.10.50"
   //libraries
   val sentryRavenLogback = "net.kencochrane.raven" % "raven-logback" % "6.0.0"
   val identityPlayAuth = "com.gu.identity" %% "identity-play-auth" % "0.13"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.5"
   val scalaUri = "com.netaporter" %% "scala-uri" % "0.4.6"
-  val membershipCommon = "com.gu" %% "membership-common" % "0.148"
+  val membershipCommon = "com.gu" %% "membership-common" % "0.152"
   val playGoogleAuth = "com.gu" %% "play-googleauth" % "0.3.3"
   val contentAPI = "com.gu" %% "content-api-client" % "6.4"
   val playWS = PlayImport.ws
