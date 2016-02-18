3ff1c3045bac73daa50fe5c7fc07573b4928021d
Roberto Tyley <roberto.tyley@theguardian.com>
2016-02-18 10:53:39 +0000

    Use play-googleauth library's new Google-Group filtering
    https://github.com/guardian/play-googleauth/pull/36

--- frontend/app/controllers/Testing.scala
+++ frontend/app/controllers/Testing.scala
@@ -1,10 +1,12 @@
 package controllers
 
+import actions.Fallbacks._
+import actions._
 import com.typesafe.scalalogging.LazyLogging
 import play.api.mvc.{Controller, Cookie}
-import play.twirl.api.Html
 import utils.TestUsers.testUsers
-import actions.ActionRefiners._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Testing extends Controller with LazyLogging {
 
@@ -13,19 +15,16 @@ object Testing extends Controller with LazyLogging {
   val analyticsOffCookie = Cookie(AnalyticsCookieName, "true", httpOnly = false)
 
   /**
-   * Make sure to use cannonical @guardian.co.uk for addresses
+   * Make sure to use canonical @guardian.co.uk for addresses
    */
-  val AuthorisedTester = GoogleAuthenticatedStaffAction andThen isInAuthorisedGroupGoogleAuthReq(
-    Set(
+  val AuthorisedTester = GoogleAuthenticatedStaffAction andThen OAuthActions.requireGroup[GoogleAuthRequest](Set(
       "membership.dev@guardian.co.uk",
       "membership.team@guardian.co.uk",
       "dig.dev.web-engineers@guardian.co.uk",
       "membership.testusers@guardian.co.uk",
       "touchpoint@guardian.co.uk",
       "crm@guardian.co.uk"
-    ),
-    views.html.fragments.oauth.staffWrongGroup()
-  )
+    ), unauthorisedStaff(views.html.fragments.oauth.staffWrongGroup())(_))
 
   def testUser = AuthorisedTester { implicit request =>
     val testUserString = testUsers.generate()
