30b3c5fed4ebdade78858e2f2ff25c3eb8a3c079
agulowaty <artur.gulowaty@guardian.co.uk>
2016-03-03 16:35:33 +0000

    * exposed endpoint returning rate plans for membership

--- frontend/app/controllers/PricingApi.scala
+++ frontend/app/controllers/PricingApi.scala
@@ -1,12 +1,18 @@
 package controllers
 
 import com.gu.i18n._
-import play.api.libs.json.{JsString, JsValue, Writes, Json}
+import com.gu.membership.MembershipCatalog
+import com.gu.salesforce.PaidTier
+import play.api.libs.json.{JsArray, JsString, JsValue, Writes, Json}
 import play.api.mvc.Controller
-import views.support.CountryWithCurrency
+import services.TouchpointBackend
+import views.support.{Pricing, CountryWithCurrency}
 
-object CountryWithCurrencyResponse {
-  implicit val countryWrites = Json.writes[Country]
+case class MembershipPlan(tier: PaidTier, prices: List[Pricing])
+
+case class MembershipPlanResponse(plans: List[MembershipPlan])
+
+object PricingFormats {
   implicit val currencyWrites = new Writes[Currency] {
     override def writes(currency: Currency): JsValue = currency match {
       case GBP => JsString("GBP")
@@ -17,13 +23,50 @@ object CountryWithCurrencyResponse {
       case USD => JsString("USD")
     }
   }
+  implicit val countryWrites = Json.writes[Country]
   implicit val countryCurrencyWrites = Json.writes[CountryWithCurrency]
+
+  implicit val pricingsWrites = new Writes[List[Pricing]] {
+    override def writes(pricings: List[Pricing]): JsValue = {
+      JsArray(
+        pricings
+          .flatMap { pricing => Seq("monthly" -> pricing.monthly, "annually" -> pricing.yearly) }
+          .map { case (bp, price) =>
+            Json.obj(
+              "billingPeriod" -> bp,
+              "amount" -> f"${ price.amount }%.2f",
+              "currency" -> price.currency
+            )
+          }
+      )
+    }
+  }
+
+  implicit val tierWrites = new Writes[PaidTier] {
+    override def writes(tier: PaidTier): JsValue = JsString(tier.name)
+  }
+  implicit val planWrites = Json.writes[MembershipPlan]
+  implicit val writes = Json.writes[MembershipPlanResponse]
 }
 
 object PricingApi extends Controller {
-  import CountryWithCurrencyResponse._
+
+  import PricingFormats._
+  import views.support.Pricing._
+
+  val membersCatalog: MembershipCatalog = TouchpointBackend.Normal.catalog
+
   def currencies = CachedAction {
     Ok(Json.toJson(CountryWithCurrency.all))
+  }
+
+  def ratePlans = CachedAction {
+    val plansByTier = for {
+      tier <- PaidTier.all
+      plan = membersCatalog.findPaid(tier)
+    } yield MembershipPlan(plan.tier, plan.allPricing)
+
+    Ok(Json.toJson(MembershipPlanResponse(plansByTier.toList)))
   }
 }
 
