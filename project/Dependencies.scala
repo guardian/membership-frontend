204a439ad92be464ee004057b616f178487a4930
agulowaty <artur.gulowaty@guardian.co.uk>
2016-03-16 11:22:51 +0000

    dependency syntax fixed

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val scalaTest =  "org.scalatestplus" %% "play" % "1.4.0-M4" % "test"
   val scalaz = "org.scalaz" %% "scalaz-core" % "7.1.1"
   val selenium = "org.seleniumhq.selenium" % "selenium-java" % "2.48.2" % "test"
-  val specs2Extra = "org.specs2" % "specs2-matcher-extra_2.11" % "3.6" % "test"
+  val specs2Extra = "org.specs2" %% "specs2-matcher-extra" % "3.6" % "test"
 
   //projects
 
