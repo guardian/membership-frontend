f03e57cfa45cf74753c400114280b7c22a980a01
Joseph Smith <joseph.smith@guardian.co.uk>
2016-03-30 12:58:39 +0100

    First draft of Giraffe frontend

---
+++ frontend/app/controllers/Giraffe.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import com.gu.i18n.CountryGroup
+import com.gu.i18n.CountryGroup._
+import configuration.CopyConfig
+import controllers.Redirects.redirectToSupporterPage
+import forms.MemberForm._
+import model._
+import play.api.mvc.Controller
+import services.{AuthenticationService, EmailService, GuardianContentService, TouchpointBackend}
+import views.support.{Asset, PageInfo}
+import com.netaporter.uri.dsl._
+import com.netaporter.uri.Uri
+import scala.concurrent.Future
+
+object Giraffe extends Controller {
+  def support = CachedAction { implicit request =>
+    Ok(views.html.giraffe.support())
+  }
+}
