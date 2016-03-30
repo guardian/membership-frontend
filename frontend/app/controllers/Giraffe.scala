336132eeab58cb9d019dc0e7bc5d36d4d3a48e1f
Joseph Smith <joseph.smith@guardian.co.uk>
2016-03-30 16:54:28 +0100

    Main image

--- frontend/app/controllers/Giraffe.scala
+++ frontend/app/controllers/Giraffe.scala
@@ -1,21 +1,34 @@
 package controllers
 import com.gu.stripe.Stripe
+import model.{ResponsiveImageGenerator, ResponsiveImageGroup}
 import play.api.libs.json.{JsString, JsArray, Json}
 import play.api.mvc.{Result, Controller}
 import play.api.libs.concurrent.Execution.Implicits._
 import com.gu.stripe.Stripe.Serializer._
 import forms.MemberForm.supportForm
 import services.TouchpointBackend
+import views.support.PageInfo
 import scala.concurrent.Future
+
 
 object Giraffe extends Controller {
 
   def support = CachedAction { implicit request =>
-    Ok(views.html.giraffe.support(PageInfo(
-        title = "Support",
-        url = request.path,
-        description = Some("Support the Guardian")
-    )))
+    val pageInfo = PageInfo(
+      title = "Support",
+      url = request.path,
+      description = Some("Support the Guardian")
+    )
+    val img = ResponsiveImageGroup(
+      name = Some("intro"),
+      altText = Some("Patrons of the Guardian"),
+      availableImages = ResponsiveImageGenerator(
+        id = "8caacf301dd036a2bbb1b458cf68b637d3c55e48/0_0_1140_683",
+        sizes = List(1000, 500)
+      )
+    )
+
+    Ok(views.html.giraffe.support(pageInfo, img))
   }
 
   def pay = NoCacheAction.async { implicit request =>
