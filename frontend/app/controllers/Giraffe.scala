6649367beef2e22970a8987c6b70e08780c66d99
tverran <tom.verran@guardian.co.uk>
2016-03-30 16:30:22 +0100

    Capture identity IDs if available

--- frontend/app/controllers/Giraffe.scala
+++ frontend/app/controllers/Giraffe.scala
@@ -5,7 +5,7 @@ import play.api.mvc.{Result, Controller}
 import play.api.libs.concurrent.Execution.Implicits._
 import com.gu.stripe.Stripe.Serializer._
 import forms.MemberForm.supportForm
-import services.TouchpointBackend
+import services.{AuthenticationService, TouchpointBackend}
 import views.support.PageInfo
 import scala.concurrent.Future
 
@@ -25,13 +25,11 @@ object Giraffe extends Controller {
     supportForm.bindFromRequest().fold[Future[Result]]({ withErrors =>
       Future.successful(BadRequest(JsArray(withErrors.errors.map(k => JsString(k.key)))))
     },{ f =>
-
       val metadata = Map(
         "marketing-opt-in" -> f.marketing.toString,
         "email" -> f.email,
         "name" -> f.name
-      )
-
+      ) ++ AuthenticationService.authenticatedUserFor(request).map("idUser" -> _.user.id)
       val res = stripe.Charge.create((f.amount * 100).toInt, f.currency, f.email, "Giraffe", f.token, metadata)
       res.map(_ => Ok(Json.obj("success" -> true))).recover { case e: Stripe.Error => BadRequest(Json.toJson(e))}
     })
