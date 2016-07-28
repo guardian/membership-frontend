71b1828bca1bb39245fc4fda724640c7fc63b64f
tverran <tom.verran@guardian.co.uk>
2016-07-28 10:42:25 +0100

    Use membership common 246

--- frontend/app/model/CapiContent.scala
+++ frontend/app/model/CapiContent.scala
@@ -1,14 +1,14 @@
 package model
 
 import com.gu.contentapi.client.model.v1.{Content, Tag}
-import com.gu.memsub.images.ResponsiveImageGroup
+import com.gu.memsub.images.{ResponsiveImageGroup => RIG}
 
-case class CapiContent(headline: String, trailText: Option[String], mainPicture: Option[ResponsiveImageGroup], primaryTag: Tag)
+case class CapiContent(headline: String, trailText: Option[String], mainPicture: Option[RIG], primaryTag: Tag)
 
 object CapiContent {
   def apply(content: Content): CapiContent = {
     CapiContent(content.webTitle, content.fields.flatMap(_.trailText),
-      model.ResponsiveImageGroup.fromContent(content), content.tags.head)
+      ResponsiveImageGroup.fromContent(content), content.tags.head)
   }
 }
 
