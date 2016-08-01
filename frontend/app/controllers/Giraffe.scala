2703485b0dee2bdfd7787ebabde7a18c893360e8
pvighi <patricio.vighi@guardian.co.uk>
2016-08-01 18:10:55 +0100

    added back zero weighted variants and per currency maximum amount

--- frontend/app/controllers/Giraffe.scala
+++ frontend/app/controllers/Giraffe.scala
@@ -28,7 +28,11 @@ object Giraffe extends Controller {
 
 
   val chargeId = "charge_id"
-  val maxAmount: Option[Int] = 2000.some
+
+  def maxAmount(currency: Currency): Option[Int] = currency match {
+    case CountryGroup.Australia.currency => 3500.some
+    case _ => 2000.some
+  }
 
   def contributeRedirect = NoCacheAction { implicit request =>
     val countryGroup = request.getFastlyCountry.getOrElse(CountryGroup.RestOfTheWorld)
@@ -54,7 +58,8 @@ object Giraffe extends Controller {
       navigation = Seq.empty,
       customSignInUrl = Some((Config.idWebAppUrl / "signin") ? ("skipConfirmation" -> "true"))
     )
-    Ok(views.html.giraffe.contribute(pageInfo,maxAmount,countryGroup,isUAT, chosenVariants, cmp, intCmp, CreditCardExpiryYears(java.time.LocalDate.now().getYear, 10)))
+    val maxAmountInLocalCurrency = maxAmount(countryGroup.currency)
+    Ok(views.html.giraffe.contribute(pageInfo,maxAmountInLocalCurrency,countryGroup,isUAT, chosenVariants, cmp, intCmp, CreditCardExpiryYears(java.time.LocalDate.now().getYear, 10)))
       .withCookies(Test.createCookie(chosenVariants.v1), Test.createCookie(chosenVariants.v2))
   }
 
@@ -100,7 +105,8 @@ object Giraffe extends Controller {
         "cmp" -> f.cmp.mkString,
         "intcmp" -> f.intcmp.mkString
       ) ++ AuthenticationService.authenticatedUserFor(request).map("idUser" -> _.user.id) ++ f.postCode.map("postcode" -> _)
-      val res = stripe.Charge.create(maxAmount.fold((f.amount*100).toInt)(max => Math.min(max * 100, (f.amount * 100).toInt)), f.currency, f.email, "Your contribution", f.token, metadata)
+
+      val res = stripe.Charge.create(maxAmount(f.currency).fold((f.amount*100).toInt)(max => Math.min(max * 100, (f.amount * 100).toInt)), f.currency, f.email, "Your contribution", f.token, metadata)
 
       AuthenticationService.authenticatedUserFor(request).map { user =>
         identity.updateUserMarketingPreferences(IdentityRequest(request), user, f.marketing)
--- frontend/app/views/support/ABTest.scala
+++ frontend/app/views/support/ABTest.scala
@@ -64,7 +64,15 @@ object AmountHighlightTest extends TestTrait {
     RestOfTheWorld
   )
   def variants = NonEmptyList(
+    Variant("Amount - 5 highlight","5",0,views.html.fragments.giraffe.contributeAmountButtons(List(5,25,50,100),Some(5))),
+    Variant("Amount - 25 highlight","25",0,views.html.fragments.giraffe.contributeAmountButtons(List(5,25,50,100),Some(25))),
+    Variant("Amount - no highlight","None",0,views.html.fragments.giraffe.contributeAmountButtons(List(5,25,50,100), None)),
+    Variant("Amount - 35 highlight","35",0,views.html.fragments.giraffe.contributeAmountButtons(List(10,35,65,100),Some(35))),
+    Variant("Amount - 35 highlight descending","35-descending",0,views.html.fragments.giraffe.contributeAmountButtons(List(100,65,35,10),Some(35))),
+    Variant("Amount - 100 highlight","100",0,views.html.fragments.giraffe.contributeAmountButtons(List(25,50,100,250),Some(100))),
     Variant("Amount - 50 highlight","50",1,views.html.fragments.giraffe.contributeAmountButtons(List(25,50,100,250),Some(50)), notAustralia),
+    Variant("Amount - 15","15",0,views.html.fragments.giraffe.contributeAmountButtons(List(15,35,65,100),Some(35))),
+    Variant("Amount - 40 highlight","40",0,views.html.fragments.giraffe.contributeAmountButtons(List(20,40,75,100),Some(40))),
     Variant("Amount - 100 highlight Australia", "100-Australia", 1, views.html.fragments.giraffe.contributeAmountButtons(List(50, 100, 250, 500), Some(100)), Set(Australia))
   )
 }
