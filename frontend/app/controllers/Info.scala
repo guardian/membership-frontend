372be1bbf9a140f170ea02bd342eaace72340890
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2016-10-27 16:33:49 +0100

    Added render for wide screen, solved padding issues for section titles and added before and after css issues.

--- frontend/app/controllers/Info.scala
+++ frontend/app/controllers/Info.scala
@@ -45,7 +45,7 @@ trait Info extends Controller {
         byline = None,
         credit = None
       )),
-      availableImages=ResponsiveImageGenerator("dcd0f0f703b1e784a3280438806f2feedf27dfab/0_0_1080_648", Seq(1080, 648))
+      availableImages=ResponsiveImageGenerator("dcd0f0f703b1e784a3280438806f2feedf27dfab/0_0_1080_648", Seq(1080, 1000))
     )
 
     val detailImageOrientated = OrientatedImages(portrait = detailImage, landscape = detailImage)
