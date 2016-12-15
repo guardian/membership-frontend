70cfea2301c7d1bc323e66444a76b05914a0ae95
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2016-12-15 14:17:51 +0000

    Benefits class is refactored.

--- frontend/app/model/Benefits.scala
+++ frontend/app/model/Benefits.scala
@@ -7,6 +7,7 @@ import configuration.Config.zuoraFreeEventTicketsAllowance
 
 
 object Benefits {
+
   def forTier(tier: Tier) = tier match {
     case Staff() => Benefits.staff
     case Friend() => Benefits.friend
@@ -16,58 +17,86 @@ object Benefits {
   }
 
   def promoForCountry(cg: CountryGroup) = cg match {
-    case CountryGroup.Australia => ausSupporter
+    case CountryGroup.Australia => auSupporter
+    case CountryGroup.UK => ukSupporter
+    case CountryGroup.US => usSupporter
     case _ => supporterMinimal
   }
 
   def forTierAndCountry(tier: Tier, cg: CountryGroup) = (tier,cg) match {
-    case (Supporter(), CountryGroup.Australia) => ausSupporter
+    case (Supporter(), CountryGroup.Australia) => auSupporter
     case (tier,CountryGroup.UK) => forTier(tier)
     case (tier,_) => forTier(tier).filterNot(b=>marketedOnlyToUK.contains(b))
   }
+
   val DiscountTicketTiers = Set[Tier](Staff(), Partner(), Patron())
   val PriorityBookingTiers = DiscountTicketTiers
   val ComplimentaryTicketTiers = Set[Tier](Partner(), Patron())
 
-  val welcomePack = Benefit("welcome_pack", "Welcome pack, card and gift")
-  val welcomeDog = Benefit("welcome_pack", "Welcome pack")
+  var adFreeApp = Benefit("ad_free_app","Free access to the premium tier of the Guardian app (includes crosswords and has no adverts)")
+  val adFreeAppLocalised = Benefit("ad_free_app","An ad-free experience in our mobile app")
   val accessTicket = Benefit("access_tickets", "Access to tickets")
-  val emailUpdates = Benefit("email_updates", "Regular member emails")
-  var app = Benefit("app","Free access to the premium tier of the Guardian app (includes crosswords and has no adverts)")
-  val priorityBooking = Benefit("priority_booking", "48hrs priority booking")
-  val noBookingFees = Benefit("no_booking_fees", "No booking fees")
-  val guest = Benefit("guest", "Bring a guest")
   val booksOrTickets = Benefit("books_or_tickets", s"$zuoraFreeEventTicketsAllowance tickets or 4 books", isNew = true)
   val booksAndTickets = Benefit("books_and_tickets", s"$zuoraFreeEventTicketsAllowance tickets and 4 books", isNew = true)
-
   val discount = Benefit("discount", "20% discount for you and a guest")
+  val emailsUpdatesJournalists = Benefit("emails_updates_journalists", "Exclusive emails from Guardian journalists")
+  val globalCommunity = Benefit("add_free_app","Joining the global Guardian Members community")
+  val guest = Benefit("guest", "Bring a guest")
+  val liveEvents = Benefit("live_events", "Invitations to Guardian Live events")
+  val noBookingFees = Benefit("no_booking_fees", "No booking fees")
+  val priorityBooking = Benefit("priority_booking", "48hrs priority booking")
+  val regularEmails = Benefit("regular_emails", "Regular member emails")
+  val regularEmailsAU =  Benefit("regular_emails", "Regular behind-the-scenes emails from our newsroom")
+  val regularEmailsUS =  Benefit("regular_emails", "Regular behind-the-scenes emails from our newsroom")
+  val regularEmailsUK =  Benefit("regular_emails", "A weekly look \"Inside the Guardian\" for our Members")
   val uniqueExperiences = Benefit("unique_experiences", "Exclusive behind-the-scenes functions")
+  val welcomePack = Benefit("welcome_pack", "Welcome pack, card and gift")
+  val welcomePackAU = Benefit("welcome_pack", "Welcome letter with First Dog on the Moon certificate")
+  val welcomePackUK = Benefit("welcome_pack", "A welcome gift")
+  val welcomePackUS = Benefit("welcome_pack", "A Guardian branded tote bag")
 
   val marketedOnlyToUK = Set[Benefit](accessTicket)
 
   val friend = Seq(
     accessTicket,
-    emailUpdates
+    regularEmails
   )
 
   val supporter = Seq(
     welcomePack,
-    app,
+    adFreeApp,
     accessTicket,
-    emailUpdates
+    regularEmails
   )
 
-  val ausSupporter = Seq(
-    welcomeDog,
-    app,
-    emailUpdates
+  val auSupporter = Seq(
+    adFreeAppLocalised,
+    regularEmailsAU,
+    welcomePackAU,
+    liveEvents
+  )
+
+  val usSupporter = Seq(
+    adFreeAppLocalised,
+    regularEmailsUS,
+    welcomePackUS,
+    liveEvents
+  )
+
+  val ukSupporter = Seq(
+    emailsUpdatesJournalists,
+    adFreeAppLocalised,
+    regularEmailsUK,
+    globalCommunity,
+    welcomePackUK
   )
 
   val supporterMinimal = Seq(
     welcomePack,
-    app,
-    emailUpdates
+    adFreeApp,
+    regularEmails
   )
+
   val partner = Seq(
     booksOrTickets,
     priorityBooking,
@@ -75,9 +104,10 @@ object Benefits {
     discount,
     guest,
     welcomePack,
-    app,
-    emailUpdates
+    adFreeApp,
+    regularEmails
   )
+
   val patron = Seq(
     booksAndTickets,
     uniqueExperiences,
@@ -86,15 +116,15 @@ object Benefits {
     discount,
     guest,
     welcomePack,
-    app,
-    emailUpdates
+    adFreeApp,
+    regularEmails
   )
 
   val comparisonBasicList = Seq(
     welcomePack,
-    app,
+    adFreeApp,
     accessTicket,
-    emailUpdates
+    regularEmails
   )
 
   val staff = (partner ++ supporter).filterNot(_ == booksOrTickets).distinct
