cf3863052bae24f20e3dc11e46a5b4cbd1951ffa
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2016-12-20 11:54:51 +0000

    Refactored the payment view.

--- frontend/assets/javascripts/src/modules/form/submitButton.js
+++ frontend/assets/javascripts/src/modules/form/submitButton.js
@@ -14,8 +14,7 @@ define(
             if ($SUBMIT_SPAN.length > 0) {
                 $SUBMIT_SPAN.html(template(submitButtonTemplate)(guardian.membership.checkoutForm));
                 if(payload) {
-                    var currentValue = $SUBMIT_BUTTON[0].getAttribute('data-metric-label');
-                    $SUBMIT_BUTTON[0].setAttribute('data-metric-label', currentValue.split('-')[0] + '-' + payload);
+                    $SUBMIT_BUTTON[0].setAttribute('data-metric-label', payload);
                 }
             }
         }
