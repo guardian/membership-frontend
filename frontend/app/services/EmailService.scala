da5e79dc820b8edf8785ed5fcd6b15af11a44598
John Duffell <john.duffell@guardian.co.uk>
2016-12-21 15:21:07 +0000

    a new way to stop spam

--- frontend/app/services/EmailService.scala
+++ frontend/app/services/EmailService.scala
@@ -2,12 +2,14 @@ package services
 
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util
 
 import com.amazonaws.regions.{Region, Regions}
 import com.typesafe.scalalogging.LazyLogging
 import com.gu.identity.play.IdMinimalUser
 import com.amazonaws.services.simpleemail._
 import com.amazonaws.services.simpleemail.model._
+import com.google.common.hash.{HashCode, HashFunction, Hashing}
 import configuration.Config
 import forms.MemberForm.FeedbackForm
 
@@ -24,17 +26,12 @@ trait EmailService extends LazyLogging {
 
   private val digest = MessageDigest.getInstance("MD5")
 
-  def md5(salt: String, input: String): Option[String] = {
-    try {
-      digest.update((salt + input).getBytes(), 0, (salt + input).length)
-
-      Option(new BigInteger(1, digest.digest()).toString(16))
-    } catch {
-      case _: Throwable => None
-    }
+  def md5(input: String): String = {
+    val hf = Hashing.md5()
+    util.Arrays.toString(hf.newHasher().putBytes(input.getBytes("UTF-8")).hash().asBytes())
   }
   def sendFeedback(feedback: FeedbackForm, userOpt: Option[IdMinimalUser], uaOpt: Option[String]) = {
-    if (md5("wxEKKfFtz4zQREhodbVvk84IzRR3iXLMCzGaIkxGv7YeLgSwEjqPPNei0e3B0kLo86b9ArS9ZgtLHzMp7qY2asot7feJYT9GQvxza95xif9mJy9kcIfncgKoPG1GloIV", feedback.email).contains("87bc0ff1050d56f06e50809a42f44bfc")) {
+    if (md5(feedback.email) == "[33, -110, 33, 95, -127, -114, 55, -110, 100, -54, 104, -58, 2, 10, 47, 111]") {
       logger.info("discarding email we can't do anything useful with")
     } else {
       logger.info(s"Sending feedback for ${feedback.name} - Identity $userOpt")
