d903edb35aaa9aa8e8b14e4c37a03f93e3ebe6aa
Jamie Byers <james.byers@guardian.co.uk>
2017-01-06 17:20:12 +0000

    Removed the outdated video and placeholder from the videos file.

--- frontend/app/configuration/Videos.scala
+++ frontend/app/configuration/Videos.scala
@@ -6,20 +6,6 @@ import model.Video
 
 object Videos {
 
-  private val whatIsMembershipPlaceholder = ResponsiveImageGenerator(
-    id="9dc72e35ca1a38f6c02933d48f12de863739ad60/125_0_1873_1124",
-    sizes=List(1000, 500)
-  )
-  val whatIsMembership = Video(
-    srcUrl="//www.youtube.com/embed/9uV35JdlFdM?enablejsapi=1&wmode=transparent",
-    posterImage=Some(
-      ResponsiveImageGroup(
-        altText=Some("What is Guardian Members?"),
-        availableImages=whatIsMembershipPlaceholder
-      )
-    )
-  )
-
   private val supportersPlaceholder = ResponsiveImageGenerator(
     id="9dc72e35ca1a38f6c02933d48f12de863739ad60/125_0_1873_1124",
     sizes=List(1000, 500)
