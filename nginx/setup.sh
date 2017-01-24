aa744f0bd20b2724d015470cdfc281847b6dcbd8
Roberto Tyley <roberto.tyley@gmail.com>
2017-01-24 16:48:25 +0000

    Use the new wildcard cert for *.thegulocal.com
    Relying on the new CA-signed wildcard-cert that's provided by
    https://github.com/guardian/identity-platform/pull/149 means we can delete
    some code - there's not much point in us having different certificates for
    our dev environment.
    
    This change also removes `membership.crt` & `membership.key` from the
    Membership Frontend repository - we haven't been using these keys since
    https://github.com/guardian/membership-frontend/pull/1098 went out in
    April 2016.
    
    Additionally, removing redundant config for `members.thegulocal.com` - we
    ended up staying with `membership.theguardian.com`, and any possible
    change to `members` is likely to be swamped by work done for Simple and
    Coherent Product.

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -1,20 +1,10 @@
 #!/bin/bash -x
 
-GU_KEYS="${HOME}/.gu/keys"
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 NGINX_HOME=$(nginx -V 2>&1 | grep 'configure arguments:' | sed 's#.*conf-path=\([^ ]*\)/nginx\.conf.*#\1#g')
 
 sudo mkdir -p $NGINX_HOME/sites-enabled
 sudo ln -fs $DIR/membership.conf $NGINX_HOME/sites-enabled/membership.conf
-sudo ln -fs $DIR/membership.crt $NGINX_HOME/membership.crt
-sudo ln -fs $DIR/membership.key $NGINX_HOME/membership.key
-
-aws s3 cp s3://identity-local-ssl/idapi-code-proxy-thegulocal-com-exp2017-03-31-bundle.crt ${GU_KEYS}/ --profile membership
-aws s3 cp s3://identity-local-ssl/idapi-code-proxy-thegulocal-com-exp2017-03-31.key ${GU_KEYS}/ --profile membership
-aws s3 cp s3://identity-local-ssl/mem-thegulocal-com-exp2017-03-31-bundle.crt ${GU_KEYS}/ --profile membership
-aws s3 cp s3://identity-local-ssl/mem-thegulocal-com-exp2017-03-31.key ${GU_KEYS}/ --profile membership
-
-sudo ln -fs ${GU_KEYS}/ $NGINX_HOME/keys
 
 sudo nginx -s stop
 sudo nginx
