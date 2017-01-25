b49bb38a8c782297e7ebde53cfc4f7c87f06629d
Roberto Tyley <roberto.tyley@theguardian.com>
2017-01-25 11:25:32 +0000

    Update Nginx setup instructions
    Most of the updates come from the fact you need to perform the
    `Identity-Platform` setup first, which already gives you Nginx
    and a `sites-enabled` folder.
    
    https://github.com/guardian/identity-platform/blob/master/README.md#setup-nginx-for-local-development
    
    The auto-start of nginx with `sudo nginx` removed from `/nginx/setup.sh`
    because it actually causes an inconsistent state on Ubuntu 16.10, where the
    Nginx PID file can not be created.

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -3,9 +3,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 NGINX_HOME=$(nginx -V 2>&1 | grep 'configure arguments:' | sed 's#.*conf-path=\([^ ]*\)/nginx\.conf.*#\1#g')
 
+echo "\n\nUsing NGINX_HOME=$NGINX_HOME"
+
 sudo mkdir -p $NGINX_HOME/sites-enabled
 sudo ln -fs $DIR/membership.conf $NGINX_HOME/sites-enabled/membership.conf
-
-sudo nginx -s stop
-sudo nginx
-
