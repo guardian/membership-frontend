135fcbcfd5f55a95b3f1316d78ef0d45668bfcb3
Jamie Byers <james.byers@guardian.co.uk>
2017-02-21 18:06:19 +0000

    Added a new scenario to the Join Supporter spec for acceptance testing PayPal.

--- frontend/test/acceptance/JoinSupporterSpec.scala
+++ frontend/test/acceptance/JoinSupporterSpec.scala
@@ -30,6 +30,7 @@ class JoinSupporterSpec extends FeatureSpec with Browser
   }
 
   feature("Become a Supporter in UK") {
+
     scenario("User joins as Supporter by clicking 'Become a Supporter' button on Membership homepage", Acceptance) {
       checkDependenciesAreAvailable
 
@@ -101,6 +102,83 @@ class JoinSupporterSpec extends FeatureSpec with Browser
       And("should be signed in as Supporter.")
       assert(ThankYou.userIsSignedInAsSupporter)
     }
+
+    scenario("User joins as Supporter using PayPal", Acceptance) {
+
+      checkDependenciesAreAvailable
+
+      val testUser = new TestUser
+
+      Given("users click 'Become a Supporter' button on Membership homepage")
+
+      When("they land on 'Identity Frontend' page,")
+      val register = pages.Register(testUser)
+      go.to(register)
+      assert(register.pageHasLoaded)
+
+      And("fill in personal details,")
+      register.fillInPersonalDetails()
+
+      And("submit the form to create their Identity account,")
+      register.submit()
+
+      Then("they should land on 'Enter Details' page,")
+      val enterDetails = pages.EnterDetails(testUser)
+      assert(enterDetails.pageHasLoaded)
+
+      When("we switch to the PayPal AB variant")
+      enterDetails.switchToPayPalVariant
+
+      Then("they should have Identity cookies,")
+      Seq("GU_U", "SC_GU_U", "SC_GU_LA").foreach { idCookie =>
+        assert(Driver.cookiesSet.map(_.getName).contains(idCookie))
+      }
+
+      And("should be logged in with their Identity account.")
+      assert(enterDetails.userIsSignedIn)
+
+      When("users select USA delivery country,")
+      enterDetails.changeCountry("US")
+
+      Then("the currency should change.")
+      assert(enterDetails.currencyHasChanged)
+
+      When("Users fill in delivery address details,")
+      enterDetails.fillInDeliveryAddress()
+
+      And("click Continue")
+      enterDetails.clickContinue()
+
+      And("click PayPal button,")
+      enterDetails.payPal
+
+      Then("the PayPal Express Checkout mini-browser should display")
+      enterDetails.switchToPayPal
+      assert(enterDetails.payPalCheckoutHasLoaded)
+
+      When("Users fill in their PayPal credentials")
+      enterDetails.payPalFillInDetails
+
+      And("click 'Log In'")
+      enterDetails.payPalLogin
+
+      Then("payment summary appears")
+      assert(enterDetails.payPalHasPaymentSummary)
+
+      And("it displays the correct amount")
+      assert(enterDetails.payPalSummaryHasCorrectAmount)
+
+      When("User agrees to payment")
+      enterDetails.acceptPayPalPayment
+
+      Then("they should land on 'Thank You' page,")
+      assert(ThankYou.pageHasLoaded)
+
+      And("should be signed in as Supporter.")
+      assert(ThankYou.userIsSignedInAsSupporter)
+
+    }
+
   }
 }
 
