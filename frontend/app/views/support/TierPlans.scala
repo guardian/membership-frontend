5f5077bdc887865983e9569e76322376fec2d3b4
John Duffell <john.duffell@guardian.co.uk>
2017-03-03 15:05:33 +0000

    use the new tidied up catalog structure

--- frontend/app/views/support/TierPlans.scala
+++ frontend/app/views/support/TierPlans.scala
@@ -1,10 +1,12 @@
 package views.support
 import com.gu.i18n.{Country, CountryGroup, Currency}
+import com.gu.memsub.Benefit.PaidMemberTier
 import com.gu.memsub._
 import com.gu.memsub.subsv2.CatalogPlan.{PaidMember, Partner}
 import com.gu.memsub.subsv2._
 import com.gu.salesforce.Tier
 import views.support.MembershipCompat._
+
 import scala.language.implicitConversions
 /**
   * Hack to unify FreeMembershipPlan and PaidMembershipPlans in views
@@ -23,7 +25,7 @@ case class FreePlan(plan: CatalogPlan.FreeMember) extends TierPlans {
   override def currency(country: Country): Option[Currency] = CountryGroup.availableCurrency(currencies)(country)
 }
 
-case class PaidPlans[M <: MonthYearPlans[PaidMember]](plans: M) extends TierPlans {
+case class PaidPlans[M <: PaidMembershipPlans[PaidMemberTier]](plans: M) extends TierPlans {
   override def tier = plans.month.tier
   override def currencies = plans.month.charges.price.currencies.intersect(plans.year.charges.price.currencies)
   override def currency(country: Country): Option[Currency] = CountryGroup.availableCurrency(currencies)(country)
@@ -33,7 +35,7 @@ object TierPlans {
   implicit def fromFreeMembershipPlan(plan: CatalogPlan.FreeMember): TierPlans =
     FreePlan(plan)
 
-  implicit def fromPaidMembershipPlan(plans: MonthYearPlans[CatalogPlan.PaidMember]): TierPlans = {
+  implicit def fromPaidMembershipPlan(plans: PaidMembershipPlans[Benefit.PaidMemberTier]): TierPlans = {
     PaidPlans(plans)
   }
 }
