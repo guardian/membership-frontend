20d9246db33824c289ad83340db7fa5627c9537d
Roberto Tyley <roberto.tyley@theguardian.com>
2017-04-13 13:55:14 +0100

    Remove comments accidentally included
    https://github.com/guardian/membership-frontend/pull/1589/files/aed1c7be121d3179ba173be04257570c4b95e82e#r111363303

--- frontend/app/controllers/Joiner.scala
+++ frontend/app/controllers/Joiner.scala
@@ -215,16 +215,6 @@ object Joiner extends Controller with ActivityTracking with PaymentGatewayErrorH
         s"There has been an error in updating your email. You may already have an Identity account with $googleEmail. Please try signing in with that email."),
       _ => Redirect(routes.Joiner.enterStaffDetails()).flashing("success" ->
         s"Your email address has been changed to $googleEmail"))
-//      yield {
-//        responseCode match {
-//          case 200 => Redirect(routes.Joiner.enterStaffDetails())
-//            .flashing("success" ->
-//              s"Your email address has been changed to $googleEmail")
-//          case _ => Redirect(routes.Joiner.staff())
-//            .flashing("error" ->
-//              s"There has been an error in updating your email. You may already have an Identity account with $googleEmail. Please try signing in with that email.")
-//        }
-//      }
   }
 
   def unsupportedBrowser = CachedAction(Ok(views.html.joiner.unsupportedBrowser()))
