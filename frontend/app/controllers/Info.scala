e1de513932edaf1bfdf83e9068a10a290be8e49d
Joseph Smith <joseph.lochlann@gmail.com>
2017-05-05 10:30:14 +0100

    Roll out new style supporter page to all countries except US (#1616)
    There's evidence to suggest that the landing page design for the UK
    caused a strong uplift in conversions, estimated at 1.73% to 4.01% based on data here: https://docs.google.com/spreadsheets/d/1QzU656ip-TrF2PJTDmfsdOhK6FhYgxr0B-KtA4GuGYI/edit#gid=522088769
    
    So I've done the following:
    
    * Pointed all countries except for the US at the new-style page
    * Deleted a lot of now redundant code
    * Left the US as is since apparently will need specific copy for it which we don't have yet

--- frontend/app/controllers/Info.scala
+++ frontend/app/controllers/Info.scala
@@ -26,87 +26,7 @@ trait Info extends Controller {
 
   val CachedAndOutageProtected = CachedAction andThen PlannedOutageProtection
 
-  def supporterUK = CachedAndOutageProtected { implicit request =>
-    implicit val countryGroup = UK
-
-    val heroImage = ResponsiveImageGroup(
-      name = Some("intro"),
-      metadata = Some(Grid.Metadata(
-        description = Some("Montage of The Guardian Headlines"),
-        byline = None,
-        credit = None
-      )),
-      availableImages = ResponsiveImageGenerator("7b6e7b64f194b1f85bfc0791a23b8a25b72f39ba/0_0_1300_632", Seq(1300, 500), "png")
-    )
-
-    val heroOrientated = OrientatedImages(portrait = heroImage, landscape = heroImage)
-
-    val detailImage = ResponsiveImageGroup(
-      name = Some("intro"),
-      metadata = Some(Grid.Metadata(
-        description = Some("A scene in The Guardian editorial office."),
-        byline = None,
-        credit = None
-      )),
-      availableImages = ResponsiveImageGenerator("dcd0f0f703b1e784a3280438806f2feedf27dfab/0_0_1080_648", Seq(1080, 500))
-    )
-
-    val detailImageOrientated = OrientatedImages(portrait = detailImage, landscape = detailImage)
-
-    Ok(views.html.info.elevatedSupporter(
-      heroOrientated,
-      TouchpointBackend.Normal.catalog.supporter,
-      PageInfo(
-        title = CopyConfig.copyTitleSupporters,
-        url = request.path,
-        description = Some(CopyConfig.copyDescriptionSupporters)
-      ),
-      detailImageOrientated))
-  }
-
-  def supporterAustralia = CachedAndOutageProtected { implicit request =>
-    implicit val countryGroup = Australia
-
-    val heroImage = ResponsiveImageGroup(
-      name = Some("intro"),
-      metadata = Some(Grid.Metadata(
-        description = Some("""Same-Sex marriage activists march in the street during a Same-Sex Marriage rally in Sydney, Sunday, Aug. 9, 2015""".stripMargin),
-        byline = None,
-        credit = Some("Carol Cho/AAP")
-      )),
-      availableImages = ResponsiveImageGenerator("73f50662f5834f4194a448e966637fc88c0b36f6/0_0_5760_3840", Seq(2000, 1000))
-    )
-
-    val heroOrientated = OrientatedImages(portrait = heroImage, landscape = heroImage)
-
-    val pageImages = Seq(
-      ResponsiveImageGroup(
-        name = Some("coral"),
-        metadata = Some(Grid.Metadata(
-          description = Some("The impact of coral bleaching at Lizard Island on the Great Barrier Reef: (left) the coral turns white, known as 'bleaching', in March 2016; (right) the dead coral is blanketed by seaweed in May 2016"),
-          byline = None,
-          credit = None
-        )),
-        availableImages = ResponsiveImageGenerator(
-          id = "03d7db325026227b0832bfcd17b2f16f8eb5cfed/0_167_5000_3000",
-          sizes = List(1000, 500)
-        )
-      ))
-
-    Ok(views.html.info.supporterAustralia(
-      heroOrientated,
-      TouchpointBackend.Normal.catalog.supporter,
-      PageInfo(
-        title = CopyConfig.copyTitleSupporters,
-        url = request.path,
-        description = Some(CopyConfig.copyDescriptionSupporters),
-        navigation = Nil
-      ),
-      pageImages))
-  }
-
-
-  def supporterUSA = CachedAndOutageProtected { implicit request =>
+   def supporterUSA = CachedAndOutageProtected { implicit request =>
     implicit val countryGroup = US
 
     val pageImages = Seq(
@@ -146,73 +66,41 @@ trait Info extends Controller {
     )
   }
 
-  def supporterEurope = CachedAndOutageProtected { implicit request =>
-    implicit val countryGroup = Europe
-
-    val hero = OrientatedImages(
-      portrait = ResponsiveImageGroup(availableImages = Seq(
-        ResponsiveImage(Asset.at("images/join-challenger/s_EU_hero01_980x980.jpg"), 980))),
-      landscape = ResponsiveImageGroup(availableImages = Seq(
-        ResponsiveImage(Asset.at("images/join-challenger/s_EU_hero01_1280x800.jpg"), 1280)))
-    )
-
-    val pageImages = Seq(
-      ResponsiveImageGroup(
-        name = Some("fearless"),
-        availableImages = ResponsiveImageGenerator(
-          id = "88f98c8706beafeae6dc32886ccd71da60e6e7d7/0_0_5212_3129",
-          sizes = List(1000, 500)
-        )
-      )
-    )
-
-    Ok(
-      views.html.info.supporterEurope(
-        hero,
-        TouchpointBackend.Normal.catalog.supporter,
-        PageInfo(
-          title = CopyConfig.copyTitleSupporters,
-          url = request.path,
-          description = Some(CopyConfig.copyDescriptionSupporters),
-          navigation = Nav.internationalLandingPageNavigation
-        ),
-        pageImages
-      )
-    )
-  }
-
   def supporterFor(implicit countryGroup: CountryGroup) = CachedAndOutageProtected { implicit request =>
 
-    val hero = OrientatedImages(
-      portrait = ResponsiveImageGroup(availableImages =
-        ResponsiveImageGenerator("6e2613b6442f1af7109e349eec38cffc0c54df6d/1872_0_3742_3744", Seq(2000, 1000, 500))),
-      landscape = ResponsiveImageGroup(availableImages =
-        ResponsiveImageGenerator("6e2613b6442f1af7109e349eec38cffc0c54df6d/0_0_5611_1979", Seq(2000, 1000, 500)))
+    val heroImage = ResponsiveImageGroup(
+      name = Some("intro"),
+      metadata = Some(Grid.Metadata(
+        description = Some("Montage of The Guardian Headlines"),
+        byline = None,
+        credit = None
+      )),
+      availableImages = ResponsiveImageGenerator("7b6e7b64f194b1f85bfc0791a23b8a25b72f39ba/0_0_1300_632", Seq(1300, 500), "png")
     )
 
-    val pageImages = Seq(
-      ResponsiveImageGroup(
-        name = Some("fearless"),
-        availableImages = ResponsiveImageGenerator(
-          id = "88f98c8706beafeae6dc32886ccd71da60e6e7d7/0_0_5212_3129",
-          sizes = List(1000, 500)
-        )
-      )
+    val heroOrientated = OrientatedImages(portrait = heroImage, landscape = heroImage)
+
+    val detailImage = ResponsiveImageGroup(
+      name = Some("intro"),
+      metadata = Some(Grid.Metadata(
+        description = Some("A scene in The Guardian editorial office."),
+        byline = None,
+        credit = None
+      )),
+      availableImages = ResponsiveImageGenerator("dcd0f0f703b1e784a3280438806f2feedf27dfab/0_0_1080_648", Seq(1080, 500))
     )
 
-    Ok(
-      views.html.info.supporterInternational(
-        hero,
-        TouchpointBackend.Normal.catalog.supporter,
-        PageInfo(
-          title = CopyConfig.copyTitleSupporters,
-          url = request.path,
-          description = Some(CopyConfig.copyDescriptionSupporters),
-          navigation = Nav.internationalLandingPageNavigation
-        ),
-        pageImages
-      )
-    )
+    val detailImageOrientated = OrientatedImages(portrait = detailImage, landscape = detailImage)
+
+    Ok(views.html.info.supporter(
+      heroOrientated,
+      TouchpointBackend.Normal.catalog.supporter,
+      PageInfo(
+        title = CopyConfig.copyTitleSupporters,
+        url = request.path,
+        description = Some(CopyConfig.copyDescriptionSupporters)
+      ),
+      detailImageOrientated))
   }
 
   def patron() = CachedAndOutageProtected { implicit request =>
--- frontend/test/controllers/RedirectsTest.scala
+++ frontend/test/controllers/RedirectsTest.scala
@@ -6,9 +6,9 @@ import Redirects._
 
 class RedirectsTest extends Specification {
   "redirectToSupporterPage" in {
-    redirectToSupporterPage(CountryGroup.UK) must_=== routes.Info.supporterUK()
+    redirectToSupporterPage(CountryGroup.UK) must_=== routes.Info.supporterFor(CountryGroup.UK)
     redirectToSupporterPage(CountryGroup.US) must_=== routes.Info.supporterUSA()
-    redirectToSupporterPage(CountryGroup.Europe) must_=== routes.Info.supporterEurope()
+    redirectToSupporterPage(CountryGroup.Europe) must_=== routes.Info.supporterFor(CountryGroup.Europe)
     redirectToSupporterPage(CountryGroup.Australia) must_=== routes.Info.supporterFor(CountryGroup.Australia)
     redirectToSupporterPage(CountryGroup.Canada) must_=== routes.Info.supporterFor(CountryGroup.Canada)
     redirectToSupporterPage(CountryGroup.RestOfTheWorld) must_=== routes.Info.supporterFor(CountryGroup.RestOfTheWorld)
