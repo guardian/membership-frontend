a6aaa96d9c638883c94c8e547e4fd8659d072500
rupertbates <rupert.bates@guardian.co.uk>
2017-06-27 10:36:27 +0100

    Formatting

--- frontend/app/model/Header.scala
+++ frontend/app/model/Header.scala
@@ -7,11 +7,8 @@ object Header {
 	case object DefaultHeader extends Header
 	case object SimpleHeader extends Header
 	case object BundlesHeader extends Header
-
   case object MasterClassesHeader extends Header
-
   case object LiveHeader extends Header
-
   case object GuardianHeader extends Header
 
 }
