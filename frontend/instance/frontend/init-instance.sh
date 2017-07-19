31c38c6bb2caeb5bdad31c6a4666cc8d0d230825
John Duffell <john.duffell@guardian.co.uk>
2017-07-19 14:45:28 +0100

    install liberato

--- frontend/instance/frontend/init-instance.sh
+++ frontend/instance/frontend/init-instance.sh
@@ -16,4 +16,19 @@ chmod 0600 /etc/gu/membership.private.conf
 # enable cloudwatch log uploader
 /opt/cloudwatch-logs/configure-logs application ${stack} ${stage} ${app} /var/log/frontend/frontend.log
 # enable liberato jvm gather agent
-curl -s https://metrics-api.librato.com/agent_installer/bd15a274590a10f2 | sudo bash
+#see https://www.librato.com/docs/kb/collect/integrations/agent/index.html
+# use the advanced method otherwise it complains the key has expired
+mkdir -p /etc/apt/sources.list.d/
+cp librato_librato-collectd.list /etc/apt/sources.list.d/
+
+sudo apt-get install debian-archive-keyring
+sudo apt-get install apt-transport-https
+curl https://packagecloud.io/gpg.key 2> /dev/null | sudo apt-key add -
+
+cp librato-collectd /etc/apt/preferences.d/
+sudo apt-get update
+sudo apt-get -y install collectd
+
+aws --region ${region} s3 cp s3://membership-private/liberato.sed /tmp/
+mv /opt/collectd/etc/collectd.conf.d/librato.conf /opt/collectd/etc/collectd.conf.d/librato.conf.old
+sed -f /tmp/liberato.sed /opt/collectd/etc/collectd.conf.d/librato.conf.old >/opt/collectd/etc/collectd.conf.d/librato.conf
