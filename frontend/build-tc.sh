6743ca059408fe5cab722ef4d305ab5dc0849d02
John Duffell <john.duffell@guardian.co.uk>
2017-07-19 11:43:21 +0100

    script is called init instance not start

--- frontend/build-tc.sh
+++ frontend/build-tc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo ${BUILD_NUMBER:-DEV} > conf/build.txt
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "##teamcity[publishArtifacts '$DIR/start-instance.sh => .']"
+echo "##teamcity[publishArtifacts '$DIR/init-instance.sh => .']"
