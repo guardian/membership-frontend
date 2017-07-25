3bff49d404f888c9f54bf679cd66f3750a2238f8
John Duffell <john.duffell@guardian.co.uk>
2017-07-25 17:27:39 +0100

    clarify instance script copying comment

--- frontend/build-tc.sh
+++ frontend/build-tc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo ${BUILD_NUMBER:-DEV} > conf/build.txt
+
 # put the script needed on the machine into the package so it ends up on the instance via riffraff
+# the special ##teamcity command causes teamcity to write it to the build output
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "##teamcity[publishArtifacts '$DIR/instance => .']"
