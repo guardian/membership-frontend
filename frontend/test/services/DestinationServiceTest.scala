04b15ba8177e6bbdd1df2c5b879e79be61e1e36b
davidfurey <david.furey@guardian.co.uk>
2018-02-09 14:08:36 +0000

    Scala NEL is now invariant - use CovariantNonEmptyList instead

--- frontend/test/services/DestinationServiceTest.scala
+++ frontend/test/services/DestinationServiceTest.scala
@@ -56,7 +56,7 @@ class DestinationServiceTest extends Specification {
         promoCode = None,
         casActivationDate = None,
         isCancelled = false,
-        plans = scalaz.NonEmptyList(new PaidSubscriptionPlan[Product.Membership, PaidCharge[Partner.type, Month.type]](
+        plans = CovariantNonEmptyList(new PaidSubscriptionPlan[Product.Membership, PaidCharge[Partner.type, Month.type]](
           id = RatePlanId(""), productRatePlanId = ProductRatePlanId(""), name = "name", product = Product.Membership, description = "", features = Nil,
           charges = partnerCharge,
           chargedThrough = None, start = new LocalDate("2015-01-01"), end = new LocalDate("2099-01-01"), productName = "")),
