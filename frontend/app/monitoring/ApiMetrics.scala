5f04ba3b0b0f0e0a2df1ec2e294007ce9e4ae4ae
davidfurey <david.furey@guardian.co.uk>
2018-03-16 17:21:37 +0000

    Make code compile

--- frontend/app/monitoring/ApiMetrics.scala
+++ frontend/app/monitoring/ApiMetrics.scala
@@ -5,6 +5,10 @@ import com.gu.monitoring.{ServiceMetrics, StatusMetrics}
 object DummyMetrics extends ServiceMetrics("", "", "") with Metrics with StatusMetrics {
   override val service = "Dummy Service"
 
+  override val stage = ""
+
+  override val application = ""
+
   override def putResponseCode(status: Int, responseMethod: String): Unit = {}
 
   override def recordRequest() { }
