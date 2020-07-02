7232ffd4b1d571facae94294465bb14769640e41
George Haberis <george.haberis@guardian.co.uk>
2020-07-02 14:17:36 +0100

    store country code in memory

--- frontend/assets/javascripts/src/modules/ccpa.js
+++ frontend/assets/javascripts/src/modules/ccpa.js
@@ -1,6 +1,13 @@
+// ----- Imports ----- //
+
+import { raven } from 'src/modules/raven';
+
 // ----- Functions ----- //
 
 const GEOCOUNTRY_URL = '/geocountry';
+const US_COUNTRY_CODE = 'US';
+
+let countryCode;
 
 export const ccpaEnabled = () => {
     const useCCPA = true; // set false to switch CCPA off
@@ -9,15 +16,21 @@ export const ccpaEnabled = () => {
         return Promise.resolve(false);
     }
 
+    if (countryCode) {
+        return Promise.resolve(countryCode === US_COUNTRY_CODE);
+    }
+
     return fetch(GEOCOUNTRY_URL).then(response => {
         if (response.ok) {
             return response.text();
         } else {
-            throw new Error('failed to get country code');
+            throw new Error('failed to get geocountry');
         }
-    }).then(data => {
-        return data === 'US';
-    }).catch(() => {
+    }).then(responseCountryCode => {
+        countryCode = responseCountryCode;
+        return responseCountryCode === US_COUNTRY_CODE;
+    }).catch(err => {
+        raven.Raven.captureException(err);
         return false;
     });
 };
