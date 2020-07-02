f4510a6fa573103da2ed4be5c438c229d1e1b196
George Haberis <george.haberis@guardian.co.uk>
2020-07-02 09:42:54 +0100

    install node version at build time

--- frontend/build-tc.sh
+++ frontend/build-tc.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo ${BUILD_NUMBER:-DEV} > conf/build.txt
 
+export NVM_DIR="$HOME/.nvm"
+[[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm install
+nvm use
+
 # put the init-instance.sh script into the riff-raff package so that it ends up in S3 ready
 # to be fetched by the instance on boot
 # the special ##teamcity command causes teamcity to write it to the build output
