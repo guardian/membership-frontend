22d8ced22b06602926d67a12068e3ecb4ec2f401
Scala Steward <me@scala-steward.org>
2022-03-01 22:21:12 +0100

    Update scala-uri to 2.2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val jacksonVersion = "2.11.4"
   //libraries
   val sentryRavenLogback = "io.sentry" % "sentry-logback" % "1.7.30"
-  val scalaUri = "io.lemonlabs" %% "scala-uri" % "2.2.2"
+  val scalaUri = "io.lemonlabs" %% "scala-uri" % "2.2.6"
   val identityAuthPlay = "com.gu.identity" %% "identity-auth-play" % "3.251"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.8"
   val membershipCommon = "com.gu" %% "membership-common" % "0.608"
